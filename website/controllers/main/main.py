d84520c7c81d3502712900c51110f9685c4c2c58
Michael Purcaro <purcaro@gmail.com>
2016-06-23 11:47:57 -0400

    make overlap query

---
+++ website/controllers/main/main.py
@@ -0,0 +1,19 @@
+from common.page_info import PageInfoMain
+
+class MainController:
+    def __init__(self, templates, es, version):
+        self.t = templates
+        self.es = es
+        self.version = version
+
+    def Index(self):
+        pageInfo = PageInfoMain(self.es, self.version)
+        return self.t('main/index', **pageInfo.wholePage())
+
+    def Query(self, q, url):
+        pageInfo = PageInfoMain(self.es, self.version)
+        return self.t('main/query', **pageInfo.queryPage(q, url))
+
+    def Overlap(self, chrom, start, end):
+        pageInfo = PageInfoMain(self.es, self.version)
+        return self.t('main/query', **pageInfo.overlapPage(chrom, start, end))
