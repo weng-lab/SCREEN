7e5d2d3154901ceddac498fa647105e36004e551
Henry Pratt <hpratt111@live.com>
2016-06-24 10:17:14 -0400

    try responding to results

--- website/static/searchresults.js
+++ website/static/searchresults.js
@@ -1,3 +1,19 @@
+function process_histogram_result(control_prefix, result)
+{
+    $( "#" + control_prefix + "_range_slider" ).slider( "option", "min", result["minvalue"] );
+    $( "#" + control_prefix + "_range_slider" ).slider( "option", "max", result["maxvalue"] + searchquery["aggs"][control_prefix]["histogram"]["interval"] - 1);
+    searchquery["aggs"][control_prefix]["histogram"]["interval"] = Math.round((result["maxvalue"] - result["minvalue"]) / 50);
+    // draw_histogram(result["datapairs"])
+}
+
+function process_agglist(facetbox_id, agglist)
+{
+    clear_facetlist(facetbox_id);
+    for (var i = 0; i < agglist.datapairs.length; i++) {
+	add_filterresult(facetbox_id, agglist.datapairs[i]);
+    }
+}
+
 function clear_facetlist(facetbox_id)
 {
     var facetbox_div = document.getElementById(facetbox_id + "_facet_container");
@@ -18,8 +34,8 @@ function add_filterresult(id, result)
 
     var name_span = document.createElement("span");
     var rght_span = document.createElement("span");
-    var rght_text = document.createTextNode("(" + result.doc_count + ")");
-    var name_text = document.createTextNode(result.key);
+    var rght_text = document.createTextNode("(" + result[1] + ")");
+    var name_text = document.createTextNode(result[0]);
     name_span.appendChild(name_text);
     rght_span.className = "pull-right";
     rght_span.appendChild(name_text);
