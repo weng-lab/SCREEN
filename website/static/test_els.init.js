900b3d510f5234e906261ae440d6834bff65ab86
Michael Purcaro <purcaro@gmail.com>
2016-06-28 02:10:48 -0400

    throw in table

--- website/static/test_els.init.js
+++ website/static/test_els.init.js
@@ -9,7 +9,7 @@ var facet_link_handlers = {
 };
 
 function socket_message_handler(e) {
-    console.log("received " + e.data);
+    //console.log("received " + e.data);
     results = JSON.parse(e.data);
     if (searchquery.has_chromosome_filter() && document.getElementById("coordinates_facet_panel").style.display == "none")
     {
@@ -17,6 +17,7 @@ function socket_message_handler(e) {
         clear_div_contents(document.getElementById("coordinates_range_slider"));
         create_range_slider("coordinates_range_slider", 2000000, document.getElementById("coordinates_textbox"), update_coordinate_filter, update_coordinate_histogram_selection);
     }
+
     for (aggname in results["aggs"]) {
         if (results["aggs"][aggname]["type"] == "list") {
             process_agglist(aggname, results["aggs"][aggname]);
@@ -24,8 +25,35 @@ function socket_message_handler(e) {
             histograms[aggname] = process_histogram_result(aggname, results["aggs"][aggname]);
         }
     }
-    document.getElementById("searchresults_div").innerHTML = "<strong>" + results.results.total + " results</strong><br><br>";
+
+    var elements = ["accession", "confidence", "genes", "genome", "position", "ranks"];
+    var rtable = $("#searchresults_div");
+    var html = "<strong>" + results.results.total + " results</strong><br><br>";
+    html += '<table class="table">';
+
+    html += "<thead>";
+    $.each(elements, function(i, v) {
+        html += "<th>" + v + "</th>";
+    });
+    html += "</thead>";
+
     for (var i = 0; i < 10 && i < results.results.total; i++) {
-        document.getElementById("searchresults_div").innerHTML += results.results.hits[i]._source.accession + "<br>";
+        //console.log(results.results.hits[i]._source);
+        var r = results.results.hits[i]._source;
+        html += "<tr>";
+        $.each(elements, function(i, v) {
+            if("genes" == v){
+                html += "<td>" + "" + "</td>";
+            }else if("ranks" == v){
+                html += "<td>" + "" + "</td>";
+            }else if("position" == v){
+                var p = r["position"]
+                html += "<td>" + p["chrom"] + ":" + p["start"] + "-" + p["end"] + "</td>";
+            }else {
+                html += "<td>" + String(r[v]) + "</td>";
+            }
+        });
+        html += "</tr>";
     }
+    rtable.html(html);
 }
