15b12ea0359f5d9f927f2234e54bd4ff0ea5a185
Henry Pratt <hpratt111@live.com>
2016-07-07 10:45:20 -0400

    centralize bulk import

--- 2_import/main/01_regelms.py
+++ 2_import/main/01_regelms.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import os, sys
-import requests
-import argparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
-sys.path.append("../../common")
-from es_bulk_importer import ESBulkImporter
-from utils import Utils
-from files_and_paths import Dirs, Tools, Genome, Datasets
-
-def parseargs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--rootpath', type=str, default="../../data/")
-    parser.add_argument("--elasticsearch_server", type=str, default="127.0.0.1")
-    parser.add_argument('--elasticsearch_port', type=int, default=9200)
-    parser.add_argument('--debug', action="store_true", default=False)
-    return parser.parse_args()
+sys.path.append("../common")
+from bulk_es_import import executable_importer
+from files_and_paths import Dirs
 
 def main():
-    args = parseargs()
-    importer = ESBulkImporter(args.elasticsearch_server, args.elasticsearch_port)
-
-    fn = 'regulatory-element-registry-hg19.V2.json.gz'
-    d = os.path.join(Dirs.encyclopedia, "Version-4")
-    fnp = os.path.join(d, fn)
-
-    try:
-        importer.do_import(fnp, "regulatory_elements", doc_type="element")
-    except:
-        if args.debug:
-            raise
-        return 1
-
-    return 0
+    fnp = os.path.join(Dirs.encyclopedia, "Version-4", "regulatory-element-registry-hg19.V2.json.gz")
+    importer = executable_importer(fnp, "regulatory_elements", "element")
+    return importer.exe()
 
 if __name__ == "__main__":
     sys.exit(main())
--- 2_import/main/02_genealiases.py
+++ 2_import/main/02_genealiases.py
@@ -1,46 +1,16 @@
+#!/usr/bin/env python
+
 import os, sys
-import argparse
-import json
 
-from elasticsearch import Elasticsearch
-
-def parseargs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--rootpath', type=str, default="../../data/")
-    parser.add_argument("--elasticsearch_server", type=str, default="127.0.0.1")
-    parser.add_argument('--elasticsearch_port', type=int, default=9200)
-    parser.add_argument("--debug", action="store_true", default=False)
-    return parser.parse_args()
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
+sys.path.append("../common")
+from bulk_es_import import executable_importer
+from files_and_paths import Dirs
 
 def main():
-    args = parseargs()
-    es = Elasticsearch([args.elasticsearch_server],
-                       port = args.elasticsearch_port)
-    rootpath = os.path.join(args.rootpath, "index_name2", "type")
-    if not os.path.exists(args.rootpath):
-        print("directory %s not found" % args.rootpath)
-        sys.exit(0)
-    for subdir, _dirs, filenames in os.walk(os.path.dirname(rootpath)):
-        for fnp in [os.path.join(subdir, f) for f in filenames]:
-            if fnp.endswith("~"): continue
-            try:
-                with open(fnp, "rb") as f:
-                    r = es.index(index="index_name2", doc_type="type", body=json.load(f))
-                if args.debug:
-                    print("index succeeded for %s" % fnp)
-                else:
-                    print("indexed %s at %s" % (fnp, "http://%s:%d/%s" % (args.elasticsearch_server,
-                                                                          args.elasticsearch_port,
-                                                                          fnp.replace(args.rootpath, ""))))
-            except:
-                print("error indexing file %s; check that the file's JSON is valid and elasticsearch is running" % fnp)
-                if args.debug:
-                    e = sys.exc_info()[:2]
-                    print(e[0])
-                    if hasattr(e, "message"): e.message
-                    raise
-            break
-    return 0
+    fnp = os.path.join(Dirs.encyclopedia, "Version-4", "genelist.tsj")
+    importer = executable_importer(fnp, "gene_aliases", "gene")
+    return importer.exe()
 
 if __name__ == "__main__":
     sys.exit(main())
