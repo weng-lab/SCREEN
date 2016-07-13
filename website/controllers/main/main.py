add4ca3dc5f9d27266657c0c40bd1f6923c7cb75
Michael Purcaro <purcaro@gmail.com>
2016-07-13 18:06:11 -0400

    wire in production and change ws url

--- website/controllers/main/main.py
+++ website/controllers/main/main.py
@@ -1,31 +1,32 @@
 from common.page_info import PageInfoMain
 
 class MainController:
-    def __init__(self, templates, es, version):
+    def __init__(self, templates, es, version, production):
         self.t = templates
         self.es = es
         self.version = version
+        self.production = production
 
     def Index(self):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t('main/index', **pageInfo.wholePage())
 
     def RawQuery(self, q, url):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t('main/query', **pageInfo.rawQueryPage(q, url))
 
     def Query(self, q):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t('main/search', **pageInfo.queryPage(q))
 
     def Overlap(self, chrom, start, end):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t('main/query', **pageInfo.overlapPage(chrom, start, end))
 
     def TestQuery(self, args, kwargs):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t('main/test_els', **pageInfo.testqueryPage(args, kwargs))
 
     def HexplotView(self, args, kwargs):
-        pageInfo = PageInfoMain(self.es, self.version)
+        pageInfo = PageInfoMain(self.es, self.version, self.production)
         return self.t("main/hexplot", **pageInfo.hexplotPage(args, kwargs))
