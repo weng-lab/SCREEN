86cac4c3671109687d85bcfa065d98dff36ea8b4
Henry <hpratt111@live.com>
2016-07-14 17:07:31 -0400

    venn fix 2

--- website/static/search.init.js
+++ website/static/search.init.js
@@ -38,7 +38,8 @@ function create_venn(){
     clear_div_contents(document.getElementById("venn_div"));
     venn_results.sets[0].size += venn_results.overlaps[0].size;
     venn_results.sets[1].size += venn_results.overlaps[0].size;
-    venn_slider.set_range(0, GUI.facets[document.getElementById("vennrank_dropdown").value].range_slider.get_range()[1]);
+    var nrange = GUI.facets[document.getElementById("vennrank_dropdown").value].range_slider.get_range();
+    if (!ae(venn_slider.get_range(), nrange)) venn_slider.set_range(...nrange);
     create_venn_diagram("venn_div", venn_results);
 }
 
