b37ad5b6632adbc10b1c9f691dd9f0c1c030f4dd
Michael Purcaro <purcaro@gmail.com>
2016-07-14 16:16:04 -0400

    postgersql restore

--- website/controllers/main/main.py
+++ website/controllers/main/main.py
@@ -1,28 +1,29 @@
 from common.page_info import PageInfoMain
 
 class MainController:
-    def __init__(self, templates, es, version, webSocketUrl):
+    def __init__(self, templates, es, DBCONN, version, webSocketUrl):
         self.t = templates
         self.es = es
+        self.DBCONN = DBCONN
         self.version = version
         self.webSocketUrl = webSocketUrl
 
     def Index(self):
-        pageInfo = PageInfoMain(self.es, self.version, self.webSocketUrl)
+        pageInfo = PageInfoMain(self.es, self.DBCONN, self.version, self.webSocketUrl)
         return self.t('main/index', **pageInfo.wholePage())
 
     def RawQuery(self, q, url):
-        pageInfo = PageInfoMain(self.es, self.version, self.webSocketUrl)
+        pageInfo = PageInfoMain(self.es, self.DBCONN, self.version, self.webSocketUrl)
         return self.t('main/query', **pageInfo.rawQueryPage(q, url))
 
     def search(self, args, kwargs):
-        pageInfo = PageInfoMain(self.es, self.version, self.webSocketUrl)
+        pageInfo = PageInfoMain(self.es, self.DBCONN, self.version, self.webSocketUrl)
         return self.t('main/search', **pageInfo.searchPage(args, kwargs))
 
     def Overlap(self, chrom, start, end):
-        pageInfo = PageInfoMain(self.es, self.version, self.webSocketUrl)
+        pageInfo = PageInfoMain(self.es, self.DBCONN, self.version, self.webSocketUrl)
         return self.t('main/query', **pageInfo.overlapPage(chrom, start, end))
 
     def HexplotView(self, args, kwargs):
-        pageInfo = PageInfoMain(self.es, self.version, self.webSocketUrl)
+        pageInfo = PageInfoMain(self.es, self.DBCONN, self.version, self.webSocketUrl)
         return self.t("main/hexplot", **pageInfo.hexplotPage(args, kwargs))
