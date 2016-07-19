88abd4e6c87a6c8e329e90b85b1fa6633c10ed21
Henry <hpratt111@live.com>
2016-07-19 14:22:26 -0400

    move intersection code to regelm model

--- website/models/regelm_detail.py
+++ website/models/regelm_detail.py
@@ -1,9 +1,10 @@
 import os, sys, json
 
 class RegElementDetails:
-    def __init__(self, es):
+    def __init__(self, es, ps):
         self.index = "regulatory_elements"
         self.es = es
+        self.ps = ps
 
     def reFull(self, reAccession):
         q = { "query" :{
@@ -14,7 +15,20 @@ class RegElementDetails:
                   }
             }
         }
-        return self.es.search(index = self.index, body = q)
+        retval = self.es.search(index = self.index, body = q)
+        if "hits" not in retval or retval["hits"]["total"] == 0:
+            return { "error" : "no hits for " + reAccession}
+        if retval["hits"]["total"] > 1:
+            return { "error" : "too many hits (%d) for " + reAccession }
+        retval["hit"] = retval["hits"]["hits"][0]["_source"]
+        return retval
 
     def get_intersecting_beds(self, reAccession):
-        pass
+        re = self.reFull(reAccession)
+        if "error" in re: return re
+        hit = re["hit"]
+        pos = hit["position"]
+        return {"experiments": self.ps.findBedOverlap(hit["genome"],
+                                                 pos["chrom"],
+                                                 pos["start"],
+                                                 pos["end"] )}
