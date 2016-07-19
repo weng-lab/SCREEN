a38d2d026172d345245fffdfcf1f3012b225f603
Michael Purcaro <purcaro@gmail.com>
2016-07-19 08:19:40 -0400

    split up SNP genomic coord into chrom, start, end

--- 1_pipeline/main/03_snpaliases.py
+++ 1_pipeline/main/03_snpaliases.py
@@ -1,21 +1,23 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 import json
 import sys
 import os
 import requests
+import gzip
 
-sys.path.append("../../../metadata/utils")
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
 from files_and_paths import Dirs, Tools, Genome, Datasets
 
 def main():
     encyclopedia_dir = os.path.join(Dirs.encyclopedia, "Version-4")
     
-    component_fnps = [("mm9", os.path.join(Dirs.dbsnps, "snps128common.mm9.csv")),
-                      ("mm10", os.path.join(Dirs.dbsnps, "snps142common.mm10.csv")),
+    component_fnps = [("mm10", os.path.join(Dirs.dbsnps, "snps142common.mm10.csv")),
                       ("hg19", os.path.join(Dirs.dbsnps, "snps144common.hg19.csv"))]
-    outfnp = os.path.join(encyclopedia_dir, "snplist.lsj")
+    outfnp = os.path.join(encyclopedia_dir, "snplist.lsj.gz")
     
-    with open(outfnp, "wb") as o:
+    with gzip.open(outfnp, "wb") as o:
         for assembly, infnp in component_fnps:
             print("working with assembly %s" % assembly)
             with open(infnp, "r") as f:
@@ -23,8 +25,11 @@ def main():
                 for line in lines[1:]:
                     line = line.split(",")
                     snpobj = {"accession": line[3].strip(),
-                               "coordinates": "%s:%s-%s" % (line[0], line[1], line[2]),
-                               "assembly": assembly }
+                              "position": {
+                                  "chrom": line[0],
+                                  "start": line[1],
+                                  "end": line[2]},
+                              "assembly": assembly }
                     o.write(json.dumps(snpobj) + "\n")
     return 0
 
