715bda1eacd2c0a9d18d42bed9ebd9294326c9db
Michael Purcaro <purcaro@gmail.com>
2016-07-21 15:34:14 -0400

    use int for start and end

--- 1_pipeline/main/03_snpaliases.py
+++ 1_pipeline/main/03_snpaliases.py
@@ -12,11 +12,11 @@ from files_and_paths import Dirs, Tools, Genome, Datasets
 
 def main():
     encyclopedia_dir = os.path.join(Dirs.encyclopedia, "Version-4")
-    
+
     component_fnps = [("mm10", os.path.join(Dirs.dbsnps, "snps142common.mm10.csv")),
                       ("hg19", os.path.join(Dirs.dbsnps, "snps144common.hg19.csv"))]
     outfnp = os.path.join(encyclopedia_dir, "snplist.lsj.gz")
-    
+
     with gzip.open(outfnp, "wb") as o:
         for assembly, infnp in component_fnps:
             print("working with assembly %s" % assembly)
@@ -27,8 +27,8 @@ def main():
                     snpobj = {"accession": line[3].strip(),
                               "position": {
                                   "chrom": line[0],
-                                  "start": line[1],
-                                  "end": line[2]},
+                                  "start": int(line[1]),
+                                  "end": int(line[2])},
                               "assembly": assembly }
                     o.write(json.dumps(snpobj) + "\n")
     return 0
