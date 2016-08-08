915c8c864ce8b4713312bc6fa3fdde6411520491
Michael Purcaro <purcaro@gmail.com>
2016-08-07 22:16:35 -0400

    init webpack

---
+++ website/ui/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
