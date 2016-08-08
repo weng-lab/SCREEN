48213824ee966a424d31ec6440cf492bf2beab34
Michael Purcaro <purcaro@gmail.com>
2016-08-08 16:57:37 -0400

    fiddle

--- website/ui/src/index.jsx
+++ website/ui/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './vot';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const pair = ['asdfating', '28 Days Later'];
 
 ReactDOM.render(
   <Voting pair={pair} />,
