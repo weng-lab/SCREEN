151eb1a1774680d82107a05fd2af96410ec070f6
Michael Purcaro <purcaro@gmail.com>
2016-08-07 22:24:57 -0400

    add webacpk, hot reload, etc.

---
+++ website/ui/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# npm install -g webpack webpack-dev-server
+# npm install --save-dev webpack webpack-dev-server
+
+webpack-dev-server  --host 0.0.0.0 --port 8080
--- website/ui/src/index.js
+++ website/ui/src/index.js
@@ -1,1 +1,10 @@
-console.log('I am alive!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
