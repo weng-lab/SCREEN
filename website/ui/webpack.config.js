5cb2a289a26afe57a94255d68484d553c9f394d8
Michael Purcaro <purcaro@gmail.com>
2016-08-08 17:14:44 -0400

    working on it

--- website/ui/webpack.config.js
+++ website/ui/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
     './src/index'
   ],
@@ -24,8 +24,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist',
-    hot: true
+      contentBase: './dist',
+      hot: true
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
