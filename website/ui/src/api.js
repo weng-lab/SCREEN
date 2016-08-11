91f6473184119c0de3198c835719f121aa7e8813
Michael Purcaro <purcaro@gmail.com>
2016-08-11 14:33:52 -0400

    playing

--- website/ui/src/api.js
+++ website/ui/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+//const url = 'http://megatux.purcaro.com:9002/ver4/search/autocomplete';
+const url = '/datav4/ver4/search/autocomplete';
+
 export const fetchTodos = (userQuery) => {
-    return axios.post('/data/ver4/search/autocomplete',
-		      {userQuery});
+    return axios.post(url, {userQuery});
 };
