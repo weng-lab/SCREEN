c549e84f88e266845e6b070684bf605b16c66691
Henry Pratt <hpratt111@live.com>
2016-08-23 08:47:37 -0400

    gene names pipeline step

--- 1_pipeline/main/02_genealiases.py
+++ 1_pipeline/main/02_genealiases.py
@@ -3,6 +3,7 @@ import json
 import sys
 import os
 import requests
+import gzip
 
 sys.path.append("../../../metadata/utils")
 from files_and_paths import Dirs, Tools, Genome, Datasets
@@ -21,9 +22,32 @@ def get_gene_map(assembly="hg19"):
         retval[gene.genename_] = "%s:%s-%s" % (gene.chr_, gene.start_, gene.end_)
     return retval
 
+def ensembl_to_symbol(fnp, emap):
+    print("converting ensembl IDs to gene symbols")
+    i = 0
+    with gzip.open(fnp, "r") as f:
+        with gzip.open(fnp.replace(".V2.", ".V2.genenames."), "w") as o:
+            for line in f:
+                if i % 100000 == 0: print("working with entry %d\r" % i, end = "")
+                sys.stdout.flush()
+                i += 1
+                d = json.loads(line)
+                pc = d["genes"]["nearest-pc"]["gene-id"].split(".")[0]
+                an = d["genes"]["nearest-all"]["gene-id"].split(".")[0]
+                d["genes"]["nearest-pc"]["gene-name"] = None
+                d["genes"]["nearest-all"]["gene-name"] = None
+                if pc in emap:
+                    d["genes"]["nearest-pc"]["gene-name"] = emap[pc]
+                if an in emap:
+                    d["genes"]["nearest-all"]["gene-name"] = emap[an]
+                o.write(json.dumps(d) + "\n")
+    os.rename(fnp.replace(".V2.", ".V2.genenames."), fnp)
+
 def main():
     infnp = os.path.join(Dirs.encyclopedia, "Version-4", "genelist.tsv")
     outfnp = os.path.join(Dirs.encyclopedia, "Version-4", "genelist.lsj")
+    emap = {}
+    
     i = -1
     skipped = 0
 
@@ -51,12 +75,16 @@ def main():
                            "Vega_ID": line[16],
                            "UCSC_ID": line[17],
                            "mouse_genome_ID": line[18] }
+                emap[geneobj["ensemblid"]] = geneobj["approved_symbol"]
                 if geneobj["ensemblid"] == "":
                     skipped += 1
                     continue
                 if geneobj["approved_symbol"] in hg19_genes:
                     geneobj["coordinates"] = hg19_genes[geneobj["approved_symbol"]]
                 o.write(json.dumps(geneobj) + "\n")
+
+    ensembl_to_symbol(os.path.join(Dirs.encyclopedia, "Version-4", "regulatory-element-registry-hg19.V2.json.new.gz"),
+                      emap)
                 
     print("wrote %d gene objects less %d skipped" % (i, skipped))
     return 0
