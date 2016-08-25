ba8bd74cb00002468a51f51822ad6c398829d8e7
Michael Purcaro <purcaro@gmail.com>
2016-08-25 15:59:17 -0400

    cut out rest of genes

--- 1_pipeline/main/02_genealiases.py
+++ 1_pipeline/main/02_genealiases.py
@@ -38,14 +38,20 @@ def ensembl_to_symbol(inFnp, outFnp, emap):
                 sys.stdout.flush()
                 i += 1
                 d = json.loads(line)
-                pc = d["genes"]["nearest-pc"]["gene-id"].split(".")[0]
-                an = d["genes"]["nearest-all"]["gene-id"].split(".")[0]
-                d["genes"]["nearest-pc"]["gene-name"] = None
-                d["genes"]["nearest-all"]["gene-name"] = None
+
+                gpc = d["genes"]["nearest-pc"]["gene1"]
+                d["genes"]["nearest-pc"] = gpc
+                pc = gpc["gene name"].split(".")[0]
                 if pc in emap:
                     d["genes"]["nearest-pc"]["gene-name"] = emap[pc]
+
+                gall = d["genes"]["nearest-all"]["gene1"]
+                an = gall["gene name"].split(".")[0]
+                d["genes"]["nearest-all"] = gall
+                d["genes"]["nearest-all"]["gene-name"] = None
                 if an in emap:
                     d["genes"]["nearest-all"]["gene-name"] = emap[an]
+                    
                 o.write(json.dumps(d) + "\n")
 
 def tryparse(coordinate):
