e75a48ee6e0a57dcc72355412ac9b93749e458f9
Michael Purcaro <purcaro@gmail.com>
2016-08-24 22:04:46 -0400

    oops

--- website/run_prod.sh
+++ website/run_prod.sh
@@ -4,6 +4,6 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 while [ 1 ]; do
-    $SCRIPTPATH/server.py --production --elasticsearch_port=8014
+    $SCRIPTPATH/server.py --production --websocket_port=8014
     sleep 5
 done
