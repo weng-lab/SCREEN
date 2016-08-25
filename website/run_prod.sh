fa9239f2acbb9d079c541e0ba8cfeebaf3d36748
Michael Purcaro <purcaro@gmail.com>
2016-08-24 22:01:35 -0400

    set correct port

--- website/run_prod.sh
+++ website/run_prod.sh
@@ -4,6 +4,6 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 while [ 1 ]; do
-    $SCRIPTPATH/server.py --production
+    $SCRIPTPATH/server.py --production --elasticsearch_port=8014
     sleep 5
 done
