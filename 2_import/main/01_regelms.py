3a256d1715ae0a8f1962859ec7731402344e188f
Michael Purcaro <purcaro@gmail.com>
2016-08-25 21:14:56 -0400

    merge commons; use paths

--- 2_import/main/01_regelms.py
+++ 2_import/main/01_regelms.py
@@ -3,7 +3,7 @@
 import os, sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
-sys.path.append(os.path.join(os.path.dirname(__file__), "../common"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
 from bulk_es_import import executable_importer
 from files_and_paths import Dirs
 
