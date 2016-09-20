fff1e35ddbf612e294bee61e1d3f310497a5b92e
hpratt <hpratt111@live.com>
2016-09-20 14:38:00 -0400

    add missing function

--- common/common.py
+++ common/common.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 import sys
+import time
 
 def printr(s):
     print(s, end="\r")
     sys.stdout.flush()
+
+def printt(s):
+    print("%s %s" % (time.strftime("%m/%d/%Y %H:%M:%S"), s))
