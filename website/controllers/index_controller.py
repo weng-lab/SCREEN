86efb67fa268fcfb135e0b98470e0c666a3a3439
Michael Purcaro <purcaro@gmail.com>
2016-10-12 12:10:40 -0400

    whitespace

---
+++ website/controllers/index_controller.py
@@ -0,0 +1,14 @@
+import cherrypy
+
+from common.page_info import PageInfoMain
+from common.session import Sessions
+
+class IndexController:
+    def __init__(self, templates, es, ps):
+        self.t = templates
+        self.ps = ps
+        self.params = (es, ps)
+
+    def Index(self):
+        pageInfo = PageInfoMain(*self.params)
+        return self.t('main/index', **pageInfo.wholePage(True))
