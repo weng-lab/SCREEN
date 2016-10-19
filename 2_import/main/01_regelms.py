409b9c6735328c47afbd6d2416de785165f1aa8b
Michael Purcaro <purcaro@gmail.com>
2016-10-19 13:00:17 -0400

    make cell types and tissue stable

--- 2_import/main/01_regelms.py
+++ 2_import/main/01_regelms.py
@@ -2,6 +2,9 @@
 
 import os, sys, argparse
 
+import importlib
+LoadCellTypes = importlib.import_module('10_cellTypes')
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
 from bulk_es_import import executable_importer
 from constants import paths, chroms
@@ -15,6 +18,7 @@ def parse_args():
     parser.add_argument("--fnp", type=str, default="")
     parser.add_argument("--elasticsearch_server", type=str, default="127.0.0.1")
     parser.add_argument('--elasticsearch_port', type=int, default=9200)
+    parser.add_argument('--local', action="store_true", default=False)
     parser.add_argument('--assembly', type=str, default="hg19")
     return parser.parse_args()
 
@@ -32,6 +36,8 @@ def main():
                                    args.elasticsearch_server,
                                    args.elasticsearch_port)
     importer.exe(500)
+
+    LoadCellTypes.LoadCellTypes.Run(args)
 
 if __name__ == "__main__":
     sys.exit(main())
