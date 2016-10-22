6f26ad9dbde19c4dabd528ebd38904306034e589
Michael Purcaro <purcaro@gmail.com>
2016-10-21 21:07:08 -0400

    auto make cols

--- website/ui/src/apps/search/components/details_app.js
+++ website/ui/src/apps/search/components/details_app.js
@@ -13,6 +13,41 @@ class DetailsApp extends React.Component {
 	var tabs = this.props.tabs;
 	var tables = this.props.tables;
 	var data = this.props.data;
+	
+	function tabEle(key, table, numCols){
+	    return (<div className={"col-md-" + (12/numCols)} key={key}>
+		    <h4>{table.title}</h4>
+		    <ResultsTable cols={table.cols} order={table.order} paging={table.paging}
+		    bInfo={table.bInfo} bFilter={table.bFilter} data={data[key]} /><br/>
+		    </div>);
+	}
+
+	function chunkArr(arr, chunk){
+	    // from https://jsperf.com/array-splice-vs-underscore
+	    var i, j, temparray = [];
+	    for (i = 0, j = arr.length; i < j; i += chunk) {
+		temparray.push(arr.slice(i, i + chunk));
+	    }
+	    return temparray;
+	}
+	
+	function tabEles(tables, numCols = 4){
+	    var cols = [];
+	    for(var key of Object.keys(tables)){
+		cols.push(tabEle(key, tables[key], numCols));
+	    };
+	    if(0 == numCols){
+		return cols;
+	    }
+	    var chunks = chunkArr(cols, numCols);
+	    var ret = []
+	    for(var i = 0; i < chunks.length; i++) {
+		var chunk = chunks[i];
+		ret.push(<div className="row" key={"chunk" + i}>{chunk}</div>);
+	    }
+	    return (<div>{ret}</div>);
+	}
+
 	return (<div className="container" style={{width: "100%"}}>
 		    <h3>{this.props.q.accession}</h3>
 		    <ul className="nav nav-tabs">
@@ -25,14 +60,7 @@ class DetailsApp extends React.Component {
 		        {Object.keys(tabs).map((k) => {
 		            var tab = tabs[k];
 			    return (<div className={k == 0 ? "tab-pane active" : "tab-pane"} id={"tab_" + k} key={"tpane_" + k}>
-		                       {Object.keys(tab.tables).map((key) => {
-		                           var table = tab.tables[key];
-					   console.log(table);
-					   return (<div className="col-md-3" key={key}>
-			  	               <h4>{table.title}</h4>
-						   <ResultsTable cols={table.cols} order={table.order} paging={table.paging} bInfo={table.bInfo} bFilter={table.bFilter} data={data[key]} /><br/>
-				           </div>);
-		                       })}
+		                    {tabEles(tab.tables, tab.numCols)}
 				    </div>);
 			})}
 		    </div>
