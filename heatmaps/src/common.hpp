afa91b7df86b7a6d7bacc5ab01e95666e5e5c3dd
Michael Purcaro <purcaro@gmail.com>
2016-10-26 01:54:32 -0400

    const ref

--- heatmaps/src/common.hpp
+++ heatmaps/src/common.hpp
@@ -1,7 +1,7 @@
 typedef std::function<double(const Heatmap &, int, int)> distance_function;
 typedef std::vector<int> clusterlist;
 
-int index_of_min(std::vector<double> d) {
+int index_of_min(const std::vector<double>& d) {
   return std::distance(d.begin(),
 		       std::min_element(d.begin(), d.end()));
 }
