5cd6c5b415826ba6f599eac68b502ef5b4a1c626
Michael Purcaro <purcaro@gmail.com>
2016-10-26 02:30:47 -0400

    fiddle w/ cout

--- heatmaps/src/main.cpp
+++ heatmaps/src/main.cpp
@@ -8,11 +8,11 @@ int main(int argc, char **argv) {
 					  {3.1, 3.0, 3.1, 3.0},
 					  {2.0, 2.0, 2.1, 2.0}};
   zlab::Heatmap hm(matrix);
-  std::cout << hm << "\n";
+  std::cout << "start:\n" << hm << "\n";
   
   hm.RowCluster();
-  std::cout << hm << "\n";
+  std::cout << "cluster by row:\n" << hm << "\n";
   
   hm.ColCluster();
-  std::cout << hm << "\n";
+  std::cout << "cluster by col:\n" << hm << "\n";
 }
