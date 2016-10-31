18142adfb898e245578e27443caa77f33c82cb8e
Henry Pratt <hpratt111@live.com>
2016-10-31 13:09:32 -0400

    add cell types to elasticsearch for fuzzy searching

---
+++ 1_pipeline/main/10_cellTypes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys, os, argparse
+import json
+
+def main():
+    output = []
+    with open(os.path.join(os.path.dirname(__file__), "../../celltypes.txt"), "r") as f:
+        ct = json.loads(f.read())
+    for k, v in ct.iteritems():
+        output.append({ "cell_type": k, "tissue": v })
+    with open(os.path.join(os.path.dirname(__file__), "../../celltypes.lsj"), "wb") as o:
+        for item in output:
+            o.write(json.dumps(item) + "\n")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
