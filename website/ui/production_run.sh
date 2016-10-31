c8a4accc73ec23f9cb781b685a78e510ba998b93
Michael Purcaro <purcaro@gmail.com>
2016-10-31 14:28:21 -0400

    one line expot NODE_ENV = production

--- website/ui/production_run.sh
+++ website/ui/production_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-webpack -p --config ./webpack.production.config.js --watch --progress --colors
+NODE_ENV="production" webpack -p --config ./webpack.production.config.js --watch --progress --colors
