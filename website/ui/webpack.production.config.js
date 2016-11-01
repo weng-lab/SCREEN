8cd0b4af6a1dc4fb6adde7ee59feb67c333be2fe
Michael Purcaro <purcaro@gmail.com>
2016-10-31 20:27:00 -0400

    fix production?

--- website/ui/webpack.production.config.js
+++ website/ui/webpack.production.config.js
@@ -42,7 +42,9 @@ module.exports = {
 	new webpack.ProvidePlugin({
 	    "$": "jquery",
 	    "jQuery": "jquery",
-	    "window.jQuery": "jquery"
+	    "window.jQuery": "jquery"}),
+	new webpack.DefinePlugin({
+	    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
 	})
     ]
 };
