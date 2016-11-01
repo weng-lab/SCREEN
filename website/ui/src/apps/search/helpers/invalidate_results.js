a6a64eaadee350bdc74d9e8c9b59b111803fc6e8
Henry Pratt <hpratt111@live.com>
2016-11-01 11:27:08 -0400

    fixes for vertical bar query

--- website/ui/src/apps/search/helpers/invalidate_results.js
+++ website/ui/src/apps/search/helpers/invalidate_results.js
@@ -77,12 +77,6 @@ export const invalidate_results = (state) => {
     return (dispatch) => {
 
 	var n_query = FacetQueryMap(state);
-
-	Object.keys(state.results_displays).map((k) => {
-	    var r = state.results_displays[k];
-	    QueryAJAX(r.append_query(n_query), d_success(k), d_error(k));
-	    r.dispatcher(dispatch)({type: SET_LOADING});
-	});
 	
 	var e_success = (response, status, jqxhr) => {
 	    dispatch(expression_done(response));
@@ -108,6 +102,11 @@ export const invalidate_results = (state) => {
 	dispatch(results_fetching());
 	dispatch(expression_loading());
 	QueryAJAX(n_query, f_success, f_error);
+	Object.keys(state.results_displays).map((k) => {
+	    var r = state.results_displays[k];
+	    QueryAJAX(r.append_query(n_query), d_success(k), d_error(k));
+	    r.dispatcher(dispatch)({type: SET_LOADING});
+	});	
 	ExpressionAJAX(n_query, e_success, f_error);
 	
     }
