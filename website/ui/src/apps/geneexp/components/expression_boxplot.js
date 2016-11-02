2177dd96f4e2c8c1c4cc8a81505e5b7a0c5defd9
Michael Purcaro <purcaro@gmail.com>
2016-11-02 11:48:52 -0400

    more wiring; seperate json

--- website/ui/src/apps/geneexp/components/expression_boxplot.js
+++ website/ui/src/apps/geneexp/components/expression_boxplot.js
@@ -7,9 +7,9 @@ import Boxplot from '../../../common/components/boxplot'
 
 const props_map = (state) => {
     return {
-	data: state.results.expression_boxplot.data,
-	mmax: state.results.expression_boxplot.mmax,
-	loading: state.results.expression_boxplot.fetching
+	data: GEdata, //state.results.expression_boxplot.data,
+	mmax: GEmmax, //state.results.expression_boxplot.mmax,
+	loading: false //state.results.expression_boxplot.fetching
     };
 };
 
--- website/ui/src/apps/geneexp/config/maintabs.js
+++ website/ui/src/apps/geneexp/config/maintabs.js
@@ -11,14 +11,14 @@ import {main_comparison_connector} from '../reducers/root_reducer'
 
 export const maintabs = {
     id: "main",
-    selection: "results",
+    selection: "gene_expression",
     tabs: {
 	gene_expression: {
 	    title: "Gene Expression",
 	    visible: true,
 	    render: (store, key) => {
 		var ExpressionBoxplot = expression_boxplot_connector(Boxplot);
-		return <ExpressionBoxplot store={store} key={key} />;
+		return <ExpressionBoxplot store={store} key={key}/>;
 	    }
 	}
     }
