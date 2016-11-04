ff47cb98bea8b32b55d7fd062ffeada4cb595149
Michael Purcaro <purcaro@gmail.com>
2016-11-04 14:36:01 -0400

    reverting browser buttons to old way (but adding async: false); cleaning up geneexp

--- website/ui/src/index.js
+++ website/ui/src/index.js
@@ -4,15 +4,17 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, Link, withRouter } from 'react-router';
 import thunk from 'redux-thunk';
+//import createLogger from 'redux-logger';
 
 import SearchPage from './apps/search/main';
 import CartPage from './apps/cart/main';
 import GeneExpPage from './apps/geneexp/main';
-import ComparisonPage from './apps/search/comparison';
+//import * as cart_app from './apps/cart/main';
+
+//console.log(Route);
 
 ReactDOM.render((<Router history={browserHistory}>
 		    <Route path="/search(.*)" component={SearchPage} />
 		    <Route path="/cart" component={CartPage} />
-		    <Route path="/geneexp" component={GeneExpPage} />
-		    <Route path="/comparison" component={ComparisonPage} />
+		    <Route path="/geneexp(.*)" component={GeneExpPage} />
 		 </Router>), document.getElementById('root'));
