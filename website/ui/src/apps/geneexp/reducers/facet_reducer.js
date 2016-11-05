51021bc16f05dcb214515a4a5adafd84a4e697ac
Michael Purcaro <purcaro@gmail.com>
2016-11-04 22:15:03 -0400

    dead

--- website/ui/src/apps/geneexp/reducers/facet_reducer.js
+++ website/ui/src/apps/geneexp/reducers/facet_reducer.js
@@ -1,37 +1,18 @@
-export const HIDE_FACET = 'HIDE_FACET';
-export const SHOW_FACET = 'SHOW_FACET';
-
 const facet_default_state = (subreducer) => {
     return {
 	title: "",
 	visible: false,
 	subreducer: null,
 	state: subreducer(null),
-	es_field: null,
-	es_map: null
     };
 };
 
 const FacetReducer = (subreducer) => (state = facet_default_state(subreducer), action) => {
 
     if (action == null) return state;
-
-    switch (action.type) {
-
-    case HIDE_FACET:
-	return Object.assign({}, state, {
-	    visible: false
-	});
-
-    case SHOW_FACET:
-	return Object.assign({}, state, {
-	    visible: true
-	});
-
+    
     return Object.assign({}, state, {
 	state: subreducer(state.state, action)
     });
-	
-    };
 };
 export default FacetReducer;
