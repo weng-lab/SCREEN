043d4eea7412e97b5d690d280421ee3e056d408a
Henry Pratt <hpratt111@live.com>
2016-11-12 11:24:49 -0500

    fix TSS bar graph

--- website/models/tss_bar.py
+++ website/models/tss_bar.py
@@ -1,20 +1,9 @@
 import os, sys
 
 class TSSBarGraph:
-    def __init__(self, histogram, interval = 1):
-        self.histogram = histogram
-        self.interval = interval
+    def __init__(self, _list):
+        self._list = _list
 
-    def rebin(self, new_bins):
-        new_bins.append(1e12)
-        for k in new_bins:
-            if k != 1e12 and k % self.interval != 0:
-                raise Exception("cannot rebin histogram: original bins do not align with new bins")
-        retval = {k: 0 for k in new_bins}
-        for v in self.histogram["buckets"]:
-            for nbi in new_bins:
-                if nbi > v["key"]:
-                    retval[nbi] += v["doc_count"]
-                    break
-        keys = sorted([k for k, v in retval.iteritems()])
-        return [{"key": k, "value": retval[k]} for k in keys]
+    def format(self):
+        return [{"key": int(k.split("-")[1].split(".")[0]) if len(k.split("-")) > 1 and k.split("-")[1] != "*" else 1e12,
+                "value": v} for k, v in self._list["datapairs"].iteritems()]
