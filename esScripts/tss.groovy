ef43e7ae91df3d36d839338dec1fe2114ff36ed4
Henry Pratt <hpratt111@live.com>
2016-11-29 09:43:55 -0500

    fix vertical bar plot

--- esScripts/tss.groovy
+++ esScripts/tss.groovy
@@ -1,1 +1,6 @@
-_source['genes']['nearest-all'][0].distance
+def result = 1000000000;
+def c = {
+   if (it['distance'] < result) result = it['distance'];
+}
+_source['genes']['nearest-all'].each(c);
+return result;
