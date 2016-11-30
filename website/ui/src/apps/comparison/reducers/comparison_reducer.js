b56766abb714ad728cb2bf95d0f74cfbb5bff368
Henry Pratt <hpratt111@live.com>
2016-11-30 14:15:55 -0500

    play with chromosome similarity maps for DNase

--- website/ui/src/apps/comparison/reducers/comparison_reducer.js
+++ website/ui/src/apps/comparison/reducers/comparison_reducer.js
@@ -2,12 +2,16 @@ import {get_root_reducer} from '../../search/reducers/root_reducer'
 import VennReducer from './venn_reducer'
 import ResultsReducer, {default_formatter} from './results_reducer'
 import {venn_connector} from '../components/venn'
+import {histogram_connector} from '../components/histogram'
 import {tablelist_connector} from '../components/table_list'
 
 import ResultsTableColumns, {table_order} from '../../search/config/results_table'
 
 export const main_venn_connector = (store) => venn_connector(store)((state) => (state.venn), (dispatch) => (dispatch));
 export const main_results_connector = tablelist_connector((state) => (state.results), (dispatch) => (dispatch));
+export const main_histogram_connector = histogram_connector((state) => (state.chrom_spearman));
+
+export const SET_SPEARMAN = 'SET_SPEARMAN';
 
 const table_layout = {
     cols: ResultsTableColumns,
@@ -20,13 +24,24 @@ export const get_root_comparison_reducer = (tabs) => {
     var default_state = Object.assign({}, root_reducer());
     default_state = Object.assign(default_state, {
 	results: Object.assign(default_state.results, results_reducer()),
-	venn: VennReducer()
+	venn: VennReducer(),
+	chrom_spearman: {
+	    histograms: {}
+	}
     });
     return (state = default_state, action) => {
+
 	var n_state = root_reducer(state, action);
+
+	switch (action.type) {
+	case SET_SPEARMAN:
+	    n_state.chrom_spearman.histograms = action.chrom_spearman;
+	}
+	
 	return Object.assign(n_state, {
 	    venn: VennReducer(n_state.venn, action),
 	    results: results_reducer(n_state.results, action)
 	});
+	
     };
 };
