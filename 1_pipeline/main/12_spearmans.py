e59c214f7435a016c4b626939d30093adc415bf3
Henry Pratt <hpratt111@live.com>
2016-12-05 06:07:28 -0500

    parallelize correlation

--- 1_pipeline/main/12_spearmans.py
+++ 1_pipeline/main/12_spearmans.py
@@ -4,6 +4,8 @@ import os
 import sys
 import argparse
 
+from joblib import Parallel, delayed
+
 from elasticsearch import Elasticsearch
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
 from elastic_search_wrapper import ElasticSearchWrapper
@@ -24,8 +26,8 @@ marks = {"DNase": ("dnase", None),
          "H3K4me3_DNase": ("promoter", "DNase+H3K4me3"),
          "H3K27ac": ("enhancer", "H3K27ac-Only"),
          "H3K27ac_DNase": ("enhancer", "DNase+H3K27ac"),
-         "CTCF": ("CTCF", "CTCF-Only"),
-         "CTCF_DNase": ("CTCF", "DNase+CTCF") }
+         "CTCF": ("ctcf", "CTCF-Only"),
+         "CTCF_DNase": ("ctcf", "DNase+CTCF") }
 
 def _find_in_list(_list, i):
     try:
@@ -89,10 +91,11 @@ class Correlator:
         for k, v in self.cts.iteritems():
             self.cell_types[v - 1] = k
         
-    def do_correlation(self, _chr, res, fields = ("dnase", None)):
+    def do_correlation(self, _chr, res, fields = {"DNase": ("dnase", None)}):
         start = 0
-        _ret = []
+        _ret = {k: [] for k, v in fields.iteritems()}
         totals = []
+        r = {}
         self._query["query"]["bool"]["must"][0]["match"]["position.chrom"] = _chr
 
         # perform query for each bin on the chromosome
@@ -101,29 +104,32 @@ class Correlator:
             self._query["query"]["bool"]["must"][1]["range"]["position.end"]["gte"] = start
             results = self.es.search(body = self._query, index = paths.re_json_index)["hits"]["hits"]
             start += res
-            _ret.append(Correlation(results).spearmanr(fields[0], fields[1]))
             totals.append(len(results))
+            for k, field in fields.iteritems():
+                _ret[k].append(Correlation(results).spearmanr(field[0], field[1]))
 
-        # reorder according to cell type pair
-        ret = [[[] for j in range(0, len(self.cell_types))] for i in range(0, len(self.cell_types))]
-        for labels, _corr in _ret:
-            corr, pval = _corr
+        for field, _ in fields.iteritems():
+                
+            # reorder according to cell type pair
+            ret = [[[] for j in range(0, len(self.cell_types))] for i in range(0, len(self.cell_types))]
+            for labels, _corr in _ret[field]:
+                corr, pval = _corr
+                for i in range(0, len(self.cell_types)):
+                    _ii = _find_in_list(labels, self.cell_types[i])
+                    for j in range(i + 1, len(self.cell_types)):
+                        _ij = _find_in_list(labels, self.cell_types[j])
+                        ret[i][j].append(corr[_ii][_ij] if _ii != -1 and _ij != -1 else 0.0)
+
+            # format for database insertion
+            r[field] = []
             for i in range(0, len(self.cell_types)):
-                _ii = _find_in_list(labels, self.cell_types[i])
+                if self.cell_types[i] not in self.cts: continue
                 for j in range(i + 1, len(self.cell_types)):
-                    _ij = _find_in_list(labels, self.cell_types[j])
-                    ret[i][j].append(corr[_ii][_ij] if _ii != -1 and _ij != -1 else 0.0)
-
-        # format for database insertion
-        r = []
-        for i in range(0, len(self.cell_types)):
-            if self.cell_types[i] not in self.cts: continue
-            for j in range(i + 1, len(self.cell_types)):
-                if self.cell_types[j] not in self.cts: continue
-                r.append({"ct1": self.cts[self.cell_types[i]],
-                          "ct2": self.cts[self.cell_types[j]],
-                          "chr": _chr, "resolution": res,
-                          "correlation": ret[i][j] })
+                    if self.cell_types[j] not in self.cts: continue
+                    r[field].append({"ct1": self.cts[self.cell_types[i]],
+                                     "ct2": self.cts[self.cell_types[j]],
+                                     "chr": _chr, "resolution": res,
+                                     "correlation": ret[i][j] })
         
         return (totals, r)
             
@@ -131,31 +137,34 @@ def parseargs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--local', action="store_true", default=False)
     parser.add_argument('--assembly', type=str, default="hg19")
+    parser.add_argument('-j', type=int, default=8)
     return parser.parse_args()
+
+# for parallelizing
+def run(_chr, res, local, assembly):
+    DBCONN = db_connect(os.path.realpath(__file__), local)
+    db = DB(DBCONN, assembly)
+    es = ElasticSearchWrapper(Elasticsearch())
+    correlator = Correlator(es, db, assembly)
+    try:
+        totals, corrs = correlator.do_correlation(_chr, res, marks)
+        for k, v in marks.iteritems():
+            print("inserting %s/%d (%s)" % (_chr, res, k))
+            db.insert_correlations(k, corrs[k])
+    except:
+        print("failed to insert %s/%d" % (_chr, res))
+    db.insert_totals(_chr, res, totals)
 
 def main():
     args = parseargs()
 
-    # connect to DB, elasticsearch, create wrapper objects
+    # connect to DB, recreate tables
     DBCONN = db_connect(os.path.realpath(__file__), args.local)
-    es = ElasticSearchWrapper(Elasticsearch())
-    db = DB(DBCONN, args.assembly)
-    correlator = Correlator(es, db, args.assembly)
-
-    # recreate tables
-    db.recreate_tables()
-
+    DB(DBCONN, args.assembly).recreate_tables()
+    
     # do correlation for each combo and insert
     for res in [1000000]:
-        for _chr in ["chr22"]: #chroms[args.assembly]:
-            for k, v in marks.iteritems():
-                try:
-                    totals, corrs = correlator.do_correlation(_chr, res, v)
-                    print("inserting %s/%d (%s)" % (_chr, res, k))
-                    db.insert_correlations(k, corrs)
-                except:
-                    print("failed to insert %s/%d (%s)" % (_chr, res, k))
-            db.insert_totals(_chr, res, totals)
+        Parallel(n_jobs = args.j)(delayed(run)(_chr, res, args.local, args.assembly) for _chr in chroms[args.assembly])
             
     return 0
 
