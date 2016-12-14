603cf30d627b6a9986a6a42890484cedaaeef734
Pratt <hpratt111@live.com>
2016-12-13 22:04:29 -0500

    detach gene alias resolution from elasticsearch
    Former-commit-id: 2f6087931ea1267ce88449daed23cb4053784253

--- 1_pipeline/rna_seq/02_fc.py
+++ 1_pipeline/rna_seq/02_fc.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
 from postgres_wrapper import PostgresWrapper
 from elastic_search_wrapper import ElasticSearchWrapper
 from dbconnect import db_connect
+from constants import paths, chroms
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../website/common"))
 from compute_gene_expression import ComputeGeneExpression
@@ -15,20 +16,62 @@ from coord import Coord
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
 from db_utils import getcursor
+from get_tss import Genes
 
 def _unpack_tuple_array(a):
     return ([i[0] for i in a], [i[1] for i in a])
 
 class ResolveAlias:
-    def __init__(self, es):
+    def __init__(self, es, assembly):
         self.es = es
+        self.genemap = self._get_gene_map(assembly)
+        self.symbolmap = self._map_symbols(assembly)
+
+    def _get_gene_map(self, assembly):
+        if assembly not in paths.gene_files:
+            print("WARNING: cannot get gene coordinates for assembly",
+                  assembly, "-- no gene file found")
+            return
+        fnp, filetype = paths.gene_files[assembly]
+        ggff = Genes(fnp, filetype)
+        ret = {}
+        for g in ggff.getGenes():
+            ret[g.genename_] = Coord.parse("%s:%s-%s" % (g.chr_, g.start_, g.end_))
+        return ret
+
+    def _map_symbols(self, assembly):
+        ret = {}
+        with open(paths.genelist[assembly], "r") as f:
+            for idx, line in enumerate(f):
+                if idx == 0:
+                    continue
+                line = line.strip().split("\t")
+                while len(line) < 19:
+                    line.append("")
+                geneobj = {"ensemblid": line[9].strip(),
+                           "HGNC_ID": line[0],
+                           "approved_symbol": line[1],
+                           "approved_name": line[2],
+                           "previous_symbols": line[4].split(","),
+                           "synonyms": line[5].split(","),
+                           "accession_numbers": line[7],
+                           "RefSeq_ID": line[10],
+                           "UniProt_ID": line[14],
+                           "Vega_ID": line[16],
+                           "UCSC_ID": line[17],
+                           "mouse_genome_ID": line[18] }
+                if geneobj["approved_symbol"] not in self.genemap:
+                    continue
+                for k, v in geneobj.iteritems():
+                    if k == "previous_symbols" or k == "synonyms": continue
+                    if v.strip() != "":
+                        ret[v] = geneobj["approved_symbol"]
+        return ret
 
     def resolve(self, s):
-        gene_suggestions, gene_results = self.es.gene_aliases_to_coordinates(s)
-        gene_toks, gene_coords = _unpack_tuple_array(gene_results)
-        if len(gene_coords) > 0:
-            return Coord.parse(gene_coords[-1])
-        return None
+        if s not in self.symbolmap:
+            return None
+        return self.genemap[self.symbolmap[s]]
 
 class DB:
     def __init__(self, DBCONN, assembly):
@@ -75,7 +118,7 @@ def main():
 
     # for mapping genes to coordinates
     coord_map = {}
-    alias = ResolveAlias(es)
+    alias = ResolveAlias(es, args.assembly)
     
     # get all celltypes, loop through pairs
     cts = db.get_celltypes()
@@ -85,12 +128,10 @@ def main():
             count = 0
             fcs = cg.computeFoldChange(cts[i], cts[j])
             for gene, fc in fcs.iteritems():
-                if gene not in coord_map:
-                    coord_map[gene] = alias.resolve(gene)
-                if coord_map[gene] is None:
+                pos = alias.resolve(gene)
+                if pos is None:
                     print("error: cannot map %s to coordinate; skipping" % gene)
                     continue
-                pos = coord_map[gene]
                 db.insert_value(cts[i], cts[j], gene, pos.chrom, pos.start, pos.end, fc)
                 count += 1
             print("inserted %d items" % count)
