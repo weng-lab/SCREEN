8666b9dd7360f6aa29003da99f6656191cb98293
Michael Purcaro <purcaro@gmail.com>
2016-12-19 00:29:18 -0500

    start pulling in mouse
    Former-commit-id: 4cf0b0d319810afbd8293162bac6f2f0c42a1fed

---
+++ 2_import/old/mm10/02_genealiases.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
+from bulk_es_import import executable_importer
+from files_and_paths import Dirs
+
+def main():
+    fnp = os.path.join(Dirs.encyclopedia, "Version-4", "genelist.lsj")
+    importer = executable_importer(fnp, "gene_aliases", "gene")
+    return importer.exe()
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ 2_import/old/mm10/08_setup_log.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os, sys, json, psycopg2, argparse, fileinput
+import cStringIO
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../common/'))
+from dbconnect import db_connect
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils/'))
+from db_utils import getcursor
+
+def setupDb(cur):
+    tableName = "query_logs";
+    print('\t', "dropping and creating", tableName)
+    cur.execute("""
+    DROP TABLE IF EXISTS {tableName};
+    CREATE TABLE {tableName}
+    (id serial PRIMARY KEY,
+    query json,
+    userQuery text,
+    esIndex text,
+    numResults integer,
+    ip text,
+    timeAndDate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    ) """.format(tableName = tableName))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local', action="store_true", default=False)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+
+    DBCONN = db_connect(os.path.realpath(__file__), args.local)
+    with getcursor(DBCONN, "08_setup_log") as curs:
+        setupDb(curs)
+
+if __name__ == '__main__':
+    main()
