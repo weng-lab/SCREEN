3e291707be8dd5ae49e3bcb5951df6c0e19e43b3
Michael Purcaro <purcaro@gmail.com>
2016-12-21 06:54:16 -0500

    update lsj files

--- 1_pipeline/main/10_cellTypes.py
+++ 1_pipeline/main/10_cellTypes.py
@@ -5,14 +5,24 @@ import sys, os, argparse
 import json
 
 def main():
-    output = []
-    with open(os.path.join(os.path.dirname(__file__), "../../celltypes.json"), "r") as f:
-        ct = json.loads(f.read())
-    for k, v in ct.iteritems():
-        output.append({ "cell_type": k, "tissue": v })
-    with open(os.path.join(os.path.dirname(__file__), "../../celltypes.lsj"), "wb") as o:
-        for item in output:
-            o.write(json.dumps(item) + "\n")
+    for assembly in ["hg19", "mm10"]:
+        fn = "cellTypeToTissue." + assembly + ".json"
+        fnp = os.path.join(os.path.dirname(__file__),
+                           "../../", fn)
+        print(fnp)
+        output = []
+        with open(fnp) as f:
+            ct = json.loads(f.read())
+            
+        for k, v in ct.iteritems():
+            output.append({ "cell_type": k, "tissue": v })
+
+        fn = "cellTypeToTissue." + assembly + ".lsj"
+        fnp = os.path.join(os.path.dirname(__file__),
+                           "../../", fn)
+        with open(fnp, "wb") as o:
+            for item in output:
+                o.write(json.dumps(item) + "\n")
     return 0
 
 if __name__ == "__main__":
