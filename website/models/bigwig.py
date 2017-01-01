2b93076464bd044fb7004608d55d7df9e6df1c9f
Henry Pratt <hpratt111@live.com>
2017-01-01 01:02:15 -0500

    play with bigwigs
    Former-commit-id: 298ab1620a428585555a24353c32b1b86d0a7141

---
+++ website/models/bigwig.py
@@ -0,0 +1,37 @@
+import subprocess
+import md5
+import time
+import sys
+import os
+
+class BigWig:
+
+    @staticmethod
+    def getfile():
+        m = md5.new()
+        m.update(str(time.gmtime()))
+        return os.path.join("/tmp", m.hexdigest() + ".bed")
+    
+    @staticmethod
+    def getregions(regions, _file, n_bars):
+        bed = BigWig.getfile()
+        retval = {}
+        with open(bed, "wb") as o:
+            for region in regions:
+                o.write("%s\t%d\t%d\t%s\n" % (region["chr"], region["start"], region["end"], region["acc"]))
+        results = subprocess.check_output(["bwtool", "extract", "bed", bed, _file, "/dev/stdout"]).split("\n")
+        for line in results:
+            print(line)
+            p = line.split("\t")
+            if len(p) >= 6:
+                retval[p[3]] = BigWig._condense_regions(p[5].split(","), n_bars)
+        return retval
+
+    @staticmethod
+    def _condense_regions(regions, n):
+        regions = [float(x) for x in regions]
+        l = len(regions) / n
+        results = []
+        for i in xrange(n):
+            results.append(sum(regions[l * i : l * (i + 1)]) / l)
+        return results
