35e40179823ad7ae58a95ed9e9fd4d75c571f980
Michael Purcaro <purcaro@gmail.com>
2017-01-01 14:22:38 -0500

    read signal files

--- 2_import/00_build_cre/builder/src/build.cpp
+++ 2_import/00_build_cre/builder/src/build.cpp
@@ -27,138 +27,164 @@ ZiARG_int32(j, zi::system::cpu_count, "num threads");
 
 namespace bib {
 
-namespace bfs = boost::filesystem;
+  namespace bfs = boost::filesystem;
 
-class MousePaths {
-public:
+  class MousePaths {
+  public:
     const std::string chr_;
     const bfs::path base_ = "/home/purcarom/0_metadata/encyclopedia/Version-4/ver8/mm10/raw";
     bfs::path path_;
 
     MousePaths(std::string chr)
-        : chr_("chr" + chr)
+      : chr_("chr" + chr)
     {
-        path_ = base_ / chr_;
+      path_ = base_ / chr_;
     }
 
     bfs::path allGenes(){
-        return path_ / (chr_ + "_AllGenes");
+      return path_ / (chr_ + "_AllGenes");
     }
     bfs::path pcGenes(){
-        return path_ / (chr_ + "_PCGenes");
+      return path_ / (chr_ + "_PCGenes");
     }
     bfs::path peaks(){
-        return path_ / (chr_ + "_sorted-peaks");
+      return path_ / (chr_ + "_sorted-peaks");
     }
     bfs::path signalDir(){
-        return path_ / "signal";
+      return path_ / "signal";
     }
-};
+  };
 
-template <typename T>
-class GetData {
+  template <typename T>
+  class GetData {
     T paths_;
 
-public:
+  public:
     GetData(T paths)
-        : paths_(paths)
+      : paths_(paths)
     {}
 
     // chrY    808996  809318  MP-2173311-100.000000   EE0756098
     struct Peak {
-        std::string chrom;
-        std::string start;
-        std::string end;
-        std::string mpID;
-        std::string negLogP;
-        std::string accession;
+      std::string chrom;
+      std::string start;
+      std::string end;
+      std::string mpID;
+      std::string negLogP;
+      std::string accession;
     };
     std::vector<Peak> peaks(){
-        auto lines = bib::files::readStrings(paths_.peaks());
+      auto lines = bib::files::readStrings(paths_.peaks());
 
-        std::vector<Peak> ret;
-        for(const auto& p : lines){
-            auto toks = bib::str::split(p, '\t');
-            auto mpToks = bib::str::split(toks[3], '-');
-            ret.emplace_back(Peak{toks[0], toks[1], toks[2],
-                        mpToks[1], mpToks[2], toks[4]});
-        }
-        std::cout << "loaded " << ret.size() << " peaks\n";
-        return ret;
+      std::vector<Peak> ret;
+      for(const auto& p : lines){
+	auto toks = bib::str::split(p, '\t');
+	auto mpToks = bib::str::split(toks[3], '-');
+	ret.emplace_back(Peak{toks[0], toks[1], toks[2],
+	      mpToks[1], mpToks[2], toks[4]});
+      }
+      std::cout << "loaded " << ret.size() << " peaks\n";
+      return ret;
     }
 
     struct Gene {
-        std::string name;
-        std::string distance;
+      std::string name;
+      std::string distance;
     };
     using AccessionToGenes = std::unordered_map<std::string, std::vector<Gene>>;
 
     // 0    1      2      3                   4    5      6      7                     8 9 10
     // chrY,141692,141850,MP-2173235-3.088310,chrY,206151,207788,ENSMUSG00000101796.1 ,.,+,64302
     AccessionToGenes allGenes(){
-        return loadGenes(paths_.allGenes(), "all");
+      return loadGenes(paths_.allGenes(), "all");
     }
 
     AccessionToGenes pcGenes(){
-        return loadGenes(paths_.pcGenes(), "pc");
+      return loadGenes(paths_.pcGenes(), "pc");
     }
 
     AccessionToGenes loadGenes(bfs::path fnp, std::string typ){
-        auto lines = bib::files::readStrings(fnp);
+      auto lines = bib::files::readStrings(fnp);
 
-        uint32_t count;
-        AccessionToGenes ret;
-        for(const auto& g : lines){
-            auto toks = bib::str::split(g, '\t');
-            auto mpToks = bib::str::split(toks[3], '-');
-            std::string accession = mpToks[1];
-            ret[accession].emplace_back(Gene{toks[7], toks[10]});
-            ++count;
-        }
-        std::cout << "loaded " << count << " " << typ << " genes\n";
-        return ret;
+      uint32_t count{0};
+      AccessionToGenes ret;
+      for(const auto& g : lines){
+	auto toks = bib::str::split(g, '\t');
+	auto mpToks = bib::str::split(toks[3], '-');
+	std::string accession = mpToks[1];
+	ret[accession].emplace_back(Gene{toks[7], toks[10]});
+	++count;
+      }
+      std::cout << "loaded " << count << " " << typ << " genes\n";
+      return ret;
     }
 
-    void loadSignals(){
-        bfs::path dir = paths_.signalDir();
-        for(const auto& f : bib::files::dir(dir)){
-            std::cout << f << std::endl;
-        }
-    }
-};
+    struct SignalLine {
+      std::string name;
+      float zscore;
+      float signal;
+      uint32_t rank;
+    };
 
-template <typename T>
-class Builder {
+    struct SignalFile {
+      bfs::path fnp;
+      std::vector<SignalLine> lines;
+    };
+
+    std::vector<SignalFile> loadSignals(){
+      bfs::path dir = paths_.signalDir();
+      std::vector<SignalFile> ret;
+      
+      for(const auto& fnp : bib::files::dir(dir)){
+	std::cout << fnp << std::endl;
+	auto lines = bib::files::readStrings(fnp);
+	SignalFile sf;
+	sf.fnp = fnp;
+	for(const auto& g : lines){
+	  auto toks = bib::str::split(g, '\t');
+	  sf.lines.emplace_back(SignalLine{toks[0],
+		std::stof(toks[1]),
+		std::stof(toks[2]),
+		std::stoi(toks[3])});
+	}
+	ret.emplace_back(sf);
+      }
+      return ret;
+    }
+  };
+
+  template <typename T>
+  class Builder {
     T paths_;
 
-public:
+  public:
     Builder(MousePaths paths)
-        : paths_(paths)
+      : paths_(paths)
     {}
 
     void build(){
-        GetData<T> d(paths_);
-        const auto peaks = d.peaks();
-        const auto allGenes = d.allGenes();
-        const auto pcGenes = d.pcGenes();
-        d.loadSignals();
+      GetData<T> d(paths_);
+      const auto peaks = d.peaks();
+      const auto allGenes = d.allGenes();
+      const auto pcGenes = d.pcGenes();
+      d.loadSignals();
     }
-};
+  };
 
 } // namespace bib
 
 int main(int argc, char* argv[]){
-    zi::parse_arguments(argc, argv, true);  // modifies argc and argv
-    const auto args = std::vector<std::string>(argv + 1, argv + argc);
+  zi::parse_arguments(argc, argv, true);  // modifies argc and argv
+  const auto args = std::vector<std::string>(argv + 1, argv + argc);
 
-    try {
-        bib::MousePaths paths("Y");
-        bib::Builder<bib::MousePaths> b(paths);
-        b.build();
-    } catch(const std::exception& ex){
-        std::cerr << ex.what() << std::endl;
-        return 1;
-    }
+  try {
+    bib::MousePaths paths("Y");
+    bib::Builder<bib::MousePaths> b(paths);
+    b.build();
+  } catch(const std::exception& ex){
+    std::cerr << ex.what() << std::endl;
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
