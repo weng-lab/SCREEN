36e99e9b4c9e82856d3c87612e5feccfd137f863
Michael Purcaro <purcaro@gmail.com>
2017-01-02 12:00:14 -0500

    refactor assignment of genes
    Former-commit-id: dc41eb0ea768414f3d74705b74b3f8eda102abc4

--- 2_import/00_build_cre/builder/src/build.cpp
+++ 2_import/00_build_cre/builder/src/build.cpp
@@ -17,21 +17,11 @@ namespace bib {
     {}
 
     Peaks build(){
-      GetData<T> gd(paths_);
-      HelperData d;
-      d.assayInfos_ = gd.assayInfos();
-      d.allGenes_ = gd.allGenes();
-      d.pcGenes_ = gd.pcGenes();
-      d.signalFiles_ = gd.loadSignals();
+      HelperData d(paths_);
 
-      Peaks peaks = gd.peaks(); // map of peaks by accession
-
-      std::vector<std::string> accessions;
-      accessions.reserve(peaks.size());
-      for(auto& kv : peaks){
-	accessions.push_back(kv.first);
-      }
-
+      Peaks& peaks = d.peaks();
+      const auto& accessions = d.accessions();
+      
       std::cout << "merging genes and signals into peaks...\n";
 #pragma omp parallel for
       for(size_t i = 0; i < accessions.size(); ++i){
@@ -45,24 +35,12 @@ namespace bib {
       return peaks;
     }
 
-    void processPeak(const HelperData& d,
-		     Peak& p){
-      const auto& i = p.mpName;
+    void processPeak(const HelperData& d, Peak& p){
+      const auto& mpName = p.mpName;
       p.genome = paths_.genome_;
-      
-      if(bib::in(i, d.allGenes_)){
-	//std::cout << i << " all\n";
-	p.gene_nearest_all = d.allGenes_.at(i);
-	std::sort(p.gene_nearest_all.begin(),
-		  p.gene_nearest_all.end());
-      }
 
-      if(bib::in(i, d.pcGenes_)){
-	//std::cout << i << " pc\n";
-	p.gene_nearest_pc = d.pcGenes_.at(i);
-	std::sort(p.gene_nearest_pc.begin(),
-		  p.gene_nearest_pc.end());
-      }
+      d.setAllGenes(mpName, p.gene_nearest_all);
+      d.setPcGenes(mpName, p.gene_nearest_pc);
 
     }
   };
