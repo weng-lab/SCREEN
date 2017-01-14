9d88d1f144d98f5dae9a99777244531b813415a7
Michael Purcaro <purcaro@gmail.com>
2017-01-14 01:29:13 -0500

    first try at tfs

--- website/ui/src/apps/search/reducers/facetboxen_reducers.js
+++ website/ui/src/apps/search/reducers/facetboxen_reducers.js
@@ -8,7 +8,19 @@ const facetboxen_reducers = (state, action) => {
     case Actions.SET_CHROM: return {...state, coord_chrom: action.chrom };
     case Actions.SET_COORDS: return {...state, coord_start: action.start,
                                      coord_end: action.end };
+    case Actions.TOGGLE_TF: {
+        let tfs = new Set(state.tfs_selection);
+        let tf = action.tf;
+        if(tfs.has(tf)){
+            tfs.delete(tf);
+        } else {
+            tfs.add(tf);
+        }
+        return { ...state, tfs_selection: tfs}
+    }
+    case Actions.SET_TFS_MODE: return { ...state, tfs_mode: action.mode};
     case Actions.SET_ACCESSIONS: return {...state, accessions: action.accs};
+
     case Actions.SET_GENE_ALL_DISTANCE:
         return {...state, gene_all_start: action.start, gene_all_end: action.end };
     case Actions.SET_GENE_PC_DISTANCE:
