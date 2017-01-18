392540501a8ccd83d3279d919456a63e180531c3
Michael Purcaro <purcaro@gmail.com>
2017-01-18 18:28:09 -0500

    strip down correlations and trees (hg19 broken))

--- website/models/trees.py
+++ website/models/trees.py
@@ -1,3 +1,8 @@
+from correlation import Correlation
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../heatmaps/API"))
+from heatmaps.heatmap import Heatmap
+
 class Trees:
     def __init__(self, cache, pg, assembly, tree_rank_method):
         self.cache = cache
@@ -5,46 +10,59 @@ class Trees:
         self.assembly = assembly
         self.tree_rank_method = tree_rank_method
 
+        if "dnase" == tree_rank_method:
+            self.inner = None
+            self.outer = "dnase"
+        else:
+            toks = tree_rank_method.split('$')
+            self.inner = toks[1].lower()
+            self.outer = toks[0]
+
     def getTree(self):
-        results = {}
+        ret = {}
         biosampleTypes = self.cache.biosampleTypes
         for typ in biosampleTypes:
-            def ctFilter(ct):
-                if not ct in self.cache.biosamples:
-                    print("missing", ct)
-                    return False
-                return typ == self.cache.biosamples[ct].biosample_type
+            ret[typ] = self._processTyp(typ)
+        title = ' / '.join([x for x in [self.outer, self.inner)] if x])
+        return {"tree": ret, "title" : title }
 
-            c = Correlation(_ret["hits"]["hits"], self.ps.DBCONN)
-            k = "dnase" if j["inner"] is None else j["inner"].lower()
+    def _processTyp(self, typ):
+        def ctFilter(ct):
+            if not ct in self.cache.biosamples:
+                print("missing", ct)
+                return False
+            return typ == self.cache.biosamples[ct].biosample_type
 
-            with Timer(typ + ": spearman correlation time"):
-                if self.assembly == "hg19":
-                    labels, corr = c.spearmanr(j.get("outer", "dnase"),
-                                               j.get("inner", None),
-                                               ctFilter )
-                else:
-                    labels = self.cache.celltypemap[k]
-                    labels, corr = c.dbcorr(self.assembly, k, labels, lambda x: "bryo" in x)
-                    print("!got correlation")
+        c = Correlation(_ret["hits"]["hits"], self.ps.DBCONN)
 
-            if not labels:
-                continue
-            rho = corr[0] if len(corr) == 2 else corr
+        with Timer(typ + ": spearman correlation time"):
+            if self.assembly == "hg19":
+                raise Exception("fix me")
+            else:
+                labels = self.cache.celltypemap[self.inner]
+                labels, corr = c.dbcorr(self.assembly, self.inner, labels,
+                                        lambda x: "bryo" in x)
+                print("!got correlation")
 
-            try:
-                rhoList = rho.tolist() if type(rho) is not list else rho
-                _heatmap = Heatmap(rhoList)
-            except:
-                print("rho", rho)
-                print("pval", pval)
-                continue
-            with Timer(typ + ": hierarchical clustering time"):
-                roworder, rowtree = _heatmap.cluster_by_rows()
-            results[typ] = {"tree": rowtree, "labels": labels}
+        if not labels:
+            return
 
-        title = ' / '.join([x for x in
-                             [j.get("outer", "dnase"), j.get("inner", None)]
-                             if x])
-        return {"results": {"tree": results,
-                            "tree_title" : title }}
+        if 2 == len(corr):
+            rho = corr[0]
+        else:
+            rho = corr
+
+        try:
+            if type(rho) is not list:
+                rhoList = rho.tolist()
+            else:
+                rhoList = rho
+            _heatmap = Heatmap(rhoList)
+        except:
+            print("rho", rho)
+            print("pval", pval)
+            continue
+        with Timer(typ + ": hierarchical clustering time"):
+            roworder, rowtree = _heatmap.cluster_by_rows()
+        return {"tree": rowtree, "labels": labels}
+
