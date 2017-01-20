7e02e574c0ca2b3ca7e97d2290ac13328dbbc466
Michael Purcaro <purcaro@gmail.com>
2017-01-19 23:28:24 -0500

    starting to do ajax

--- website/ui/src/apps/geneexp/config/maintabs.js
+++ website/ui/src/apps/geneexp/config/maintabs.js
@@ -1,20 +1,9 @@
 import React from 'react'
 
-import ExpressionBoxplot from '../components/expression_boxplot'
 import CandidateREs from '../components/candidate_res'
-
+import GeneExp from '../components/gene_exp'
 
 //		<CandidateREs />
-
-class GeneExp extends React.Component{
-    render() {
-        console.log("GeneExp", "in render", this.props);
-        return (<div>
-                <ExpressionBoxplot />
-                </div>);
-    }
-}
-
 class CanRes extends React.Component{
     render() {
         return (<div>
