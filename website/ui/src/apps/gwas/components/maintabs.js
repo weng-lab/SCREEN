a54ce4ad4b21f66499b505737d79bbad3f0d9968
Michael Purcaro <purcaro@gmail.com>
2017-01-19 23:50:20 -0500

    redo gwas
    Former-commit-id: 8a4747ec1638b7e7a4ea2328f24edcba832ac620

--- website/ui/src/apps/gwas/components/maintabs.js
+++ website/ui/src/apps/gwas/components/maintabs.js
@@ -4,55 +4,10 @@ import {bindActionCreators} from 'redux';
 
 import * as Actions from '../actions/main_actions';
 
-class MainTabs extends React.Component {
-    render(){
-        const makeTabTitle = (key, tab) => {
-            if(!tab.visible){ return (<div />) }
-            let active = key == this.props.maintabs_active;
-	    return (<li className={active ? "active" : ""}
-                    key={"tab_" + key}
-	            style={{display: (tab.visible ? "list-item" : "none") }}
-	            onClick={ () => { this.props.actions.setMainTab(key) } }>
-                    <a data-toggle="tab">{tab.title}</a>
-                    </li>);
-        }
+import MainTabs from '../../../common/components/maintabs.js'
 
-        const makeTab = (key, tab) => {
-            if(!tab.visible){ return (<div />) }
-            let active = key == this.props.maintabs_active;
-            return (<div
-                    className={active ? "tab-pane active" : "tab-pane"}
-                    key={"tab_" + key}
-                    id={"tab_main_" + key}
-                    key={"tcontent_" + key}>
-		    {React.createElement(tab.f, this.props)}
-		    </div>);
-        }
-
-        let tabs = this.props.maintabs;
-        return (<div id="exTab1" className="container">
-
-	        <ul className="nav nav-tabs">
-	        {Object.keys(tabs).map((key) => ( makeTabTitle(key, tabs[key]) ))}
-                </ul>
-
-	        <div className="tab-content clearfix">
-	        {Object.keys(tabs).map((key) => ( makeTab(key, tabs[key]) ))}
-	        </div>
-
-                </div>);
-    }
-}
-
-const mapStateToProps = (state) => ({
-        ...state
-});
-
+const mapStateToProps = (state) => ({ ...state });
 const mapDispatchToProps = (dispatch) => ({
     actions: bindActionCreators(Actions, dispatch)
 });
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(MainTabs);
+export default connect(mapStateToProps, mapDispatchToProps)(MainTabs);
