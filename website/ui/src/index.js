9f41e1add8176911eab7932b4a5e0b938483b133
Michael Purcaro <purcaro@gmail.com>
2017-01-19 21:25:51 -0500

    disable for now

--- website/ui/src/index.js
+++ website/ui/src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory} from 'react-router';
 
 import SearchPage from './apps/search/main';
-import CartPage from './apps/cart/main';
+// import CartPage from './apps/cart/main';
 import GeneExpPage from './apps/geneexp/main';
-import ComparisonPage from './apps/comparison/main';
+// import ComparisonPage from './apps/comparison/main';
 
-ReactDOM.render((<Router history={browserHistory}>
-		    <Route path="/search(.*)" component={SearchPage} />
-		    <Route path="/cart" component={CartPage} />
-		    <Route path="/geneexp/:assembly/:geneID" component={GeneExpPage} />
-		    <Route path="/comparison" component={ComparisonPage} />
-		 </Router>), document.getElementById('root'));
+//<Route path="/cart" component={CartPage} />
+//<Route path="/comparison" component={ComparisonPage} />
+
+ReactDOM.render(
+    (<Router history={browserHistory}>
+     <Route path="/search(.*)" component={SearchPage} />
+     <Route path="/geneexp/:assembly/:geneID" component={GeneExpPage} />
+     </Router>),
+    document.getElementById('root'));
