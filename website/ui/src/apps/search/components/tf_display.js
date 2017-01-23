91bb1634f0c070e32f87ffe3e78a229b63081e84
Michael Purcaro <purcaro@gmail.com>
2017-01-23 15:00:39 -0500

    do lookup
    Former-commit-id: 7bf637bd4140a688534650b6499286be2ec979bb

--- website/ui/src/apps/search/components/tf_display.js
+++ website/ui/src/apps/search/components/tf_display.js
@@ -37,11 +37,11 @@ class TFDisplay extends React.Component {
         this.loadTFs(nextProps);
     }
 
-    loadTFs({tree_nodes_compare}) {
+    loadTFs({tree_nodes_compare, tree_rank_method}) {
         if(null == tree_nodes_compare){
             return;
         }
-        var q = {GlobalAssembly, tree_nodes_compare};
+        var q = {GlobalAssembly, tree_nodes_compare, tree_rank_method};
         var jq = JSON.stringify(q);
         if(this.state.jq == jq){
             // http://www.mattzeunert.com/2016/01/28/javascript-deep-equal.html
