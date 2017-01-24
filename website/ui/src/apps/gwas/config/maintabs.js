9d22ce8de1b03346c43847da5b2d81e1f5ac7b93
Michael Purcaro <purcaro@gmail.com>
2017-01-24 14:36:20 -0500

    gwas tweaks
    Former-commit-id: 74d54459db02fce2bd73d004281dddbad18dcece

--- website/ui/src/apps/gwas/config/maintabs.js
+++ website/ui/src/apps/gwas/config/maintabs.js
@@ -1,23 +1,18 @@
 import React from 'react'
 
-import CandidateREs from '../components/candidate_res'
-import GeneExp from '../components/gene_exp'
+import GwasExp from '../components/gwas_exp'
 
-//		<CandidateREs />
-class CanRes extends React.Component{
+class Gwas extends React.Component{
     render() {
         return (<div>
-
+		{"hi!"}
                 </div>);
     }
 }
 
 
 const MainTabInfo = {
-    gene_expression: {title: "Gene Expression", visible: true,
-                      f: GeneExp},
-    candidate_res: { title: "Candidate Regulatory Elements", visible: true,
-                     f: CanRes }
+    gwas: {title: "GWAS", visible: true, f: Gwas},
 };
 
 export default MainTabInfo;
