194886c576f53960fc5e4fb73a26ef636a6915e3
Michael Purcaro <purcaro@gmail.com>
2017-01-24 02:39:11 -0500

    tf skeleton

---
+++ website/ui/src/apps/tf/reducers/main_reducers.js
@@ -0,0 +1,30 @@
+import * as Actions from '../actions/main_actions';
+import * as SearchAction from '../../../common/actions/searchbox_actions.js'
+import {doToggle} from '../../../common/uility'
+
+const main_reducers = (state, action) => {
+    switch (action.type) {
+
+    case Actions.SET_MAIN_TAB:
+        var ret = {...state, maintabs_active: action.name}
+        ret.maintabs = {...state.maintabs};
+        ret.maintabs[action.name].visible = true;
+        return ret;
+
+    case Actions.SET_CT1: {
+        return { ...state, ct1: action.ct}
+    }
+    case Actions.SET_CT2: {
+        return { ...state, ct2: action.ct}
+    }
+
+    case SearchAction.MAKE_SEARCH_QUERY:
+        console.log("new query", action.q);
+        return state;
+
+    default:
+      return state;
+  }
+};
+
+export default main_reducers;
