027ca07bec3de3b4f2fa06d2f3e6aa14e5b561e1
Michael Purcaro <purcaro@gmail.com>
2017-01-24 01:25:48 -0500

    drawing bars
    Former-commit-id: 55dcc4ad8ce90b62ddce16ea8ff4356309722fed

--- website/ui/src/apps/de/components/expression_boxplot.js
+++ website/ui/src/apps/de/components/expression_boxplot.js
@@ -37,41 +37,33 @@ class ExpressionBoxplot extends React.Component {
     componentDidUpdate() {
         // from http://bl.ocks.org/mbostock/3887118
 
-	$(this.refs.chart).empty();
-
+        let chart = this.refs.chart;
+	$(chart).empty();
         let data = this.props.data.diffCREs.data;
 
         var margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
-
         var x_domain = d3.extent(data, function(d) { return d[0]; });
         var y_domain = d3.extent(data, function(d) { return d[1]; });
-
         var x = d3.scale.linear()
             .domain(x_domain).nice()
             .range([0, width]);
-
         var y = d3.scale.linear()
             .domain(y_domain).nice()
             .range([height, 0]);
-
         var color = d3.scale.category10();
-
         var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom");
-
         var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left");
-
-        var svg = d3.select(this.refs.chart).append("svg")
+        var svg = d3.select(chart).append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+            .attr("transform", "translate(" + margin.left + "," + margin.top+ ")");
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -82,7 +74,6 @@ class ExpressionBoxplot extends React.Component {
             .attr("y", -6)
             .style("text-anchor", "end")
             .text("coord");
-
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis)
@@ -93,7 +84,6 @@ class ExpressionBoxplot extends React.Component {
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text("log2 fold change")
-
         svg.selectAll(".dot")
             .data(data)
             .enter().append("circle")
@@ -102,26 +92,50 @@ class ExpressionBoxplot extends React.Component {
             .attr("cx", function(d) { return x(d[0]); })
             .attr("cy", function(d) { return y(d[1]); })
             .style("fill", function(d) { return color(d[2]); });
-
         var legend = svg.selectAll(".legend")
             .data(color.domain())
             .enter().append("g")
             .attr("class", "legend")
             .attr("transform", function(d, i) {
                 return "translate(0," + i * 20 + ")"; });
-
         legend.append("rect")
             .attr("x", width - 18)
             .attr("width", 18)
             .attr("height", 18)
             .style("fill", color);
-
         legend.append("text")
             .attr("x", width - 24)
             .attr("y", 9)
             .attr("dy", ".35em")
             .style("text-anchor", "end")
             .text(function(d) { return d; });
+
+        var bars = this.props.data.nearbyDEs.data;
+        x_domain = d3.extent(bars, function(d) { return d[0]; });
+        y_domain = d3.extent(bars, function(d) { return d[1]; });
+        x = d3.scale.linear()
+            .domain(x_domain).nice()
+            .range([0, width]);
+        y = d3.scale.linear()
+            .domain(y_domain).nice()
+            .range([height, 0]);
+        var bar = svg.selectAll(".bar")
+            .data(bars)
+            .enter().append("g");
+        bar.append("rect")
+            .attr("class", "bar1")
+            .attr("x", function(d) {
+                return x(d[0]);
+            })
+            .attr("width", function(d){
+                return x(d[3]) - x(d[2]);})
+            .attr("y", function(d) {
+                return y(d[1]);
+            })
+            .attr("height", function(d) {
+                return height - y(d[1]);
+            });
+
     }
 }
 
