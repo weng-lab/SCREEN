1b424440f33a9fc028397087f230ae19b9a45dd0
Michael Purcaro <purcaro@gmail.com>
2017-01-24 20:54:24 -0500

    show all cell types in tree

--- website/models/correlation.py
+++ website/models/correlation.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../metadata/utils"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../common"))
@@ -15,29 +16,29 @@ class Correlation:
         self.cache = cache
         self.tableName = assembly + "_correlations"
 
-    def dbcorr(self, assembly, assay, labels = None, _filter = lambda x: True):
+    def dbcorr(self, assembly, tableName, cellTypes):
         with getcursor(self.DBCONN, "Correlation::dbcorr") as curs:
             curs.execute("""
 SELECT correlations, assay FROM {tn} WHERE assay = %s
-""".format(tn = self.tableName), (assay,))
+""".format(tn = self.tableName), (tableName,))
             try:
                 r = curs.fetchone()[0]
             except:
                 raise Exception("correlation$Correlation::dbcorr ERROR: failed to get correlation data for assay %s" % assay)
-        if not labels:
-            return r
-
+        if not r:
+            return None, None
+                
         tokeep = []
         flabels = []
-        for i in xrange(len(labels)):
-            if _filter(labels[i]):
-                tokeep.append(i)
-                obj = self.cache.datasets.globalCellTypeInfo[labels[i]]
-                obj["key"] = labels[i]
-                flabels.append(obj)
-        ret = [[0 for j in xrange(len(tokeep))] for i in xrange(len(tokeep))]
-        for i in xrange(len(tokeep)):
-            for j in xrange(len(tokeep)):
+        for i, ct in enumerate(cellTypes):
+            tokeep.append(i)
+            obj = self.cache.datasets.globalCellTypeInfo[ct]
+            obj["key"] = ct
+            flabels.append(obj)
+        dim = len(tokeep)
+        ret = np.empty((dim, dim))
+        for i in xrange(dim):
+            for j in xrange(dim):
                 ret[i][j] = r[tokeep[i]][tokeep[j]]
         #print(flabels)
         return (flabels, ret)
