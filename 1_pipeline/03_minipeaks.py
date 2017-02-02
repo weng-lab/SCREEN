35d73c7059bfb831924752af415dd9c0b3ba60da
Michael Purcaro <purcaro@gmail.com>
2017-02-02 15:54:57 +0800

    working on pre-computing minipeaks

--- 1_pipeline/03_minipeaks.py
+++ 1_pipeline/03_minipeaks.py
@@ -13,14 +13,53 @@ class ExtractRawPeaks:
         self.bwtool = "/data/cherrypy/bin/bwtool"
         if not os.path.exists(self.bwtool):
             self.bwtool = "/usr/local/bin/bwtool"
-
+        self.masterPeakFnp = os.path.join(self.d, "raw",
+                                          "masterPeaks.bed.gz")
+        self.miniPeaksBedFnp = os.path.join(self.d, "raw",
+                                            "miniPeakSites.bed.gz")
+        self.bwtoolFilter = os.path.join(os.path.dirname(__file__),
+                                         'minipeaks/bin/read_json')
+        if not os.path.exists(self.bwtoolFilter):
+            raise Exception("missing C++ bwtool filter; please compile?")
+        
     def run(self):
-        self.writeBed()
-        self.
+        #self.writeBed()
+        self.extractAndDownsamplePeaks()
+
+    def _runBwtool(self, outD, fnp):
+        outFnp = os.path.join(outD, os.path.basename(fnp) + ".txt")
+        cmds = [self.bwtool, "extract", "bed",
+                self.miniPeaksBedFnp,
+                fnp, "/dev/stdout",
+                '|', self.bwtoolFilter,
+                '>', outFnp]
+        print("would run", " ".join(cmds))
+        #Utils.runCmds(cmds)        
+        
+    def extractAndDownsamplePeaks(self):
+        fns = ["DNase-List.txt", "H3K27ac-List.txt",
+               "H3K4me3-List.txt"]
+        bfnps = []
+
+        for fn in fns:
+            fnp = os.path.join(self.d, "raw", fn)
+            with open(fnp) as f:
+                rows = [x.rstrip().split() for x in f.readlines()]
+            for r in rows:
+                fnp = os.path.join(d, r[0], r[1] + ".bigWig")
+            if os.path.exists(fnp):
+                bfnps.append(fnp)
+            else:
+                print("WARNING: missing bigwig", fnp)
+            outD = os.path.join(self.d, "minipeaks")
+            Utils.mkdir_p(outD)
+
+            for fnp in bfnps:
+                self._runBwtool(outD, fnp)
         
     def writeBed(self):
-        inFnp = os.path.join(self.d, "raw", "masterPeaks.bed.gz")
-        outFnp = os.path.join(self.d, "raw", "miniPeakSites.bed.gz")
+        inFnp = self.masterPeakFnp
+        outFnp = self.minPeaksBedFnp
 
         with gzip.open(inFnp) as inF:
             with gzip.open(outFnp, "wb") as outF:
@@ -37,78 +76,6 @@ class ExtractRawPeaks:
                                            int(stop + padding),
                                            accession]]) + '\n')
         print("wrote", outFnp)
-                
-    def _runBw(self, bed, fnp):
-        return subprocess.check_output(
-            [self.bwtool, "extract", "bed",
-             bed, fnp, "/dev/stdout"]).split("\n")
-
-    def _doGetAvgSignals(self, accessionsToGet, bigWigs, cache, n_bars):
-        d = "/project/umw_zhiping_weng/0_metadata/encode/data"
-        bfnps = []
-        for bw in bigWigs:
-            fnp = os.path.join(d, bw["accession"], bw["bigwig"] + ".bigWig")
-            if os.path.exists(fnp):
-                bfnps.append({"fnp": fnp, "ct": bw["ct"]})
-            else:
-                print("WARNING: missing bigwig", fnp)
-
-        bedFnp = self.getfile()
-        with open(bedFnp, "w") as o:
-            for c in accessionsToGet:
-                padding = (1000 - (c["end"] - c["start"])) / 2
-                accession = c["accession"]
-                o.write("%s\t%d\t%d\t%s\n" %
-                        (c["chrom"],
-                         max(0, c["start"] - padding),
-                         c["end"] + padding,
-                         accession))
-        for bw in bfnps:
-            signals = self._runBw(bedFnp, bw["fnp"])
-            ct = bw["ct"]
-            for line in signals:
-                p = line.split("\t")
-                if len(p) >= 6:
-                    accession = p[3]
-                    avgSignal = self._condense_regions(p[5].split(","), n_bars)
-                    cache[accession][ct] = avgSignal
-        os.remove(bedFnp)
-        return cache
-
-    def getregions(self, cres, bigWigs, n_bars):
-        cache = {k["accession"]: {} for k in cres}
-#        cache = self.minipeaks_cache.getVec([c["accession"] for c in cres])
-        _accessionsToGet = {c["accession"]: c for c in cres}
-
-        for acc, ctAndAvgSignals in cache.iteritems():
-            if ctAndAvgSignals:
-                _accessionsToGet.pop(acc, None)
-        accessionsToGet = [c for k, c in _accessionsToGet.iteritems()]
-
-        if accessionsToGet:
-            cache = self._doGetAvgSignals(accessionsToGet, bigWigs, cache, n_bars)
-#            self.minipeaks_cache.insertVec(cache)
-
-        ret = {}
-        for bw in bigWigs:
-            ct = bw["ct"]
-            ret[ct] = {"tissue": bw["tissue"]}
-            for c in cres:
-                accession = c["accession"]
-                if accession not in cache or ct not in cache[accession]:
-                    #print("bigwig$BigWig::getregions WARNING: missing ct %s for accession %s" % (ct, accession))
-                    ret[ct][accession] = []
-                else:
-                    ret[ct][accession] = cache[accession][ct]
-        return ret
-
-    def _condense_regions(self, regions, n):
-        regions = [float(x) if x != "NA" else 0.0 for x in regions]
-        l = len(regions) / n
-        results = []
-        for i in xrange(n):
-            results.append(round(sum(regions[l * i : l * (i + 1)]) / l, 2))
-        return results
 
 def parse_args():
     parser = argparse.ArgumentParser()
