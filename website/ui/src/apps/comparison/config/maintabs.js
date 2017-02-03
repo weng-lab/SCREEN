bb7b7287c7bbf1416cf43e81cbaa7eff56e76362
Michael Purcaro <purcaro@gmail.com>
2017-02-03 10:33:22 +0800

    start redoing Comparison app

---
+++ website/ui/src/apps/comparison/config/maintabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/*
+class ResultsTab extends React.Component{
+    render() { return (<ResultsTableContainer />); }
+}
+*/
+
+const MainTabInfo = {
+    //results : {title: "Search Results", visible: true, f: ResultsTab},
+
+};
+
+export default MainTabInfo;
