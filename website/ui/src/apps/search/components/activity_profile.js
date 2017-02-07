22b1ba1a620632db412ad441850ad0aae932c0fb
Michael Purcaro <purcaro@gmail.com>
2017-02-06 20:27:24 -0500

    working on bed and json download: --extract util function to get store state --rename uility.js to utility.js --start moving download methods to dataws controller

--- website/ui/src/apps/search/components/activity_profile.js
+++ website/ui/src/apps/search/components/activity_profile.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import * as Actions from '../actions/main_actions';
 
-import MiniPeaks from './minipeaks'
+import MiniPeaks from './minipeaks';
 import loading from '../../../common/components/loading';
+import {getCommonState} from '../../../common/utility';
 
 class ActivityProfileContainer extends React.Component {
     constructor(props) {
@@ -25,21 +26,14 @@ class ActivityProfileContainer extends React.Component {
             this.loadCREs(nextProps);
     }
 
-    loadCREs({accessions, coord_chrom, coord_start, coord_end,
-              gene_all_start, gene_all_end, gene_pc_start, gene_pc_end,
-              rank_dnase_start, rank_dnase_end,
-              rank_promoter_start, rank_promoter_end,
-              rank_enhancer_start, rank_enhancer_end,
-              rank_ctcf_start, rank_ctcf_end,
-              cellType, assay}){
-	if (!assay) assay = "dnase";
-        var q = {GlobalAssembly,
-                 accessions, coord_chrom, coord_start, coord_end,
-                 gene_all_start, gene_all_end, gene_pc_start, gene_pc_end,
-                 rank_dnase_start, rank_dnase_end,
-                 rank_promoter_start, rank_promoter_end,
-                 rank_enhancer_start, rank_enhancer_end,
-                 rank_ctcf_start, rank_ctcf_end, cellType, withpeaks: assay};
+    loadCREs(props){
+	let assay = "dnase";
+	if (assay in props) {
+	    assay = props.assay;
+	}
+        var q = getCommonState(props);
+	q.withpeaks = assay;
+	
         var jq = JSON.stringify(q);
         if(this.state.jq == jq){
             // http://www.mattzeunert.com/2016/01/28/javascript-deep-equal.html
@@ -59,7 +53,8 @@ class ActivityProfileContainer extends React.Component {
                                jq, isFetching: false, isError: true});
             }.bind(this),
             success: function(r) {
-                this.setState({cres: r["cres"], total: r["total"], peakdata: r["peakdata"],
+		// TODO: cache returned results across different selections...
+		this.setState({cres: r["cres"], total: r["total"], peakdata: r["peakdata"],
                                jq, isFetching: false, isError: false});
             }.bind(this)
         });
