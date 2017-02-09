7cf972676c5f84397f7271519899b85b22f60c46
Michael Purcaro <purcaro@gmail.com>
2017-02-09 03:29:25 -0500

    remve more console.log
    Former-commit-id: 2e9a94e4b32878c1bdf1bdb2580f4d684bcb7fbf

--- website/ui/src/apps/comparison/main.js
+++ website/ui/src/apps/comparison/main.js
@@ -24,7 +24,6 @@ class ComparisonPage extends React.Component {
 				      loggerMiddleware
 				  ));
 
-	console.log(store.getState());
 	// 	    <MainTabs />
 	//                     <FacetBoxen />
 	return (
