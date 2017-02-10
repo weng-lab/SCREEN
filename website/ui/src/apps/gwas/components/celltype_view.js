291eea5d53d296790ba2c00e391dd45d5ad5657e
Michael Purcaro <purcaro@gmail.com>
2017-02-10 06:16:25 -0500

    wire in view

--- website/ui/src/apps/gwas/components/celltype_view.js
+++ website/ui/src/apps/gwas/components/celltype_view.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import * as Actions from '../actions/main_actions';
 
 class CelltypeView extends React.Component {
-    componentDidMount() {
-    }
-
     render() {
 	return (<div>
+                <h3>{this.props.cellType.view}</h3>
 		</div>);
     }
 
 }
 
-export default CelltypeView;
+const mapStateToProps = (state) => ({ ...state });
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(Actions, dispatch)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CelltypeView);
