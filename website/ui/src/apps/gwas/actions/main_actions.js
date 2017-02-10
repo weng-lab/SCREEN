02e15a2be3369f5de8b40c45accc680d108fc637
Michael Purcaro <purcaro@gmail.com>
2017-02-10 05:57:42 -0500

    clikc on table

--- website/ui/src/apps/gwas/actions/main_actions.js
+++ website/ui/src/apps/gwas/actions/main_actions.js
@@ -3,3 +3,6 @@ export const setStudy = (s) => ({ type: SET_STUDY, s });
 
 export const SET_ACCESSIONS = 'SET_ACCESSIONS'
 export const setAccessions = (accs) => ({ type: SET_ACCESSIONS, accs });
+
+export const SET_CELLTYPE = 'SET_CELLTYPE'
+export const setCellType = (ct) => ({ type: SET_CELLTYPE, ct });
