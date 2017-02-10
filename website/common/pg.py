1654e2b750e8e67cfdabe16d745b905534aca9e8
Henry <hpratt111@live.com>
2017-02-10 13:59:23 +0000

    redo as list

--- website/common/pg.py
+++ website/common/pg.py
@@ -706,7 +706,8 @@ GROUP BY {groupBy}
         for a in accs:
             if len(filter(lambda x: x >= 1.64, a[3:])) > 0:
                 totalActive += 1
-                a[1] = ", ".join(a[1])
+                a = list(a)
+                a[1] = ", ".join(sorted(a[1]))
                 activeAccs.append(a)
 
         percActive = 0
