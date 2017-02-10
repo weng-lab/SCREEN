48a32c028d55983f0eee1848b379ca43f10509d0
Michael Purcaro <purcaro@gmail.com>
2017-02-10 06:04:39 -0500

    setcelltype
    Former-commit-id: 018987c4aa3c69f596c8b499d392026d373f213b

---
+++ website/ui/src/apps/gwas/components/celltype_view.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class CelltypeView extends React.Component {
+    componentDidMount() {
+    }
+
+    render() {
+	return (<div>
+		</div>);
+    }
+
+}
+
+export default CelltypeView;
