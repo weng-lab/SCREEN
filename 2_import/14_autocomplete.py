57bc27b2ba3885ff280d9b3bf13cfd0c6cce658e
Michael Purcaro <purcaro@gmail.com>
2017-02-11 18:52:07 -0500

    more tweaks and fixes for genes

--- 2_import/14_autocomplete.py
+++ 2_import/14_autocomplete.py
@@ -5,14 +5,12 @@ import os, sys, json, psycopg2, argparse, StringIO, gzip
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../common/'))
 from dbconnect import db_connect
-from constants import chroms
+from constants import chroms, paths
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils/'))
 from utils import Utils, printt
 from db_utils import getcursor
 from files_and_paths import Dirs
-
-chromlimit = {"hg19": 22, "mm10": 19}
 
 class SetupAutocomplete:
     def __init__(self, curs, assembly, save):
@@ -23,7 +21,7 @@ class SetupAutocomplete:
     def run(self):
         printt("loading snps...")
         names = []
-        for chrom in ["chr%d" % d for d in range(1, chromlimit[self.assembly] + 1)] + ["chrX", "chrY"]:
+        for chrom in chroms[self.assembly]:
             self.curs.execute("""
 SELECT name, start, stop FROM {assembly}_snps_{chrom}
 """.format(chrom=chrom, assembly=self.assembly))
@@ -33,19 +31,19 @@ SELECT name, start, stop FROM {assembly}_snps_{chrom}
 
         printt("loading gene info...")
         self.curs.execute("""
-SELECT approved_symbol, ensemblid, info, chrom, start, stop 
+SELECT approved_symbol, ensemblid, info, chrom, start, stop
 FROM {assembly}_gene_info
 """.format(assembly=self.assembly))
         r = self.curs.fetchall()
-        keys = ["UniProt_ID", "RefSeq_ID", "Vega_ID", "UCSC_ID", "HGNC_ID", "approved_name"]
+
         for row in r:
             c = "%s\t%d\t%d" % (row[3], row[4], row[5])
             names.append(row[0] + "\t" + c)
             names.append(row[1] + "\t" + c)
             if row[2]:
-                for key in keys:
-                    if key in row[2]:
-                        names.append(row[2][key] + "\t" + c)
+                for k, v in row[2].iteritems():
+                    names.append(v + "\t" + c)
+                if 0: # no longer present in newer HGNC data?
                     if "synonyms" in row[2]:
                         names += [x.strip() + "\t" + c for x in row[2]["synonyms"]]
                     if "previous_symbols" in row[2]:
@@ -56,42 +54,42 @@ FROM {assembly}_gene_info
         outF.seek(0)
 
         if self.save:
-            d = os.path.join("/project/umw_zhiping_weng/0_metadata/encyclopedia/",
-                             "Version-4", "ver9", self.assembly, "raw")
-            fnp = os.path.join(d, "autocomplete_dictionary.txt")
+            fnp = paths.path(self.assembly, "extras",
+                             "autocomplete_dictionary.txt")
             with open(fnp, "wb") as o:
                 o.write("\n".join(names))
             printt("wrote", fnp)
-            
+
         printt("found", len(names), "items")
 
         printt("inserting into table...")
+        tableName = self.assembly + "_autocomplete"
         self.curs.execute("""
-DROP TABLE IF EXISTS {assembly}_autocomplete;
-CREATE TABLE {assembly}_autocomplete 
-(id serial PRIMARY KEY, 
-name TEXT, 
-chrom TEXT, 
-start INT, 
+DROP TABLE IF EXISTS {tn};
+CREATE TABLE {tn}
+(id serial PRIMARY KEY,
+name TEXT,
+chrom TEXT,
+start INT,
 stop INT)
-""".format(assembly=self.assembly))
+""".format(tn = tableName))
 
-        self.curs.copy_from(outF, "%s_autocomplete" % self.assembly,
-                            "\t", columns=["name", "chrom", "start", "stop"])
+        self.curs.copy_from(outF, tableName, "\t",
+                            columns=["name", "chrom", "start", "stop"])
 
         printt("indexing table...")
         self.curs.execute("""
-CREATE INDEX {assembly}_autocomplete_index ON {assembly}_autocomplete 
+CREATE INDEX {assembly}_autocomplete_index ON {tn}
 USING btree (name text_pattern_ops)
-""".format(assembly=self.assembly))
+""".format(tn = tableName))
 
         # will need to run as psql postgres user:
         # \c regElmViz; CREATE EXTENSION pg_trgm;
         self.curs.execute("""
-CREATE INDEX {assembly}_fulltext_index ON {assembly}_autocomplete 
+CREATE INDEX {assembly}_fulltext_index ON {tn}
 USING gin (name gin_trgm_ops)
-""".format(assembly=self.assembly))
-                
+""".format(tn = tableName))
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--local', action="store_true", default=False)
