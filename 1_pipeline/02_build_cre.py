6bf3feae614c78458b0bbc1c4c82d0799273f9ff
Michael Purcaro <purcaro@gmail.com>
2017-02-12 14:11:55 -0500

    get run script to work

--- 1_pipeline/02_build_cre.py
+++ 1_pipeline/02_build_cre.py
@@ -25,21 +25,22 @@ def run(assembly):
         raise Exception("missing executable " + runFnp)
 
     cmds = [runFnp, "--assembly=" + assembly]
-    printt("about to run", cmds)
+    printt("about to run", " ".join(cmds))
     Utils.runCmds(cmds)
 
     fnps = paths.path(assembly, "newway", "parsed.chr*.tsv")
     cmds = ["pigz", "-f", fnps]
-    printt("about to run", cmds)
+    printt("about to run", " ".join(cmds))
     Utils.runCmds(cmds)
 
     subsampleFnp = os.path.join(os.path.dirname(__file__),
                                 '02_build_cre/subsample.sh')
     d = paths.path(assembly, "newway", "sample")
     Utils.mkdir_p(d)
+    fnps = paths.path(assembly, "newway", "parsed.chr*.tsv.gz")
     cmds = ["ls", fnps,
-            '|', "parallel", "bash", subsample]
-    printt("about to run", cmds)
+            '|', "parallel", "bash", subsampleFnp]
+    printt("about to run", " ".join(cmds))
     Utils.runCmds(cmds)
 
 def parse_args():
