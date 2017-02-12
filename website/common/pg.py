7d2b22e10d784116bf0ac6762c2d96c904285dc7
Michael Purcaro <purcaro@gmail.com>
2017-02-12 18:03:56 -0500

    refactor out PGde; smal  refactor in de model

--- website/common/pg.py
+++ website/common/pg.py
@@ -545,27 +545,6 @@ OR ensemblid_ver = %s)
             return None
         return Coord(r[0], r[1], r[2]), (r[3], r[4])
 
-    def nearbyDEs(self, coord, halfWindow, ct1, ct2, pval):
-        c = coord.expanded(halfWindow)
-        with getcursor(self.pg.DBCONN, "nearbyDEs") as curs:
-            q = """
-            SELECT start, stop, log2FoldChange, leftName, rightName, ensembl
-            from {deTn} as de
-            inner join {giTn} as gi
-            on de.ensembl = gi.ensemblid
-            where gi.chrom = %(chrom)s
-            AND de.padj <= %(pval)s
-            AND int4range(gi.start, gi.stop) && int4range(%(start)s, %(stop)s)
-            and de.leftname = %(leftName)s and de.rightname = %(rightName)s
-""".format(deTn = self.assembly + "_de",
-           giTn = self.assembly + "_gene_info")
-            curs.execute(q, { "chrom" : c.chrom, "start" : c.start,
-                              "stop" : c.end, "pval" : pval,
-                              "leftName" : ct1, "rightName" : ct2})
-            des = curs.fetchall()
-        #print("des", len(des), " ".join(q.split('\n')), c, ct1, ct2)
-        return des
-
     def allDatasets(self):
         def makeDataset(r):
             return {"assay" : r[0],
