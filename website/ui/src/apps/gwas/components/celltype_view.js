d4ae63180f90a075cd6b1630b1e923e1d4cd8801
Michael Purcaro <purcaro@gmail.com>
2017-02-12 20:45:53 -0500

    wiring cell type table

--- website/ui/src/apps/gwas/components/celltype_view.js
+++ website/ui/src/apps/gwas/components/celltype_view.js
@@ -7,8 +7,51 @@ import * as Actions from '../actions/main_actions';
 import loading from '../../../common/components/loading'
 
 class CelltypeView extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { jq: null, isFetching: false, isError: false};
+        this.loadGwas = this.loadGwas.bind(this);
+    }
 
+    componentDidMount(){
+        this.loadGwas(this.props);
+    }
+
+    componentWillReceiveProps(nextProps){
+        //console.log("componentWillReceiveProps", nextProps);
+        this.loadGwas(nextProps);
+    }
+
+    loadGwas({gwas_study, cellType, actions}){
+        var q = {GlobalAssembly, gwas_study, cellType};
+        var jq = JSON.stringify(q);
+        if(this.state.jq == jq){
+            // http://www.mattzeunert.com/2016/01/28/javascript-deep-equal.html
+            return;
+        }
+        //console.log("loadGene....", this.state.jq, jq);
+        this.setState({jq, isFetching: true});
+        $.ajax({
+            url: "/gwasJson/accs",
+            type: "POST",
+	    data: jq,
+	    dataType: "json",
+	    contentType: "application/json",
+            error: function(jqxhr, status, error) {
+                console.log("err loading cres for table");
+                this.setState({isFetching: false, isError: true});
+            }.bind(this),
+            success: function(r) {
+                this.setState({...r, isFetching: false, isError: false});
+            }.bind(this)
+        });
+    }
+
+    render() {
+        if(!(this.props.gwas_study in this.state)){
+            return loading(this.state);
+        }
+        let data = this.state[this.props.gwas_study];
 	return (<div>
                 <h3>{this.props.cellType.biosample_term_name}</h3>
 
