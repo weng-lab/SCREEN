71148e0ac340813d9280524c1d75b2e8ad561f09
Michael Purcaro <purcaro@gmail.com>
2017-02-12 20:39:15 -0500

    more wiring

--- website/ui/src/apps/gwas/components/celltype_view.js
+++ website/ui/src/apps/gwas/components/celltype_view.js
@@ -6,50 +6,12 @@ import * as Actions from '../actions/main_actions';
 
 import loading from '../../../common/components/loading'
 
-class Table2 extends React.Component {
-
-    headerCell(c){ return (<th>{c}</th>); }
-
-    cell(c){ return (<td>{c}</td>); }
-
-
-    headerRow(rd){
-	return (<tr>
-		{rd.map((c) => { return this.headerCell(c); })}
-		</tr>);
-    }
-
-    row(rd, props){
-	return (<tr>
-		{rd.map((c) => { return this.cell(c); })}
-		</tr>);
-    }
-
-    render() {
-	return (<div>
-
-                <table className="table table-bordered">
-		<thead>
-		{this.headerRow(this.props.header)}
-		</thead>
-		<tbody>
-		{this.props.rows.map((rd) => {
-		    return this.row(rd, this.props);
-		})}
-		</tbody>
-		</table>
-
-               </div>);
-    }
-}
-
 class CelltypeView extends React.Component {
     render() {
-        let d = this.props.data[this.props.cellType.ct];
-	return (<div>
-                <h3>{this.props.cellType.view}</h3>
 
-                <Table2 rows={d.accessions} header={d.header}/>
+	return (<div>
+                <h3>{this.props.cellType.biosample_term_name}</h3>
+
 		</div>);
     }
 }
