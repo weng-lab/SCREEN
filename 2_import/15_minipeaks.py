bfcbaadd438f41487d0594cf970b8aed75263b2c
Michael Purcaro <purcaro@gmail.com>
2017-02-14 10:36:56 -0500

    merge int large file
    Former-commit-id: 6e9204f7187caf2650bb923934a0a18f860ac6a3

--- 2_import/15_minipeaks.py
+++ 2_import/15_minipeaks.py
@@ -14,46 +14,56 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils/')
 from utils import Utils
 from db_utils import getcursor
 from files_and_paths import Dirs
+from paste import chunkedPaste
 
 class ImportMinipeaks:
-    def __init__(self, assembly, ver = 1):
+    def __init__(self, assembly, nbins, ver):
         self.assembly = assembly
         self.ver = ver
 
         self.cluster = Cluster()
         self.session = self.cluster.connect()
-        self.keyspace = "minipeaks_" + assembly)
+        self.keyspace = '_'.join([assembly, "minipeaks", str(ver), str(nbins)])
         self.session.execute("""
         CREATE KEYSPACE IF NOT EXISTS {ks} WITH replication
         = {'class':'SimpleStrategy', 'replication_factor':1};
         """.format(ks = self.keyspace))
         self.session.set_keyspace(self.keyspace)
 
-    def importAll(self):
-        d = "/project/umw_zhiping_weng/0_metadata/encode/data"
-        fns = ["DNase-List.txt", "H3K27ac-List.txt",
-               "H3K4me3-List.txt"]
+    def _getFileIDs(self, fn):
+        assay = fn.split('-')[0]
+        print("***********************", assay)
+        fnp = paths.path(self.assembly, "raw", fn)
+        with open(fnp) as f:
+            rows = [x.rstrip('\n').split('\t') for x in f.readlines()]
+        fileIDs = sorted([r[1] for r in rows])
+        return assay, fileIDs
 
-        outD = os.path.join(self.d, "minipeaks")
+    def importAll(self):
+        fns = ["DNase-List.txt", "H3K27ac-List.txt", "H3K4me3-List.txt"]
 
         for fn in fns:
-            print("***********************", fn)
-            fnp = os.path.join(self.d, "raw", fn)
-            with open(fnp) as f:
-                rows = [x.rstrip().split() for x in f.readlines()]
-            bfnps = []
-            for r in rows:
-                fnp = os.path.join(outD, r[1] + ".bigWig.txt.gz")
+            assay, fileIDs = self._getFileIDs(fn)
+            fnps = []
+            for fileID in fileIDs:
+                fnp = paths.path(self.assembly, "minipeaks",
+                                 fileID + ".bigWig.txt")
                 if os.path.exists(fnp):
-                    bfnps.append(fnp)
+                    fnps.append(fnp)
                 else:
                     print("WARNING: missing", fnp)
 
-            toks = fn.split('-')
-            self.processRankMethod(bfnps, toks[0])
+            self.processRankMethod(fileIDs, fnps, assay)
 
-    def processRankMethod(self, rows, rankMethod):
-        tableName = rankMethod + '_' + str(self.ver)
+    def _makeAccesionFile(self, fnp):
+        cmds = [cat(paths.path(self.assembly, "raw", "masterPeaks.bed.gz")),
+                '|', "awk -v OFS='\t' '{ print($5,$1) }'",
+                '>', fnp]
+        Utils.runCmds(cmds)
+        printWroteNumLines(fnp)
+
+    def processRankMethod(self, fileIDs, fnps, assay):
+        tableName = assay
 
         self.session.execute("""
 DROP TABLE IF EXISTS """ + tableName)
@@ -61,13 +71,20 @@ DROP TABLE IF EXISTS """ + tableName)
         self.session.execute("""
 CREATE TABLE IF NOT EXISTS {tn} (
 accession text,
-ver int,
-ctAndAvgSignals text,
-PRIMARY KEY (accession, ver) )
+chrom text,
+{fields},
+PRIMARY KEY (accession, chrom) )
 WITH compression = {{ 'sstable_compression' : 'LZ4Compressor' }};
-""".format(tn = self.tableName))
+""".format(tn = self.tableName,
+           fields = ",".join([r + " text" for r in fileIDs])))
 
+        accessionFnp = paths.path(self.assembly, "minipeaks", "accessions.txt")
+        if not os.path.exists(accessionFnp):
+            self._makeAccesionFile(accessionFnp)
 
+        mergedFnp = paths.path(self.assembly, "minipeaks", "merged.txt")
+        printt("paste into", mergedFnp)
+        chunkedPaste(mergedFnp, [accessionFnp] + fnps)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -85,7 +102,7 @@ def main():
         assemblies = [args.assembly]
 
     for assembly in assemblies:
-        im = ImportMinipeaks(assembly)
+        im = ImportMinipeaks(assembly, 20, 2)
         im.importAll()
 
 if __name__ == '__main__':
