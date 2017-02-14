3e705d0c4e208d215774d520efff1af575b4904d
Michael Purcaro <purcaro@gmail.com>
2017-02-14 12:52:42 -0500

    get minipeaks out
    Former-commit-id: 79e64ff6c44f3f8c50a444108ac0c83236850d3f

--- website/models/minipeaks_cache.py
+++ website/models/minipeaks_cache.py
@@ -3,74 +3,38 @@
 import sys
 import json
 
-if 0:
-    from cassandra.cluster import Cluster
-    from cassandra.query import BatchStatement
-    from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement, dict_factory
+from cassandra import ConsistencyLevel
 
 class MiniPeaksCache:
-    def __init__(self, rank_method, ver = 1):
-        self.rank_method = rank_method
+    def __init__(self, assembly, nbins, ver):
+        self.assembly = assembly
+        self.nbins = nbins
         self.ver = ver
-        self.tableName = rank_method + '_' + str(ver)
 
-        self.cluster = Cluster()
-        self.session = self.cluster.connect()
-        self.session.execute("""
-CREATE KEYSPACE IF NOT EXISTS minipeaks WITH replication
-= {'class':'SimpleStrategy', 'replication_factor':1};""")
-        self.session.set_keyspace("minipeaks")
+    def get(self, assay, accession):
+        cluster = Cluster()
+        session = cluster.connect()
+        session.row_factory = dict_factory
+        session.set_keyspace("minipeaks")
 
-        if 0:
-            self.session.execute("""
-DROP TABLE """ + self.tableName)
+        tableName = '_'.join([self.assembly, assay,
+                              str(self.ver), str(self.nbins)])
 
-        self.session.execute("""
-CREATE TABLE IF NOT EXISTS {tn} (
-accession text,
-ver int,
-ctAndAvgSignals text,
-PRIMARY KEY (accession, ver) )
-WITH compression = {{ 'sstable_compression' : 'LZ4Compressor' }};
-""".format(tn = self.tableName))
-
-        self.insert_stmt = self.session.prepare("""
-INSERT INTO {tn} (accession, ver, ctAndAvgSignals) values (?, ?, ?)
-""".format( tn = self.tableName))
-
-        self.select_stmt = self.session.prepare("""
-SELECT ctAndAvgSignals FROM {tn} WHERE accession = ? AND ver = ?
-""".format(tn = self.tableName ))
-
-    def insert(self, accession, values):
-        self.session.execute(self.insert_stmt,
-                             (accession, self.ver, json.dumps(values)))
-
-    def get(self, accession):
-        r = self.session.execute(self.select_stmt, (accession, self.ver))
-        if not r:
-            return {}
-        return json.loads(r[0][0])
-
-    def insertVec(self, accessionToCtAndValues):
-        batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
-        for acc, ctAndvalues in accessionToCtAndValues.iteritems():
-            batch.add(self.insert_stmt, (acc, self.ver,
-                                         json.dumps(ctAndvalues)))
-        self.session.execute(batch)
-
-    def getVec(self, accessions):
-        batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
-        ret = {}
-        for acc in accessions:
-            ret[acc] = self.get(acc)
-        return ret
+        select_stmt = session.prepare("""
+SELECT * FROM {tn} WHERE accession = ?
+""".format(tn = tableName ))
+        
+        rows = list(session.execute(select_stmt, (accession,)))
+        if 1 == len(rows):
+            return rows[0]
+        return {}
 
 if __name__ == "__main__":
-    mpc = MiniPeaksCache("dnase")
-    ret = mpc.getVec(["EM10E0371084"])
-    print(ret.keys())
-
+    mpc = MiniPeaksCache("mm10", 20, 2)
+    ret = mpc.get("DNase", "EM10E0371084")
+    print(ret)
 
     if 0:
         j = mpc.get("EM10E0371084")
