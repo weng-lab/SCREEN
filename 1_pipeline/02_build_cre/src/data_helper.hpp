5488a43f2b8c027ab08773d7e1002f8ecd6a8ab0
Michael Purcaro <purcaro@gmail.com>
2017-02-16 15:40:25 -0500

    split up

--- 1_pipeline/02_build_cre/src/data_helper.hpp
+++ 1_pipeline/02_build_cre/src/data_helper.hpp
@@ -26,6 +26,7 @@
 #include "cpp/tictoc.hpp"
 
 #include "mpname.hpp"
+#include "paths.hpp"
 #include "signal_files.hpp"
 #include "mpname.hpp"
 #include "ranks.hpp"
@@ -36,36 +37,6 @@
 namespace bib {
 
   namespace bfs = boost::filesystem;
-
-
-  class HumanMousePaths {
-  public:
-    const std::string genome_;
-    const std::string chr_;
-    const bfs::path base_;
-    bfs::path path_;
-
-    HumanMousePaths(std::string assembly, const std::string chr, bfs::path base)
-      : genome_(assembly)
-      , chr_(chr)
-      , base_(base)
-    {
-      path_ = base_ / chr_;
-    }
-
-    bfs::path allGenes(){ return path_ / "all_cre_genes.bed.gz"; }
-    bfs::path pcGenes(){ return path_ / "pc_cre_genes.bed.gz"; }
-    bfs::path peaks(){ return path_ / "masterPeaks.bed"; }
-    bfs::path tads(){ return path_ / "TADs.txt"; }
-    bfs::path geneIDfnp(){ return base_ / "ensebleToID.txt"; }
-    bfs::path signalDir(){
-      return path_ / "signal-output";
-    }
-
-    bfs::path listFile(const std::string name){
-      return base_ / (name + "-List.txt");
-    }
-  };
 
   class DataHelper {
     Peaks& peaks_;
