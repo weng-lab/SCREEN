59487344a3b71116baa07fdae55989f05243e774
Michael Purcaro <purcaro@gmail.com>
2017-02-20 14:28:05 -0500

    split up config files

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys, os
+import ConfigParser
+
+class Config:
+    fnp = os.path.join(os.path.dirname(__file__), "config.ini")
+    if not os.path.exists(fnp):
+        print("ERROR: file not found:", fnp)
+        print("\tfile should be symlink'd to a desired config.<blah>.ini file")
+        sys.exit(1)
+    
+    c = ConfigParser.ConfigParser()
+    c.read(fnp)
+
+    version = c.get("RE", "version")
+    database = c.get("RE", "database")
