4c28d11c5c8fdd53124751e26210e932e8b4aa6b
Michael Purcaro <purcaro@gmail.com>
2017-02-20 14:28:05 -0500

    split up config files

--- common/dbconnect.py
+++ common/dbconnect.py
@@ -2,12 +2,16 @@ import psycopg2.pool
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../metadata/utils"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+from config import Config
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             "../../metadata/utils"))
 from dbs import DBS
 
 def db_connect(script):
     dbs = {"host": "postgresql",
            "user": "regElmViz_usr",
-           "dbname": "regElmViz",
+           "dbname": Config.database,
            "application_name" : script}
     return psycopg2.pool.ThreadedConnectionPool(1, 32, **dbs)
