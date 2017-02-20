ca19892540467a0cacf74075ce052a2911d8eb9c
Michael Purcaro <purcaro@gmail.com>
2017-02-20 15:11:22 -0500

    cleanup of peaks class
    Former-commit-id: fb9537ee9e9c58b3c6fba04f08305988300bc853

--- common/dbconnect.py
+++ common/dbconnect.py
@@ -2,14 +2,16 @@ import psycopg2.pool
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from config import Config
-
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              "../../metadata/utils"))
 from dbs import DBS
+from utils import AddPath 
+
+AddPath(__file__, "../")
+from config import Config
 
 def db_connect(script):
+    # assumes .pgpass file, like http://stackoverflow.com/a/28801642
     dbs = {"host": "postgresql",
            "user": "regElmViz_usr",
            "dbname": Config.database,
