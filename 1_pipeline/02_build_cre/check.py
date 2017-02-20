3669f84832663e6cd893c877c9a5f16067d1955d
Michael Purcaro <purcaro@gmail.com>
2017-02-20 18:09:36 -0500

    working on checker
    Former-commit-id: 67e025c933052c96415ba2d3980d6a925f3d9852

--- 1_pipeline/02_build_cre/check.py
+++ 1_pipeline/02_build_cre/check.py
@@ -13,20 +13,55 @@ AddPath(__file__, '../../common/')
 from dbconnect import db_connect
 from constants import chroms, chrom_lengths, paths
 
+from collections import namedtuple
+CRE = namedtuple('CRE', ["chrom", "start", "stop", "mpName", "accession"])
+
 class CheckCellTypes:
     def __init__(self, curs, assembly):
         self.curs = curs
         self.assembly = assembly
+        self._load()
+
+    def _load(self):
+        fnBases = [("CTCF", 2),
+                   ("DNase", 2),
+                   ("Enhancer", 4),
+                   ("H3K27ac", 2),
+                   ("H3K4me3", 2),
+                   ("Insulator", 4),
+                   ("Promoter", 4)]
+        self.rankMethodToCtAndFileID = []
+        for fnBase, ctIdx in fnBases:
+            fn = fnBase + "-List.txt"
+            fnp = paths.path(self.assembly, "raw", fn)
+            if not os.path.exists(fnp):
+                raise Exception("missing " + fnp)
+            with open(fnp) as f:
+                rows = [x.rstrip('\n').split('\t') for x in f]
+            for r in rows:
+                efn = r[:ctIdx]
+                if 2 == len(efn):
+                    efn = '-'.join(efn) + ".txt"
+                else:
+                    efn = '.'.join(['-'.join(efn[:2]),
+                                    '-'.join(efn[2:4])]) + ".txt"
+                fnp = paths.path(self.assembly, "raw", "signal-output", efn)
+                if 0:
+                    if not os.path.exists(fnp):
+                        raise Exception("missing", fnp)
+                d = [fnBase, r[ctIdx], fnp]
+                self.rankMethodToCtAndFileID.append(d)
 
     def run(self):
         fnp = paths.path(self.assembly, "raw", "masterPeaks.bed.gz")
         cmds = ["zcat", fnp,
                 '|',
-                """awk 'BEGIN {srand()} !/^$/ { if (rand() <= .01) print $0}'"""]
-        cres = Utils.runCmds(cmds)
-        for cre in cres:
-            print(cre)
-
+                """awk 'BEGIN {srand()} !/^$/ { if(rand() <= .00001) print $0}'"""]
+        cres = [x.rstrip('\n').split('\t') for x in Utils.runCmds(cmds) if x]
+        cres = filter(lambda x: x[0] in chroms[self.assembly], cres)
+        print("selected", len(cres), "cres")
+        cres = [CRE(*x) for x in cres]
+        
 
 
 def parse_args():
@@ -45,7 +80,8 @@ def main():
         assemblies = [args.assembly]
 
     for assembly in assemblies:
-        with getcursor(DBCONN, "04_cellTypeInfo") as curs:
+        with getcursor(DBCONN, "check") as curs:
+            print('***********', assembly)
             ctt = CheckCellTypes(curs, assembly)
             ctt.run()
 
