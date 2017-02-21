ee749437faaf79b7c66f3d46febbb0f17650ea06
Michael Purcaro <purcaro@gmail.com>
2017-02-21 15:32:04 -0500

    react-tify landing
    Former-commit-id: fc79b14aa4e6b3a7b38267d7b5e283945ddc8c28

--- website/ui/src/apps/index/tab_main.js
+++ website/ui/src/apps/index/tab_main.js
@@ -1,71 +1,105 @@
-const TabMain = () => {
-    let textBox = (<div>
-		   {"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
-		   <a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
-		   {". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
-		   <hr />
-		   {"You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs."}
-		   
-		   <div id={"gwasGroup"}>
-		   <a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>
-		   {"Browse GWAS"}
-		   </a>
-		   </div>
+import React from 'react';
 
-		   </div>);
+class TabMain extends React.Component {
+    constructor(props) {
+	super(props);
+	this.searchHg19 = this.searchHg19.bind(this);
+	this.searchMm10 = this.searchMm10.bind(this);
+    }
+    
+    searchHg19() {
+	$("#searchformassembly").attr("value", "hg19")
+	$("#searchform").submit();
+    }
+    
+    searchMm10() {
+	$("#searchformassembly").attr("value", "mm10")
+	$("#searchform").submit();
+    }
 
-    let logo = (<img
+    textBox() {
+	return (<div>
+		{"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
+		<a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
+		{". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
+		<hr />
+		{"You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs."}
+		
+		<div id={"gwasGroup"}>
+		<a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>
+		{"Browse GWAS"}
+		</a>
+		</div>
+
+		</div>);
+    }
+    
+    logo(){
+	return (<img
 		className={"img-responsive mainLogo"}
 		src={"/static/encodeLogo.png"}
 		alt={"ENCODE logo"} />);
+    }
 
-    let searchBox = (
-            <div>
-            <form action={"search"} method={"get"} id={"searchform"}>
-            <span style={{color : "#ff0000", fontWeight : "bold"}}>{"failedsearch"}</span>
-            <br />
-
-            <div className={"form-group text-center"}>
-	    <input id={"mainSearchbox"} type={"text"} name={"q"} defaultValue={"K562 chr11:5226493-5403124"} />
-	    <input id={"searchformassembly"} name={"assembly"} value={"hg19"} type={"hidden"} />
-            </div>
-	    
-            <div id={"mainButtonGroup"}>
-	    <a className={"btn btn-primary btn-lg"} href={"javascript:searchHg19();"} role={"button"}>Search hg19</a>
-            {" "}
-	    <a className={"btn btn-success btn-lg"} href={"javascript:searchMm10();"} role={"button"}>Search mm10</a>
-            <br />
-            <br />
-            <i>{'Examples: "K562 chr11:5226493-5403124", "SOX4 TSS", "rs4846913"'}</i>
-            </div>
-	    
-        </form>
-	    
-            </div>);
+    searchErr() {
+	return (<span
+		style={{color : "#ff0000", fontWeight : "bold"}}>
+		{"failedsearch"}
+		</span>);
+    }
     
-    return (<div>
+    searchBox(){
+	return (
+		<div>
+		<form action={"search"} method={"get"} id={"searchform"}>
+		{this.searchErr()}
+		<br />
 
-	    <div className={"row vertical-align"}>
+		<div className={"form-group text-center"}>
+		<input id={"mainSearchbox"} type={"text"} name={"q"} defaultValue={"K562 chr11:5226493-5403124"} />
+		<input id={"searchformassembly"} name={"assembly"} value={"hg19"} type={"hidden"} />
+		</div>
+		
+		<div id={"mainButtonGroup"}>
 
-	    <div className={"col-md-6"}>
-	    {logo}
-            </div>
-	    
-            <div className={"col-md-6"}>
-            <div className={"jumbotron"} id={"mainDesc"}>
-	    {textBox}
-	    </div>
-            </div>
-	    
-	    </div>
-	    
-            <div className={"row"}>
-            <div className={"col-md-12 text-center"}>
-	    {searchBox}
-            </div>
-            </div>
-	    
+		<a className={"btn btn-primary btn-lg"} onClick={this.searchHg19} role={"button"}>Search hg19</a>
+		{" "}
+		<a className={"btn btn-success btn-lg"} onClick={this.searchMm10} role={"button"}>Search mm10</a>
+		<br />
+		<br />
+		<i>{'Examples: "K562 chr11:5226493-5403124", "SOX4 TSS", "rs4846913"'}</i>
+		</div>
+		
+            </form>
+		
             </div>);
+    }
+
+    render() {
+	return (<div>
+
+		<div className={"row vertical-align"}>
+
+		<div className={"col-md-6"}>
+		{this.logo()}
+		</div>
+		
+		<div className={"col-md-6"}>
+		<div className={"jumbotron"} id={"mainDesc"}>
+		{this.textBox()}
+		</div>
+		</div>
+		
+		</div>
+		
+		<div className={"row"}>
+		<div className={"col-md-12 text-center"}>
+		{this.searchBox()}
+		</div>
+		</div>
+		
+		</div>);
+    }
 }
 
 export default TabMain;
