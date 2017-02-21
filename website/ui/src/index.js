fdaf66fa36327236fd21ac226b196ecc522292e5
Michael Purcaro <purcaro@gmail.com>
2017-02-21 14:03:47 -0500

    refactoring index
    Former-commit-id: 901af10e93e4d8a2a248a198f89f47b81259486c

--- website/ui/src/index.js
+++ website/ui/src/index.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill'
+import 'babel-polyfill';
 
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory} from 'react-router';
@@ -7,6 +7,7 @@ import Search from './apps/search/main';
 import GeneExp from './apps/geneexp/main';
 import De from './apps/de/main';
 import Gwas from './apps/gwas/main';
+import IndexPage from './apps/index/index';
 
 //import Tf from './apps/tf/main';
 //<Route path="/comparison/:assembly" component={Comparison} />
@@ -14,97 +15,6 @@ import Gwas from './apps/gwas/main';
 //import Comparison from './apps/comparison/main';
 // import CartPage from './apps/cart/main';
 //     <Route path="/cart" component={CartPage} />
-
-class IndexPage extends React.Component {
-    render() {
-        let mainPage = (<div>
-                        <div className={"panel panel-default"}>
-                        <div className={"panel-body"}>
-                        <div className={"container-fluid"}>
-
-                        <div className={"row vertical-align"}>
-                        <div className={"col-md-6"}>
-                        <img className={"img-responsive mainLogo"} src={"/static/encodeLogo.png"} alt={"ENCODE logo"} />
-                        </div>
-                        <div className={"col-md-6"}>
-                        <div className={"jumbotron"} id={"mainDesc"}>
-                        {"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
-                        <a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
-                        {". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
-                        <hr />
-	                {"You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs."}
-                        <div id={"gwasGroup"}>
-                        <a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>{"Browse GWAS"}</a>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-
-                        <div className={"row"}>
-                        <div className={"col-md-12 text-center"}>
-                        <div>
-                        <form action={"search"} method={"get"} id={"searchform"}>
-                        <span style={{color : "#ff0000", fontWeight : "bold"}}>{"failedsearch"}</span>
-                        <br />
-                        <div className={"form-group text-center"}>
-                        <input id={"mainSearchbox"} type={"text"} name={"q"} defaultValue={"K562 chr11:5226493-5403124"} />
-                        <input id={"searchformassembly"} name={"assembly"} value={"hg19"} type={"hidden"} />
-                        </div>
-                        <div id={"mainButtonGroup"}>
-	                <a className={"btn btn-primary btn-lg"} href={"javascript:searchHg19();"} role={"button"}>Search hg19</a>
-                        {" "}
-	                <a className={"btn btn-success btn-lg"} href={"javascript:searchMm10();"} role={"button"}>Search mm10</a>
-                        <br />
-                        <br />
-                        <i>{'Examples: "K562 chr11:5226493-5403124", "SOX4 TSS", "rs4846913"'}</i>
-                        </div>
-                        </form>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        </div>);
-
-        let tabs = (<div id="exTab1" className="container">
-                    <ul  className="nav nav-pills">
-		    <li className="active">
-                    <a  href="#overview" data-toggle="tab">Overview</a>
-		    </li>
-		    <li><a href="#about" data-toggle="tab">About</a>
-		    </li>
-		    <li><a href="#tut" data-toggle="tab">Tutorial</a>
-		    </li>
-		    </ul>
-
-		    <div className="tab-content clearfix">
-		    <div className="tab-pane active" id="overview">
-                    {mainPage}
-                    </div>
-                    <div className="tab-pane" id="about">
-                    </div>
-                    <div className="tab-pane" id="tut">
-		    </div>
-		    </div>
-                    </div>);
-
-        return (<div>
-                <div className={"container-fluid"}>
-
-                <div className={"row"}>
-                <div className={"col-md-12"}>
-                <div id={"mainTitle"}>{"SCREEN: Search Candidate Regulatory Elements by ENCODE"}</div>
-                </div>
-                </div>
-
-                </div>
-
-                {tabs}
-                </div>
-               );
-    }
-}
 
 function myCreateElement(Component, props) {
     return (<Component {...props} />);
