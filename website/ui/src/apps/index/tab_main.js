3eae822c11c7afd0c2bc126b99f4c638f6b5e5fc
Michael Purcaro <purcaro@gmail.com>
2017-02-21 14:54:24 -0500

    break up tab_overview

--- website/ui/src/apps/index/tab_main.js
+++ website/ui/src/apps/index/tab_main.js
@@ -1,34 +1,35 @@
 const TabMain = () => {
-    return (
-	    <div>
-            <div className={"row vertical-align"}>
-            <div className={"col-md-6"}>
-            <img className={"img-responsive mainLogo"} src={"/static/encodeLogo.png"} alt={"ENCODE logo"} />
-            </div>
-            <div className={"col-md-6"}>
-            <div className={"jumbotron"} id={"mainDesc"}>
-            {"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
-            <a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
-            {". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
-            <hr />
-	    {"You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs."}
-            <div id={"gwasGroup"}>
-            <a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>{"Browse GWAS"}</a>
-            </div>
-            </div>
-            </div>
-            </div>
+    let textBox = (<div>
+		   {"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
+		   <a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
+		   {". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
+		   <hr />
+		   {"You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs."}
+		   
+		   <div id={"gwasGroup"}>
+		   <a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>
+		   {"Browse GWAS"}
+		   </a>
+		   </div>
 
-            <div className={"row"}>
-            <div className={"col-md-12 text-center"}>
+		   </div>);
+
+    let logo = (<img
+		className={"img-responsive mainLogo"}
+		src={"/static/encodeLogo.png"}
+		alt={"ENCODE logo"} />);
+
+    let searchBox = (
             <div>
             <form action={"search"} method={"get"} id={"searchform"}>
             <span style={{color : "#ff0000", fontWeight : "bold"}}>{"failedsearch"}</span>
             <br />
+
             <div className={"form-group text-center"}>
-            <input id={"mainSearchbox"} type={"text"} name={"q"} defaultValue={"K562 chr11:5226493-5403124"} />
-            <input id={"searchformassembly"} name={"assembly"} value={"hg19"} type={"hidden"} />
+	    <input id={"mainSearchbox"} type={"text"} name={"q"} defaultValue={"K562 chr11:5226493-5403124"} />
+	    <input id={"searchformassembly"} name={"assembly"} value={"hg19"} type={"hidden"} />
             </div>
+	    
             <div id={"mainButtonGroup"}>
 	    <a className={"btn btn-primary btn-lg"} href={"javascript:searchHg19();"} role={"button"}>Search hg19</a>
             {" "}
@@ -37,10 +38,33 @@ const TabMain = () => {
             <br />
             <i>{'Examples: "K562 chr11:5226493-5403124", "SOX4 TSS", "rs4846913"'}</i>
             </div>
-            </form>
+	    
+        </form>
+	    
+            </div>);
+    
+    return (<div>
+
+	    <div className={"row vertical-align"}>
+
+	    <div className={"col-md-6"}>
+	    {logo}
+            </div>
+	    
+            <div className={"col-md-6"}>
+            <div className={"jumbotron"} id={"mainDesc"}>
+	    {textBox}
+	    </div>
+            </div>
+	    
+	    </div>
+	    
+            <div className={"row"}>
+            <div className={"col-md-12 text-center"}>
+	    {searchBox}
             </div>
             </div>
-            </div>
+	    
             </div>);
 }
 
