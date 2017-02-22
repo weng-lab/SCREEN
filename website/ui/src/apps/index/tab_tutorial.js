1d690c0fc76f12a3a17d1c1e141e7999d3f05d16
Michael Purcaro <purcaro@gmail.com>
2017-02-22 11:01:28 -0500

    tutorial
    Former-commit-id: 3075e2134021455aabe8265bffaade24925ff4ce

--- website/ui/src/apps/index/tab_tutorial.js
+++ website/ui/src/apps/index/tab_tutorial.js
@@ -1,6 +1,11 @@
 const TabTutorial = () => {
     return (<div>
 	    <h2>Tutorials</h2>
+            <h3>Main Search</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/iB9VUgZO8NA" frameborder="0" allowfullscreen></iframe>
+
+            <h3>Gene Expression</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Vs6NQRKMAdM" frameborder="0" allowfullscreen></iframe>
 	    </div>);
 }
 
