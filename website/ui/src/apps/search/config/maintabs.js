4c4286bb4e2f473802161e9835fad0cb9b8ecb9f
Michael Purcaro <purcaro@gmail.com>
2017-02-22 15:03:44 -0500

    only load gene expression after clikcing tab; make a materialized view for the gene alias search, and use it

--- website/ui/src/apps/search/config/maintabs.js
+++ website/ui/src/apps/search/config/maintabs.js
@@ -38,13 +38,37 @@ class ExpressionPlot extends React.Component {
     constructor(props) {
 	super(props);
 	this.state = {};
+	this.state = { ges : {}, jq: null, isFetching: true, isError: false };
+        this.loadGe = this.loadGe.bind(this);
     }
-    componentWillReceiveProps() {
-	if (!GlobalParsedQuery.approved_symbol) return;
+
+    componentWillReceiveProps(nextProps) {
+        // only check/get data if we are active tab
+        if("expression" == nextProps.maintabs_active){
+            this.loadGe(nextProps);
+        }
+    }
+
+    loadGe({}){
+	let gene = GlobalParsedQuery.approved_symbol;
+	if (!gene) {
+	    return;
+	}
+	if(gene in this.state.ges){
+	    return;
+	}
+	var q = {GlobalAssembly, gene, compartments_selected: new Set(["cell"])};
+	var jq = JSON.stringify(q);
+	if(this.state.jq == jq){
+            // http://www.mattzeunert.com/2016/01/28/javascript-deep-equal.html
+            return;
+        }
+        //console.log("loadCREs....", this.state.jq, jq);
+        this.setState({jq, isFetching: true});
 	$.ajax({
 	    url: "/geneexpjson",
 	    type: "POST",
-	    data: JSON.stringify({GlobalAssembly, gene: this.props.gene, compartments_selected: new Set(["cell"])}),
+	    data: jq,
 	    dataType: "json",
 	    contentType: "application/json",
 	    error: function(jqxhr, status, error) {
@@ -52,20 +76,33 @@ class ExpressionPlot extends React.Component {
 		this.setState({isFetching: false, isError: true});
 	    }.bind(this),
 	    success: function(r) {
-		this.setState({...r, isFetching: false, isError: false});
+		this.setState({ges : {gene : r}, isFetching: false, isError: false});
 	    }.bind(this)
 	});
     }
+    
     doRenderWrapper(){
-	if (!GlobalParsedQuery.approved_symbol) return <div />;
-	let gene = this.props.gene;
-	let url = '/geneexp/' + GlobalAssembly + "/" + this.props.gene;
-	let message = <div>This plot is displaying cell-wide expression of <em>{this.props.gene}</em>. To view expression in subcellular compartments, <a href={url}>click here</a>.</div>;
-	if("items" in this.state){
-	    return (<div><h2><em>{this.props.gene}</em></h2>{message}<br /><ExpressionBoxplot data={this.state} /></div>);
+	let gene = GlobalParsedQuery.approved_symbol;
+	if (!gene) {
+	    return <div />;
 	}
+	
+	let url = '/geneexp/' + GlobalAssembly + "/" + gene;
+
+	let message = <div>This plot is displaying cell-wide expression of <em>{this.props.gene}</em>. To view expression in subcellular compartments, <a href={url}>click here</a>.</div>;
+
+	if(gene in this.state.ges){
+	    return (
+		<div>
+		    <h2><em>{this.props.gene}</em></h2>
+		    {message}<br />
+		<ExpressionBoxplot data={this.state.ges[gene]} />
+		</div>);
+	}
+
 	return loading(this.state);
     }
+
     render(){
 	return (<div style={{"width": "100%"}} >
 		{this.doRenderWrapper()}
@@ -73,22 +110,17 @@ class ExpressionPlot extends React.Component {
     }
 }
 
-class ExpressionTab extends React.Component {
-    render() {return <ExpressionPlot gene={GlobalParsedQuery.approved_symbol} />;}
+const MainTabInfo = () => {
+    let gene = GlobalParsedQuery.approved_symbol;
+    let geTitle = gene ? gene + " Expression" : "";
+    
+    return {results : {title: "cRE Search Results", visible: true, f: ResultsTab},
+	    expression: {title: geTitle, visible: !!gene, f: ExpressionPlot},
+	    aprofile: {title: "Activity Profile", visible: false, f: ActivityProfileTab},
+	    ct_tree: {title: "Cell Type Clustering", visible: GlobalAssembly == "mm10", f: TreeTab},
+	    tf_enrichment: {title: "TF Enrichment", visible: false, f: TFTab},
+	    details: {title: "cRE Details", visible: false, f: DetailsTab},
+	    gcompare: {title: "Group Comparison", visible: false, f: GcompareTab}};
 }
-
-const MainTabInfo = () => ({
-    results : {title: "cRE Search Results", visible: true, f: ResultsTab},
-    expression: {title: GlobalParsedQuery.approved_symbol ?
-                 GlobalParsedQuery.approved_symbol + " Expression" :
-                 "",
-                 visible: !!GlobalParsedQuery.approved_symbol,
-                 f: ExpressionTab},
-    aprofile: {title: "Activity Profile", visible: false, f: ActivityProfileTab},
-    ct_tree: {title: "Cell Type Clustering", visible: GlobalAssembly == "mm10", f: TreeTab},
-    tf_enrichment: {title: "TF Enrichment", visible: false, f: TFTab},
-    details: {title: "cRE Details", visible: false, f: DetailsTab},
-    gcompare: {title: "Group Comparison", visible: false, f: GcompareTab}
-});
 
 export default MainTabInfo;
