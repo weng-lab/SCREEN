e9c55e15cbf532c105b05e381fd5209dda755618
Michael Purcaro <purcaro@gmail.com>
2017-02-22 10:44:11 -0500

    more about

--- website/ui/src/apps/index/tab_main.js
+++ website/ui/src/apps/index/tab_main.js
@@ -17,7 +17,7 @@ class TabMain extends React.Component {
 
     loadSearch(assembly, userQuery){
 	this.setState({userQueryErr : (<i className="fa fa-refresh fa-spin"
-				       style={{fontSize : "24px"}}></i>)});
+				          style={{fontSize : "24px"}}></i>)});
 	let jq = JSON.stringify({assembly, userQuery});
 	$.ajax({
 	    url: "/autows/search",
@@ -31,11 +31,11 @@ class TabMain extends React.Component {
 	    success: function(r){
 		if(r.failed){
 		    let userQueryErr = (
-			    <span>
+			<span>
 			    Error: no results for your query.
 			    <br />
 			    Please check your spelling and search assembly, and try again.
-			    </span>);
+			</span>);
 		    this.setState({userQueryErr})
 		} else {
 		    let params = jQuery.param({q: userQuery, assembly});
@@ -57,62 +57,61 @@ class TabMain extends React.Component {
     }
 
     textBox() {
-	return (<div className="container">
+	return (
+            <div className="container">
 
                 <div className="row">
-                <div className="col-md-12">
-		{"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
-		<a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
-		{". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
-                </div>
+                    <div className="col-md-12">
+		        {"SCREEN is a web interface for searching and visualizing the Registry of candidate Regulatory Elements (cREs) derived from "}
+		        <a href={"https://encodeproject.org/"} target={"_blank"}>ENCODE data</a>
+		        {". The Registry contains 2.67M human cREs in hg19 and 1.67M mouse cREs in mm10, with orthologous cREs cross-referenced.  SCREEN presents the data that support biochemical activities of the cREs and the expression of nearby genes in specific cell and tissue types."}
+                    </div>
                 </div>
 
                 <div className="row"><br />
                 </div>
 
                 <div className="row">
-                <div className="col-md-8">
-                You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs.
-                </div>
-                <div className="col-md-4">
-		<a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>
-		{"Browse GWAS"}
-		</a>
-		</div>
+                    <div className="col-md-8">
+                        You may launch SCREEN using the search box below or browse a curated list of SNPs from the NHGRI-EBI GWAS catalog to annotate genetic variants using cREs.
+                    </div>
+                    <div className="col-md-4">
+		        <a className={"btn btn-primary"} href={"/gwasApp/hg19/"} role={"button"}>
+		            {"Browse GWAS"}
+		        </a>
+		    </div>
                 </div>
 
-                </div>);
+        </div>);
     }
 
     logo(){
 	return (<img
-		className={"img-responsive mainLogo"}
-		src={"/static/encode/classic-image.jpg"}
-		alt={"ENCODE logo"} />);
+className={"img-responsive mainLogo"}
+src={"/static/encode/classic-image.jpg"}
+alt={"ENCODE logo"} />);
     }
 
     searchBox(){
 	let dv = "K562 chr11:5226493-5403124";
 	let examples = 'Examples: "K562 chr11:5226493-5403124", "SOX4 TSS", "rs4846913"';
 	return (<div>
-
-		<div className={"form-group text-center"}>
+	    <div className={"form-group text-center"}>
 		<input ref="searchBox" id={"mainSearchbox"}
-		type={"text"} defaultValue={dv} />
-		</div>
+type={"text"} defaultValue={dv} />
+	    </div>
 
-		<div id={"mainButtonGroup"}>
+	    <div id={"mainButtonGroup"}>
 		<a className={"btn btn-primary btn-lg"}
-		onClick={this.searchHg19} role={"button"}>Search hg19</a>
+                   onClick={this.searchHg19} role={"button"}>Search hg19</a>
 		{" "}
 		<a className={"btn btn-success btn-lg"}
-		onClick={this.searchMm10} role={"button"}>Search mm10</a>
+                   onClick={this.searchMm10} role={"button"}>Search mm10</a>
 		<br />
 		<br />
 		<i>{examples}</i>
-		</div>
-
-		</div>);
+	    </div>
+	</div>);
     }
 
     componentDidMount(){
@@ -155,33 +154,28 @@ class TabMain extends React.Component {
     render() {
 	return (<div>
 
-		<div className={"row vertical-align"}>
-
+	    <div className={"row vertical-align"}>
 		<div className={"col-md-6"}>
-		{this.logo()}
+		    {this.logo()}
 		</div>
-
 		<div className={"col-md-6"}>
-		<div className={"jumbotron"} id={"mainDesc"}>
-		{this.textBox()}
+		    <div className={"jumbotron"} id={"mainDesc"}>
+		        {this.textBox()}
+		    </div>
 		</div>
-		</div>
+	    </div>
 
-		</div>
-
-		<div className={"row"}>
+	    <div className={"row"}>
 		<div className={"col-md-12 text-center"}>
-
-		<span className={"mainPageErr"}>
-		{this.state.userQueryErr}
-		</span>
-		<br />
-
-		{this.searchBox()}
+		    <span className={"mainPageErr"}>
+		        {this.state.userQueryErr}
+		    </span>
+		    <br />
+		    {this.searchBox()}
 		</div>
-		</div>
+	    </div>
 
-		</div>);
+	</div>);
     }
 }
 
