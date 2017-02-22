82a382001d5fd62c0355aa047fae553c1fd4cc73
Michael Purcaro <purcaro@gmail.com>
2017-02-22 12:59:25 -0500

    keep inside container

--- website/ui/src/apps/index/tab_about.js
+++ website/ui/src/apps/index/tab_about.js
@@ -2,79 +2,83 @@ import * as Para from './tab_about_paragraphs';
 
 const TabAbout = () => {
     let content = (
-        <div className="container">
+        <div>
             <div className="row">
                 <div className="col-md-12">
                     <h3>The ENCODE Encyclopedia</h3>
-                    <img src="/static/about/images/image04.png"
-                         className={"img-responsive"}
-                         alt={"ENCODE data levels"} />
-                    {Para.intro()}
+		    <div className="text-center">
+			<img src="/static/about/images/image04.png"
+                             className={"img-responsive"}
+                             alt={"ENCODE data levels"}
+			     style={{maxHeight: "500px"}}
+			/>
+		    </div>
+		    {Para.intro()}
                 </div>
-            </div>
+	    </div>
 
-            <div className="row">
+	    <div className="row">
                 <div className="col-md-12">
-                    <h4>The Registry of candidate Regulatory Elements</h4>
+		    <h4>The Registry of candidate Regulatory Elements</h4>
                 </div>
-            </div>
-            <div className="row">
+	    </div>
+	    <div className="row">
                 <div className="col-md-8">
-                    {Para.registry1()}
-                    {Para.registry2()}
-                    {Para.registry3()}
-                    {Para.registry4()}
+		    {Para.registry1()}
+		    {Para.registry2()}
+		    {Para.registry3()}
+		    {Para.registry4()}
                 </div>
                 <div className="col-md-4">
-                    <img src="/static/about/images/image00.png"
+		    <img src="/static/about/images/image00.png"
                          className={"img-responsive"}
                          alt={"UCSC data tracks"} />
-                    <img src="/static/about/images/image02.png"
+		    <img src="/static/about/images/image02.png"
                          className={"img-responsive"}
                          alt={"CRE flow chart"} />
                 </div>
-            </div>
-            <div className="row">
+	    </div>
+	    <div className="row">
                 <div className="col-md-8">
-                    <img src="/static/about/images/image06.png"
+		    <img src="/static/about/images/image06.png"
                          className={"img-responsive"}
                          alt={"pie chart"} />
                 </div>
                 <div className="col-md-4">
                 </div>
-            </div>
+	    </div>
 
-            <div className="row">
+	    <div className="row">
                 <div className="col-md-12">
-                    <h3>Annotating candidate Regulatory Elements</h3>
+		    <h3>Annotating candidate Regulatory Elements</h3>
                 </div>
-            </div>
-            <div className="row">
+	    </div>
+	    <div className="row">
                 <div className="col-md-8">
-                    <h4>Genomic Context</h4>
-                    {Para.genomicContext()}
+		    <h4>Genomic Context</h4>
+		    {Para.genomicContext()}
                 </div>
                 <div className="col-md-4">
-                    <img src="/static/about/images/image05.png"
+		    <img src="/static/about/images/image05.png"
                          className={"img-responsive"}
                          alt={"pie chart"} />
                 </div>
-            </div>
-            <div className="row">
+	    </div>
+	    <div className="row">
                 <div className="col-md-8">
-                    <h4>Histone Modifications and Transcription Factor Occupancy</h4>
-                    {Para.occupancy1()}
-                    {Para.occupancy2()}
+		    <h4>Histone Modifications and Transcription Factor Occupancy</h4>
+		    {Para.occupancy1()}
+		    {Para.occupancy2()}
                 </div>
                 <div className="col-md-4">
-                    <img src="/static/about/images/image03.png"
+		    <img src="/static/about/images/image03.png"
                          className={"img-responsive"}
                          alt={"pie chart"} />
-                    <img src="/static/about/images/image01.png"
+		    <img src="/static/about/images/image01.png"
                          className={"img-responsive"}
                          alt={"pie chart"} />
                 </div>
-            </div>
+	    </div>
         </div>);
 
     return (<div>
