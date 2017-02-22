27206a23699768be92eb6de122bea62a0367e375
Michael Purcaro <purcaro@gmail.com>
2017-02-21 22:16:55 -0500

    fix labels

--- website/ui/src/apps/search/config/details_tables.js
+++ website/ui/src/apps/search/config/details_tables.js
@@ -39,7 +39,7 @@ export const TopTissuesTables = () => ({
 	rank_f: (d) => (Math.log(d["one"]))
     },
     "ctcf": {
-	title: "CTCF Z-scores",
+	title: "CTCF-bound Z-scores",
 	cols: [
 	    {title: "cell type", data: "ct", className: "dt-right",
 	     render: Render.cell_type},
