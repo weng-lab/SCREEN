eb280868c8e665a91120d35805a2bbb1e0184e0a
Michael Purcaro <purcaro@gmail.com>
2017-02-23 18:15:28 -0500

    spinner for loading

--- website/ui/src/common/components/loading.js
+++ website/ui/src/common/components/loading.js
@@ -2,10 +2,12 @@ const loading = ({isFetching, message}) => {
     if (!message) {
         message = "";
     }
-    return (<div className={"loading"}
-            style={{"display": (isFetching ? "block" : "none")}}>
-	    Loading... {message}
-	    </div>);
+    return (
+        <div className={"loading"}
+             style={{"display": (isFetching ? "block" : "none")}}>
+            Loading... {message}
+            <i className="fa fa-refresh fa-spin" style={{fontSize: "24px"}} />
+        </div>);
 }
 
 export default loading;
