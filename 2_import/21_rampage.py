c33be6ba7e37e3d1669c43b64e52fb2bd400e675
Michael Purcaro <purcaro@gmail.com>
2017-02-23 16:49:04 -0500

    import rampage exp info

--- 2_import/21_rampage.py
+++ 2_import/21_rampage.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 from utils import AddPath, Utils, Timer, printt
 from db_utils import getcursor, vacumnAnalyze, makeIndex, makeIndexIntRange
 from files_and_paths import Dirs, Tools, Genome, Datasets
+from exp import Exp
 
 AddPath(__file__, '../common/')
 from dbconnect import db_connect
@@ -40,6 +41,7 @@ def doImport(curs, assembly):
     for h in header[3:]:
         h = h.split('_')[0]
         cols.append(h)
+    exps = cols[6:]
 
     tableName = assembly + "_rampage"
     printt("copy into", tableName)
@@ -54,7 +56,7 @@ start integer,
 stop integer,
 strand VARCHAR(1),
 {fields}
-);""".format(tn = tableName, fields = ','.join([f + " real" for f in cols[6:]])))
+);""".format(tn = tableName, fields = ','.join([f + " real" for f in exps])))
 
     curs.copy_from(outF, tableName, '\t', columns = cols)
 
@@ -62,6 +64,48 @@ def doIndex(curs, assembly):
     tableName = assembly + "_rampage"
     makeIndex(curs, tableName, ["ensemblid_ver", "chrom"])
     makeIndexIntRange(curs, tableName, ["start", "stop"])
+
+def metadata(curs, assembly):
+    if "hg19" == assembly:
+        fnp = os.path.join(Dirs.metadata_base, "roderic/public_docs.crg.es/rguigo/encode/expressionMatrices/H.sapiens/hg19/2016_11/gene.V19.hg19.RAMPAGE.2016_11_23.tsv.gz")
+    printt("reading", fnp)
+    with gzip.open(fnp) as f:
+        header = f.readline().rstrip('\n').split('\t')
+
+    expIDs = []
+    for h in header[3:]:
+        h = h.split('_')[0]
+        expIDs.append(h)
+    expIDs = list(set(expIDs))
+
+    tableName = assembly + "_rampage_info"
+    printt("dropping and creating", tableName)
+
+    curs.execute("""
+DROP TABLE IF EXISTS {tn};
+CREATE TABLE {tn}
+(id serial PRIMARY KEY,
+expID text,
+biosample_term_name text,
+biosample_type text,
+biosample_summary text
+    ) """.format(tn = tableName))
+
+    outF = StringIO.StringIO()
+
+    for expID in expIDs:
+        exp = Exp.fromJsonFile(expID)
+        outF.write('\t'.join([expID,
+                              exp.biosample_term_name,
+                              exp.biosample_type,
+                              exp.getExpJson()["biosample_summary"]
+                              ]) + '\n')
+    outF.seek(0)
+
+    cols = ["expID", "biosample_term_name", "biosample_type",
+            "biosample_summary"]
+    curs.copy_from(outF, tableName, '\t', columns = cols)
+    printt("\tok", curs.rowcount)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -81,8 +125,10 @@ def main():
     for assembly in assemblies:
         print('***********', assembly)
         with getcursor(DBCONN, "dropTables") as curs:
-            doImport(curs, assembly)
-            doIndex(curs, assembly)
+            if 0:
+                doImport(curs, assembly)
+                doIndex(curs, assembly)
+            metadata(curs, assembly)
 
     return 0
 
