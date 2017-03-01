ca180e3ff0152c699dc9d849f5d1bdb09f5374ee
Michael Purcaro <purcaro@gmail.com>
2017-03-01 17:38:15 -0500

    query tab skeleton

---
+++ website/ui/src/apps/index/components/tab_query.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import * as Actions from '../actions';
+import {tabPanelize} from '../../../common/utility'
+
+class TabQuery extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return (tabPanelize(
+            <div>
+
+	    </div>));
+    }
+}
+
+const mapStateToProps = (state) => ({ ...state });
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(Actions, dispatch)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(TabQuery);
--- website/ui/src/apps/index/config/maintabs_config.js
+++ website/ui/src/apps/index/config/maintabs_config.js
@@ -3,6 +3,7 @@ import React from 'react'
 import TabMain from '../components/tab_main'
 import TabAbout from '../components/tab_about'
 import TabTutorial from '../components/tab_tutorial'
+import TabQuery from '../components/tab_query'
 
 import loading from '../../../common/components/loading'
 
@@ -11,7 +12,8 @@ import * as Render from '../../../common/renders'
 const MainTabConfig = () => {
     return {main : {title: "Overview", visible: true, f: TabMain},
             about : {title: "About", visible: true, f: TabAbout},
-            tutorial : {title: "Tutorial", visible: true, f: TabTutorial}
+            tutorial : {title: "Tutorial", visible: true, f: TabTutorial},
+            query: {title: "Query Results", visible: false, f: TabQuery}
     }
 }
 
