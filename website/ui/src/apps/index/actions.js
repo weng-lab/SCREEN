2f944616f8b145ff700f4ebc5cb46e1bc0f69cdc
Michael Purcaro <purcaro@gmail.com>
2017-03-01 17:29:28 -0500

    tabs fully working

--- website/ui/src/apps/index/actions.js
+++ website/ui/src/apps/index/actions.js
@@ -1,2 +1,5 @@
 export const SET_ERR = 'SET_ERR'
 export const setEr = (err) => ({ type: SET_ERR, err });
+
+export const SET_MAIN_TAB = 'SET_MAIN_TAB'
+export const setMainTab = (name) => ({ type: SET_MAIN_TAB, name });
