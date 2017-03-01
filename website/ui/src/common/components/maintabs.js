e5ba2c27bcf45f000c84838ba358bdf14645c171
Michael Purcaro <purcaro@gmail.com>
2017-03-01 17:27:53 -0500

    allow overrides for maintab classes
    Former-commit-id: e1fb2b3fe284a2c3311065f84e38e1b0bba6823b

--- website/ui/src/common/components/maintabs.js
+++ website/ui/src/common/components/maintabs.js
@@ -3,38 +3,44 @@ class MainTabs extends React.Component {
         const makeTabTitle = (key, tab) => {
             if(!tab.visible){ return (<div />) }
             let active = key == this.props.maintabs_active;
-	    return (<li className={active ? "active" : ""}
+	    return (
+                <li className={active ? "active" : ""}
                     key={"tab_" + key}
 	            style={{display: (tab.visible ? "list-item" : "none") }}
 	            onClick={ () => { this.props.actions.setMainTab(key) } }>
                     <a data-toggle="tab">{tab.title}</a>
-                    </li>);
+                </li>);
         }
 
         const makeTab = (key, tab) => {
             if(!tab.visible){ return (<div />) }
             let active = key == this.props.maintabs_active;
-            return (<div
+            return (
+                <div
                     className={active ? "tab-pane active" : "tab-pane"}
                     key={"tab_" + key}
                     id={"tab_main_" + key}
                     key={"tcontent_" + key}>
 		    {React.createElement(tab.f, this.props)}
-		    </div>);
+		</div>);
         }
 
         let tabs = this.props.maintabs;
-        return (<div id="exTab1" className="container">
+        let mainDivId = this.props.mainDivId || "exTab1";
+        let mainDivClass = this.props.mainDivClass || "container";
+        let tabUlClass = this.props.tabUlClass || "nav-tabs";
+        return (
+            <div id={mainDivId} className={mainDivClass}>
 
-	        <ul className="nav nav-tabs">
-	        {Object.keys(tabs).map((key) => ( makeTabTitle(key, tabs[key]) ))}
-                </ul>
+	    <ul className={"nav " + tabUlClass}>
+	    {Object.keys(tabs).map((key) => ( makeTabTitle(key, tabs[key]) ))}
+            </ul>
 
-	        <div className="tab-content clearfix">
-	        {Object.keys(tabs).map((key) => ( makeTab(key, tabs[key]) ))}
-	        </div>
+	    <div className="tab-content clearfix">
+	    {Object.keys(tabs).map((key) => ( makeTab(key, tabs[key]) ))}
+	    </div>
 
-                </div>);
+            </div>);
     }
 }
 export default MainTabs;
