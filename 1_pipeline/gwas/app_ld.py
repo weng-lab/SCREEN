2edc7601a79bcc465423ae2880208060366cd4f9
Michael Purcaro <purcaro@gmail.com>
2017-03-01 14:48:30 -0500

    little ld server

---
+++ 1_pipeline/gwas/app_ld.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+import cherrypy, jinja2, os, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../metadata/utils'))
+from templates import Templates
+
+class SnpApp():
+    def __init__(self, args, viewDir, staticDir, ps):
+        self.templates = Templates(viewDir, staticDir)
+        self.ps = ps
+
+    @cherrypy.expose
+    def snp_ld(self, *args, **kwargs):
+        return []
+
+    @cherrypy.expose
+    def snp_coord(self, *args, **kwargs):
+        return []
