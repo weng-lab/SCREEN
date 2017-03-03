845f63060af8a90fffa112fc1b6f6d86e1d23d39
Michael Purcaro <purcaro@gmail.com>
2017-03-03 18:43:23 -0500

    normalize file names

---
+++ 1_pipeline/02_build_cre/src/paths.cpp
@@ -0,0 +1,11 @@
+#include "paths.hpp"
+
+namespace bib {
+
+  const std::string FileNames::AllGenes = "all-genes.bed";
+  const std::string FileNames::PcGenes = "pc-genes.bed";
+  const std::string FileNames::Tads = "tads.txt";
+  const std::string FileNames::cREs = "cREs.bed";
+  const std::string FileNames::EnsembleToID = "ensebleToID.txt";
+  
+} // namespace bib
--- 1_pipeline/02_build_cre/src/splitter.hpp
+++ 1_pipeline/02_build_cre/src/splitter.hpp
@@ -1,133 +1,142 @@
 #pragma once
 
-class Splitter {
-    const bfs::path d_;
+#include "paths.hpp"
+
+namespace bib {
+
+  class Splitter {
+    const bfs::path raw_;
+    const bfs::path extras_;
     const std::vector<std::string>& chroms_;
     std::unordered_map<std::string, std::string> mpToChr_;
-
-public:
-    Splitter(const bfs::path d,
-             const std::vector<std::string>& chroms)
-        : d_(d)
-        , chroms_(chroms)
+  
+  public:
+    Splitter(const bfs::path raw, const bfs::path extras,
+	     const std::vector<std::string>& chroms)
+      : raw_(raw)
+      , extras_(extras)
+      , chroms_(chroms)
     {
-        loadMasterPeaks();
+      loadMasterPeaks();
     }
 
     void loadMasterPeaks(){
-        bfs::path inFnp = d_ / "masterPeaks.bed";
+      bfs::path inFnp = raw_ / "cREs.bed";
 
-        std::cout << "loading peaks " << inFnp << std::endl;
-        auto lines = bib::files::readStrings(inFnp);
+      std::cout << "loading peaks " << inFnp << std::endl;
+      auto lines = bib::files::readStrings(inFnp);
 
-        mpToChr_.reserve(3000000);
-        for(const auto& p : lines){
-            auto toks = bib::str::split(p, '\t');
-            mpToChr_[toks[3]] = toks[0];
-        }
-        std::cout << "\tfound " << mpToChr_.size() << " peaks\n";
+      mpToChr_.reserve(3000000);
+      for(const auto& p : lines){
+	auto toks = bib::str::split(p, '\t');
+	mpToChr_[toks[3]] = toks[0];
+      }
+      std::cout << "\tfound " << mpToChr_.size() << " peaks\n";
     }
 
     void splitSignalFile(const bfs::path inFnp){
-        std::unordered_map<std::string, std::vector<std::string>> chromToLines;
-        std::cout << "loading peaks " << inFnp << std::endl;
-        auto lines = bib::files::readStrings(inFnp);
-        for(const auto& p : lines){
-            auto toks = bib::str::split(p, '\t');
-            try{
-                std::string chrom = mpToChr_.at(toks[0]);
-                chromToLines[chrom].push_back(p);
-            } catch(...){
-                std::cerr << "ERROR: missing '" << toks[0] << "' from " << inFnp << std::endl;
-            }
-        }
+      std::unordered_map<std::string, std::vector<std::string>> chromToLines;
+      std::cout << "loading peaks " << inFnp << std::endl;
+      auto lines = bib::files::readStrings(inFnp);
+      for(const auto& p : lines){
+	auto toks = bib::str::split(p, '\t');
+	try{
+	  std::string chrom = mpToChr_.at(toks[0]);
+	  chromToLines[chrom].push_back(p);
+	} catch(...){
+	  std::cerr << "ERROR: missing '" << toks[0] << "' from " << inFnp << std::endl;
+	}
+      }
 
-        for(const auto& kv : chromToLines){
-            const auto& chrom = kv.first;
-            if(!bib::in(chrom, chroms_)){
-                continue;
-            }
-            bfs::path outFnp = d_ / chrom / "signal-output" / inFnp.filename();
-            //std::cout << "about to write " << outFnp << std::endl;
-            bfs::create_directories(outFnp.parent_path());
-            bib::files::writeStrings(outFnp, kv.second);
-        }
-        std::cout << "finished " << inFnp << std::endl;
+      for(const auto& kv : chromToLines){
+	const auto& chrom = kv.first;
+	if(!bib::in(chrom, chroms_)){
+	  continue;
+	}
+	bfs::path outFnp = extras_ / chrom / "signal-output" / inFnp.filename();
+	//std::cout << "about to write " << outFnp << std::endl;
+	bfs::create_directories(outFnp.parent_path());
+	bib::files::writeStrings(outFnp, kv.second);
+      }
+      std::cout << "finished " << inFnp << std::endl;
     }
 
     void splitBedFile(const bfs::path inFnp){
-        std::unordered_map<std::string, std::vector<std::string>> chromToLines;
-        std::cout << "loading bed " << inFnp << std::endl;
-        auto lines = bib::files::readStrings(inFnp);
-        for(const auto& p : lines){
-            auto toks = bib::str::split(p, '\t');
-            std::string chrom = toks[0];
-            chromToLines[chrom].push_back(p);
-        }
+      std::unordered_map<std::string, std::vector<std::string>> chromToLines;
+      std::cout << "loading bed " << inFnp << std::endl;
+      auto lines = bib::files::readStrings(inFnp);
+      for(const auto& p : lines){
+	auto toks = bib::str::split(p, '\t');
+	std::string chrom = toks[0];
+	chromToLines[chrom].push_back(p);
+      }
 
-        for(const auto& kv : chromToLines){
-            const auto& chrom = kv.first;
-            if(!bib::in(chrom, chroms_)){
-                continue;
-            }
-            bfs::path outFnp = d_ / chrom / inFnp.filename();
-            std::cout << "about to write " << outFnp << std::endl;
-            bfs::create_directories(outFnp.parent_path());
-            bib::files::writeStrings(outFnp, kv.second);
-        }
+      for(const auto& kv : chromToLines){
+	const auto& chrom = kv.first;
+	if(!bib::in(chrom, chroms_)){
+	  continue;
+	}
+	bfs::path outFnp = extras_ / chrom / inFnp.filename();
+	std::cout << "about to write " << outFnp << std::endl;
+	bfs::create_directories(outFnp.parent_path());
+	bib::files::writeStrings(outFnp, kv.second);
+      }
     }
 
     void splitTAD(const bfs::path inFnp){
-        std::unordered_map<std::string, std::vector<std::string>> chromToLines;
-        std::cout << "loading TAD " << inFnp << std::endl;
-        auto lines = bib::files::readStrings(inFnp);
-        for(const auto& p : lines){
-            auto toks = bib::str::split(p, '\t');
-            bib::string::rtrim(toks[0]);
-            bib::string::rtrim(toks[1]);
-            try{
-                std::string chrom = mpToChr_.at(toks[0]);
-                chromToLines[chrom].push_back(toks[0] + '\t' + toks[1]);
-            } catch(...){
-                std::cerr << "ERROR: missing '" << toks[0] << "' from " << inFnp << std::endl;
-            }
-        }
+      std::unordered_map<std::string, std::vector<std::string>> chromToLines;
+      std::cout << "loading TAD " << inFnp << std::endl;
+      auto lines = bib::files::readStrings(inFnp);
+      for(const auto& p : lines){
+	auto toks = bib::str::split(p, '\t');
+	bib::string::rtrim(toks[0]);
+	bib::string::rtrim(toks[1]);
+	try{
+	  std::string chrom = mpToChr_.at(toks[0]);
+	  chromToLines[chrom].push_back(toks[0] + '\t' + toks[1]);
+	} catch(...){
+	  std::cerr << "ERROR: missing '" << toks[0] << "' from " << inFnp << std::endl;
+	}
+      }
 
-        for(const auto& kv : chromToLines){
-            const auto& chrom = kv.first;
-            if(!bib::in(chrom, chroms_)){
-                continue;
-            }
-            bfs::path outFnp = d_ / chrom / inFnp.filename();
-            std::cout << "about to write " << outFnp << std::endl;
-            bfs::create_directories(outFnp.parent_path());
-            bib::files::writeStrings(outFnp, kv.second);
-        }
+      for(const auto& kv : chromToLines){
+	const auto& chrom = kv.first;
+	if(!bib::in(chrom, chroms_)){
+	  continue;
+	}
+	bfs::path outFnp = extras_ / chrom / inFnp.filename();
+	std::cout << "about to write " << outFnp << std::endl;
+	bfs::create_directories(outFnp.parent_path());
+	bib::files::writeStrings(outFnp, kv.second);
+      }
     }
 
     void run(){
-        zi::task_manager::simple tm(ZiARG_j);
-        tm.start();
+      zi::task_manager::simple tm(ZiARG_j);
+      tm.start();
 
-        if("hg19" == ZiARG_assembly){
-            bfs::path inFnp = d_ / "TADs.txt";
-            tm.insert(zi::run_fn(zi::bind(&Splitter::splitTAD,
-                                          this, inFnp)));
-        }
+      if("hg19" == ZiARG_assembly){
+	bfs::path inFnp = raw_ / FileNames::Tads;
+	tm.insert(zi::run_fn(zi::bind(&Splitter::splitTAD,
+				      this, inFnp)));
+      }
 
-        for(const auto& fn : {"all_cre_genes.bed.gz", "pc_cre_genes.bed.gz", "masterPeaks.bed"}){
-            bfs::path inFnp = d_ / fn;
-            tm.insert(zi::run_fn(zi::bind(&Splitter::splitBedFile,
-                                          this, inFnp)));
-        }
+      for(const auto& fn : {FileNames::AllGenes,
+	    FileNames::PcGenes, FileNames::cREs}){
+	bfs::path inFnp = raw_ / fn;
+	tm.insert(zi::run_fn(zi::bind(&Splitter::splitBedFile,
+				      this, inFnp)));
+      }
 
-        auto dir = bib::files::dir(d_ / "signal-output");
-        const std::vector<bfs::path> fnps(dir.begin(), dir.end());
-        std::cout << "found " << fnps.size() << " signal files\n";
-        for(const auto& fnp : fnps){
-            tm.insert(zi::run_fn(zi::bind(&Splitter::splitSignalFile,
-                                          this, fnp)));
-        }
-        tm.join();
+      auto dir = bib::files::dir(raw_ / "signal-output");
+      const std::vector<bfs::path> fnps(dir.begin(), dir.end());
+      std::cout << "found " << fnps.size() << " signal files\n";
+      for(const auto& fnp : fnps){
+	tm.insert(zi::run_fn(zi::bind(&Splitter::splitSignalFile,
+				      this, fnp)));
+      }
+      tm.join();
     }
-};
+  };
+
+} // namespace bib
