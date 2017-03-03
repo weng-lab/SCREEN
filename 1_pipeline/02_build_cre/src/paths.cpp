bac2570714c532ab9254a6c0ba780c8443837f1a
Michael Purcaro <purcaro@gmail.com>
2017-03-03 18:53:01 -0500

    file name fixes

--- 1_pipeline/02_build_cre/src/paths.cpp
+++ 1_pipeline/02_build_cre/src/paths.cpp
@@ -2,8 +2,8 @@
 
 namespace bib {
 
-  const std::string FileNames::AllGenes = "all-genes.bed";
-  const std::string FileNames::PcGenes = "pc-genes.bed";
+  const std::string FileNames::AllGenes = "all-genes.txt";
+  const std::string FileNames::PcGenes = "pc-genes.txt";
   const std::string FileNames::Tads = "tads.txt";
   const std::string FileNames::cREs = "cREs.bed";
   const std::string FileNames::EnsembleToID = "ensebleToID.txt";
