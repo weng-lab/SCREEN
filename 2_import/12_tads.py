833bcc0ea54d815835de268762bed4bad3d884df
Michael Purcaro <purcaro@gmail.com>
2017-03-04 20:41:48 -0500

    fix TAD coords
    Former-commit-id: 3ce080d54226f2d5769ecd5e10ed79b0073d20f9

--- 2_import/12_tads.py
+++ 2_import/12_tads.py
@@ -12,51 +12,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils/')
 from utils import Utils, printt
 from db_utils import getcursor, makeIndex, makeIndexRev, makeIndexArr, makeIndexIntRange
 
-class ImportTADs:
-    def __init__(self, curs, assembly):
-        self.curs = curs
-        self.assembly = assembly
-        self.tableName = assembly + "_" + "tads"
-
-    def setupTable(self):
-        printt("dropping and creating table", self.tableName)
-        self.curs.execute("""
- DROP TABLE IF EXISTS {tableName};
- CREATE TABLE {tableName}(
- id serial PRIMARY KEY,
- accession VARCHAR(20),
- mpName text,
- tadName text
-);
-    """.format(tableName = self.tableName))
-
-    def run(self):
-        fnp = paths.path(self.assembly, "hg19-TAD-Accessions.txt.gz")
-
-        printt("reading", fnp)
-        with gzip.open(fnp) as f:
-            rows = [line.rstrip().split('\t') for line in f]
-        f = StringIO.StringIO()
-        for r in rows:
-            f.write('\t'.join(r) + '\n')
-        f.seek(0)
-
-        self.setupTable()
-        self.curs.copy_from(f, self.tableName, '\t',
-                          columns=("mpName", "tadName"))
-        printt("copied in TADs", self.curs.rowcount)
-
-        printt("updaing accessions")
-        self.curs.execute("""
-UPDATE {tadTableName} as tads
-SET accession = cre.accession
-FROM {tn} as cre
-where tads.mpname = cre.mpname
-""".format(tadTableName = "hg19_tads", tn = "hg19_cre"))
-
-    def index(self):
-        makeIndex(self.curs, self.tableName, ["accession", "tadName"])
-
 class ImportTADinfo:
     def __init__(self, curs, assembly):
         self.curs = curs
@@ -77,7 +32,7 @@ tadName text
     """.format(tableName = self.tableName))
 
     def run(self):
-        fnp = paths.path(self.assembly, "TADs.bed.gz")
+        fnp = paths.path(self.assembly, "extras", "TADs.bed.gz")
 
         printt("reading", fnp)
         with gzip.open(fnp) as f:
@@ -111,11 +66,6 @@ def main():
             iti = ImportTADinfo(curs, assembly)
             iti.run()
             iti.index()
-
-            ipi = ImportTADs(curs, assembly)
-            ipi.run()
-            ipi.index()
-
 
 if __name__ == '__main__':
     main()
