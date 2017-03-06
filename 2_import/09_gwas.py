e3caeec980a14325c26b11d97e4846c48adcba96
Michael Purcaro <purcaro@gmail.com>
2017-03-05 22:50:30 -0500

    convert gwas to class
    Former-commit-id: 1e06e73506c50438735b571295cc835d8468975b

--- 2_import/09_gwas.py
+++ 2_import/09_gwas.py
@@ -7,6 +7,7 @@ import math
 sys.path.append(os.path.join(os.path.dirname(__file__), '../common/'))
 from dbconnect import db_connect
 from constants import paths
+from config import Config
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils/'))
 from exp import Exp
@@ -14,263 +15,275 @@ from utils import Utils, printt, printWroteNumLines, cat
 from db_utils import getcursor, makeIndex, makeIndexRev, makeIndexArr, makeIndexIntRange
 from files_and_paths import Dirs
 
-def setupGWAS(curs, tableName):
-    # chr1    62963737        62963737        rs1002687       rs11207995      0.85    25961943-2      Cholesterol     25961943        Surakka"""
-    #                                         snpItself       taggedSNP       r2      unqiueLDblock(pubmed-num)       trait   pubmed  firstAuthor""
-    curs.execute("""
-DROP TABLE IF EXISTS {tableName};
-CREATE TABLE {tableName}(
-id serial PRIMARY KEY,
-chrom text,
-start integer,
-stop integer,
-snp text,
-taggedSNP text,
-r2 real,
-ldblock text,
-trait text,
-pubmed text,
-author text,
-authorPubmedTrait text
-);
-""".format(tableName = tableName))
+class ImportGwas:
+    def __init__(self, curs, assembly):
+        self.curs = curs
+        self.assembly = assembly
+        self.tableNameGwas = assembly + "_gwas"
+        self.tableNameEnrichment = assembly + "_gwas_enrichment"
+        self.tableNameDatasets = assembly + "_datasets"
+        self.tableNameStudies = assembly + "_gwas_studies"
+        self.tableNameOverlap = assembly + "_gwas_overlap"
+                
+    def setupGWAS(self):
+        # chr1    62963737        62963737        rs1002687       rs11207995      0.85    25961943-2      Cholesterol     25961943        Surakka"""
+        #                                         snpItself       taggedSNP       r2      unqiueLDblock(pubmed-num)       trait   pubmed  firstAuthor""
+        printt("drop/create", self.tableNameGwas)
+        
+        self.curs.execute("""
+        DROP TABLE IF EXISTS {tn};
+        CREATE TABLE {tn}(
+        id serial PRIMARY KEY,
+        chrom text,
+        start integer,
+        stop integer,
+        snp text,
+        taggedSNP text,
+        r2 real,
+        ldblock text,
+        trait text,
+        pubmed text,
+        author text,
+        authorPubmedTrait text
+        );
+        """.format(tn = self.tableNameGwas))
 
-def _gwas(curs, fnp):
-    printt("******************* GWAS")
-    printt("reading", fnp)
-    with open(fnp) as f:
-        rows = [r.rstrip().split('\t') for r in f if r]
+    def _gwas(self, fnp):
+        printt("******************* GWAS")
+        printt("reading", fnp)
+        with open(fnp) as f:
+            rows = [r.rstrip().split('\t') for r in f if r]
 
-    tableName = "hg19_gwas"
-    printt("drop/create", tableName)
-    setupGWAS(curs, tableName)
+        self.setupGWAS()
 
-    printt("rewrite rows")
-    outF = StringIO.StringIO()
-    for r in rows:
-        if 'Lead' == r[4]:
-            r[4] = r[3]
-        if '*' == r[5]:
-            r[5] = "-1"
-        r[2] = str(int(r[2]) + 1)
-        if '*' in '\t'.join(r):
-            print(r)
-            raise Exception("invalid line")
-        outF.write('\t'.join(r) + '\n')
-    print("example", '\t'.join(r))
-    outF.seek(0)
+        printt("rewrite rows")
+        outF = StringIO.StringIO()
+        for r in rows:
+            if 'Lead' == r[4]:
+                r[4] = r[3]
+            if '*' == r[5]:
+                r[5] = "-1"
+            r[2] = str(int(r[2]) + 1)
+            if '*' in '\t'.join(r):
+                print(r)
+                raise Exception("invalid line")
+            outF.write('\t'.join(r) + '\n')
+        print("example", '\t'.join(r))
+        outF.seek(0)
 
-    cols = "chrom start stop snp taggedSNP r2 ldblock trait pubmed author authorPubmedTrait".split(' ')
-    curs.copy_from(outF, tableName, '\t', columns=cols)
-    print("copied in", curs.rowcount)
+        cols = "chrom start stop snp taggedSNP r2 ldblock trait pubmed author authorPubmedTrait".split(' ')
+        self.curs.copy_from(outF, self.tableNameGwas, '\t', columns=cols)
+        print("copied in", self.curs.rowcount)
 
-    makeIndex(curs, tableName, ["chrom", "authorPubmedTrait", "ldblock"])
-    makeIndexIntRange(curs, tableName, ["start", "stop"])
+        makeIndex(self.curs, self.tableNameGwas,
+                  ["chrom", "authorPubmedTrait", "ldblock"])
+        makeIndexIntRange(self.curs, self.tableNameGwas, ["start", "stop"])
 
-def setupEnrichment(curs, tableName, fields):
-    curs.execute("""
-DROP TABLE IF EXISTS {tableName};
+    def _setupEnrichment(self, fields):
+        printt("drop/create", self.tableNameEnrichment)
+        self.curs.execute("""
+        DROP TABLE IF EXISTS {tn};
 
-CREATE TABLE {tableName}(
-id serial PRIMARY KEY,
-expID text,
-cellTypeName text,
-biosample_summary text,
-{fields}
-);
-""".format(tableName = tableName,
-           fields = ','.join([r + " real" for r in fields])))
+        CREATE TABLE {tn}(
+        id serial PRIMARY KEY,
+        expID text,
+        cellTypeName text,
+        biosample_summary text,
+        {fields}
+        );
+        """.format(tn = self.tableNameEnrichment,
+                   fields = ','.join([r + " real" for r in fields])))
 
-def _enrichment(curs, fnp):
-    printt("******************* GWAS enrichment")
-    printt("reading", fnp)
-    with open(fnp) as f:
-        header = f.readline().rstrip('\n').split('\t')
-        rows = [r.rstrip('\n').split('\t') for r in f if r]
+    def _enrichment(self, fnp):
+        printt("******************* GWAS enrichment")
+        printt("reading", fnp)
+        with open(fnp) as f:
+            header = f.readline().rstrip('\n').split('\t')
+            rows = [r.rstrip('\n').split('\t') for r in f if r]
 
-    tableName = "hg19_gwas_enrichment"
-    fields = [f.replace('-', '_') for f in header[2:]]
-    printt("drop/create", tableName)
-    setupEnrichment(curs, tableName, fields)
+        fields = [f.replace('-', '_') for f in header[2:]]
+        self._setupEnrichment(fields)
 
-    printt("rewrite rows")
-    outF = StringIO.StringIO()
-    for r in rows:
-        for idx in xrange(3, len(r)):
-            r[idx] = str(round(-1.0 * math.log10(float(r[idx])), 2))
-        outF.write('\t'.join(r) + '\n')
-    outF.seek(0)
-    cols = ["expID", "cellTypeName"] + fields
-    print(cols)
+        printt("rewrite rows")
+        outF = StringIO.StringIO()
+        for r in rows:
+            for idx in xrange(3, len(r)):
+                r[idx] = str(round(-1.0 * math.log10(float(r[idx])), 2))
+            outF.write('\t'.join(r) + '\n')
+        outF.seek(0)
+        cols = ["expID", "cellTypeName"] + fields
+        print(cols)
 
-    printt("import to db")
-    curs.copy_from(outF, tableName, '\t', columns=cols)
-    printt("\tcopied in", curs.rowcount)
+        printt("import to db")
+        self.curs.copy_from(outF, self.tableNameEnrichment, '\t', columns=cols)
+        printt("\tcopied in", self.curs.rowcount)
 
-    curs.execute("""
-UPDATE hg19_gwas_enrichment as ge
-set cellTypeName = d.cellTypeName,
-biosample_summary = d.biosample_summary
-from hg19_datasets as d
-where ge.expID = d.expID""")
-    printt("updated", curs.rowcount)
+        self.curs.execute("""
+    UPDATE {tne} as ge
+    set cellTypeName = d.cellTypeName,
+    biosample_summary = d.biosample_summary
+    from {tnd} as d
+    where ge.expID = d.expID
+        """.format(tne = self.tableNameEnrichment, tnd = self.tableNameDatasets))
+        printt("updated", self.curs.rowcount)
+        
+    def _setupStudies(self):
+        printt("drop/create", self.tableNameStudies)
+        self.curs.execute("""
+        DROP TABLE IF EXISTS {tn};
 
-def setupStudies(curs, tableName):
-    curs.execute("""
-DROP TABLE IF EXISTS {tableName};
+        CREATE TABLE {tn}(
+        id serial PRIMARY KEY,
+        author text,
+        pubmed text,
+        trait text,
+        authorPubmedTrait text,
+        numLDblocks integer
+        );
+        """.format(tn = self.tableNameStudies))
 
-CREATE TABLE {tableName}(
-id serial PRIMARY KEY,
-author text,
-pubmed text,
-trait text,
-authorPubmedTrait text,
-numLDblocks integer
-);
-""".format(tableName = tableName))
+    def _studies(self, fnp):
+        printt("******************* GWAS studies")
+        printt("reading", fnp)
+        with open(fnp) as f:
+            header = f.readline().rstrip('\n').split('\t')
+        
+        self._setupStudies()
 
-def _studies(curs, fnp):
-    printt("******************* GWAS studies")
-    printt("reading", fnp)
-    with open(fnp) as f:
-        header = f.readline().rstrip('\n').split('\t')
+        printt("import to db")
+        self.curs.execute("""
+    INSERT INTO {tn} (authorpubmedtrait,  author, pubmed, trait, numLDblocks)
+    SELECT DISTINCT(authorpubmedtrait), author, pubmed, trait, COUNT(DISTINCT(ldblock))
+    FROM {gwasTn}
+    GROUP BY authorpubmedtrait, author, pubmed, trait
+     """.format(tn = self.tableNameStudies,
+                gwasTn = self.tableNameGwas))
+        printt("inserted", self.curs.rowcount)
 
-    tableName = "hg19_gwas_studies"
-    printt("drop/create", tableName)
-    setupStudies(curs, tableName)
+        self.curs.execute("""
+    SELECT authorpubmedtrait
+    FROM {tn}
+    ORDER BY authorpubmedtrait
+     """.format(tn = self.tableNameStudies))
+        return [r[0] for r in self.curs.fetchall()]
 
-    printt("import to db")
-    curs.execute("""
-INSERT INTO {tn} (authorpubmedtrait,  author, pubmed, trait, numLDblocks)
-SELECT DISTINCT(authorpubmedtrait), author, pubmed, trait, COUNT(DISTINCT(ldblock))
-FROM {gwasTn}
-GROUP BY authorpubmedtrait, author, pubmed, trait
- """.format(tn = tableName, gwasTn = "hg19_gwas"))
-    printt("inserted", curs.rowcount)
+    def processGwasBed(self, origBedFnp, bedFnp):
+        printt("reading", origBedFnp)
+        with open(origBedFnp) as f:
+            rows = [r.rstrip().split('\t') for r in f if r]
+        printt("split rows")
+        split = []
+        for r in rows:
+            if ',' not in r[4]:
+                split.append(r)
+                continue
+            taggedSNPs = r[4].split(',')
+            r2s = r[5].split(',')
+            a = list(r)
+            b = list(r)
+            a[4] = taggedSNPs[0]
+            b[4] = taggedSNPs[1]
+            a[5] = r2s[0]
+            b[5] = r2s[1]
+            split.append(a)
+            split.append(b)
+        printt("split rows", len(rows), "to", len(split))
 
-    curs.execute("""
-SELECT authorpubmedtrait
-FROM {tn}
-ORDER BY authorpubmedtrait
- """.format(tn = tableName))
-    return [r[0] for r in curs.fetchall()]
-
-def processGwasBed(origBedFnp, bedFnp):
-    printt("reading", origBedFnp)
-    with open(origBedFnp) as f:
-        rows = [r.rstrip().split('\t') for r in f if r]
-    printt("split rows")
-    split = []
-    for r in rows:
-        if ',' not in r[4]:
-            split.append(r)
-            continue
-        taggedSNPs = r[4].split(',')
-        r2s = r[5].split(',')
-        a = list(r)
-        b = list(r)
-        a[4] = taggedSNPs[0]
-        b[4] = taggedSNPs[1]
-        a[5] = r2s[0]
-        b[5] = r2s[1]
-        split.append(a)
-        split.append(b)
-    printt("split rows", len(rows), "to", len(split))
-
-    printt("adding authorPubmedTrait")
-    for r in split:
-        authorPubmedTrait = r[-1]
-        r[-1] = authorPubmedTrait.replace('-', '_')
-
-    print("***********", split[0][-1])
-
-    printt("writing", bedFnp)
-    with open(bedFnp, 'w') as f:
+        printt("adding authorPubmedTrait")
         for r in split:
-            f.write('\t'.join(r) + '\n')
-    Utils.sortFile(bedFnp)
-    printWroteNumLines(bedFnp)
+            authorPubmedTrait = r[-1]
+            r[-1] = authorPubmedTrait.replace('-', '_')
 
-def setupOverlap(curs, tableName):
-    curs.execute("""
-DROP TABLE IF EXISTS {tableName};
+        print("***********", split[0][-1])
 
-CREATE TABLE {tableName}(
-id serial PRIMARY KEY,
-authorPubmedTrait text,
-accession text,
-snp text
-);
-""".format(tableName = tableName))
+        printt("writing", bedFnp)
+        with open(bedFnp, 'w') as f:
+            for r in split:
+                f.write('\t'.join(r) + '\n')
+        Utils.sortFile(bedFnp)
+        printWroteNumLines(bedFnp)
 
-def _overlap(curs, bedFnp):
-    printt("******************* GWAS overlap")
-    tableName = "hg19_gwas_overlap"
-    setupOverlap(curs, tableName)
+    def _setupOverlap(self):
+        self.curs.execute("""
+        DROP TABLE IF EXISTS {tn};
 
-    cresFnp = paths.path("hg19", "extras", "cREs.sorted.bed")
-    if not os.path.exists(cresFnp):
-        Utils.sortFile(paths.path("hg19", "raw", "cREs.bed"),
-                       cresFnp)
+        CREATE TABLE {tn}(
+        id serial PRIMARY KEY,
+        authorPubmedTrait text,
+        accession text,
+        snp text
+        );
+        """.format(tn = self.tableNameOverlap))
 
-    printt("running bedtools intersect...")
-    cmds = [cat(bedFnp),
-            '|', "cut -f -4,11-",
-            '|', "bedtools intersect",
-            "-a", "-",
-            "-b", cresFnp,
-            "-wo",
-            "-sorted"]
-    snpsIntersecting = Utils.runCmds(cmds)
-    print("example", snpsIntersecting[0].rstrip('\n').split('\t'))
+    def _overlap(self, bedFnp):
+        printt("******************* GWAS overlap")
+        self._setupOverlap()
 
-    printt("rewriting...")
-    outF = StringIO.StringIO()
-    for r in snpsIntersecting:
-        toks = r.rstrip('\n').split('\t')
-        snp = toks[3]
-        authorPubmedTrait = toks[4].replace('-', '_')
-        accession = toks[9]
+        cresFnp = paths.path(self.assembly, "extras", "cREs.sorted.bed")
+        if not os.path.exists(cresFnp):
+            Utils.sortFile(paths.path(self.assembly, "raw", "cREs.bed"),
+                           cresFnp)
 
-        if '_' not in authorPubmedTrait:
-            print(r)
-            print(toks)
-            raise Exception("bad authorPubmedTrait?")
-        if not snp.startswith("rs"):
-            print(r)
-            print(toks)
-            raise Exception("bad rs?")
-        if not accession.startswith("EH3"):
-            print(r)
-            print(toks)
-            raise Exception("bad line?")
-        outF.write('\t'.join([authorPubmedTrait, accession, snp]) + '\n')
-    print("example", '\t'.join([authorPubmedTrait, accession, snp]))
-    outF.seek(0)
+        printt("running bedtools intersect...")
+        cmds = [cat(bedFnp),
+                '|', "cut -f -4,11-",
+                '|', "bedtools intersect",
+                "-a", "-",
+                "-b", cresFnp,
+                "-wo",
+                "-sorted"]
+        snpsIntersecting = Utils.runCmds(cmds)
+        print("example", snpsIntersecting[0].rstrip('\n').split('\t'))
 
-    printt("copying into DB...")
-    cols = "authorPubmedTrait accession snp".split(' ')
-    curs.copy_from(outF, tableName, '\t', columns=cols)
-    printt("copied in", curs.rowcount)
+        printt("rewriting...")
+        outF = StringIO.StringIO()
+        for r in snpsIntersecting:
+            toks = r.rstrip('\n').split('\t')
+            snp = toks[3]
+            authorPubmedTrait = toks[4].replace('-', '_')
+            accession = toks[9]
 
-    makeIndex(curs, tableName, ["authorPubmedTrait"])
+            if '_' not in authorPubmedTrait:
+                print(r)
+                print(toks)
+                raise Exception("bad authorPubmedTrait?")
+            if not snp.startswith("rs"):
+                print(r)
+                print(toks)
+                raise Exception("bad rs?")
+            if not accession.startswith("EH3"):
+                print(r)
+                print(toks)
+                raise Exception("bad line?")
+            outF.write('\t'.join([authorPubmedTrait, accession, snp]) + '\n')
+        print("example", '\t'.join([authorPubmedTrait, accession, snp]))
+        outF.seek(0)
 
-def setupAll(curs):
-     dataF = os.path.join(paths.v4d, "GWAS", "old")
+        printt("copying into DB...")
+        cols = "authorPubmedTrait accession snp".split(' ')
+        self.curs.copy_from(outF, self.tableNameOverlap, '\t', columns=cols)
+        printt("copied in", self.curs.rowcount)
 
-     origBedFnp = os.path.join(dataF, "GWAS.v2.bed")
-     bedFnp = os.path.join(dataF, "GWAS.v2.sorted.bed")
-     processGwasBed(origBedFnp, bedFnp)
+        makeIndex(self.curs, self.tableNameOverlap, ["authorPubmedTrait"])
 
-     _gwas(curs, bedFnp)
-
-     enrichFnp = os.path.join(dataF, "GWAS.Enrichment.v2.Matrix.txt")
-     _enrichment(curs, enrichFnp)
-     _studies(curs, enrichFnp)
-     _overlap(curs, bedFnp)
+    def run(self):
+        dataF = paths.path(self.assembly, "gwas", "h3k27ac")
+        dataF = os.path.join(paths.v4d, "GWAS", "old")
+        
+        origBedFnp = os.path.join(dataF, "GWAS.v2.bed")
+        bedFnp = os.path.join(dataF, "GWAS.v2.sorted.bed")
+        self.processGwasBed(origBedFnp, bedFnp)
+        
+        self._gwas(bedFnp)
+        
+        enrichFnp = os.path.join(dataF, "GWAS.Enrichment.v2.Matrix.txt")
+        self._enrichment(enrichFnp)
+        self._studies(enrichFnp)
+        self._overlap(bedFnp)
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--assembly", type=str, default="")
     args = parser.parse_args()
     return args
 
@@ -279,8 +292,14 @@ def main():
 
     DBCONN = db_connect(os.path.realpath(__file__))
 
-    with getcursor(DBCONN, "main") as curs:
-        setupAll(curs)
+    assemblies = ["hg19"] #Config.assemblies
+    if args.assembly:
+        assemblies = [args.assembly]
 
+    for assembly in assemblies:
+        with getcursor(DBCONN, "main") as curs:
+            ig = ImportGwas(curs, assembly)
+            ig.run()
+            
 if __name__ == '__main__':
     main()
