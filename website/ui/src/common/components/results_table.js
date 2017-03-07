50231b055871cc8643fbaf7374ad1985732b9b14
Michael Purcaro <purcaro@gmail.com>
2017-03-06 19:32:17 -0500

    fix tooltip not disappearing
    Former-commit-id: 3863b8cd9f9068e7230735a5c480e8d2adc4fbef

--- website/ui/src/common/components/results_table.js
+++ website/ui/src/common/components/results_table.js
@@ -69,9 +69,13 @@ class ResultsTable extends React.Component {
 	    })
             .on('draw.dt', function () {
                 $('[data-toggle="tooltip"]').tooltip(
-                    {delay: { show: 100, hide: 3000 }
+                    {delay: { show: 100, hide: 3000 },
                     }
                 );
+                $('[data-toggle="tooltip"]').on('click', function(e){
+                    // nuke tooltip if content clicked
+                    $(this).tooltip('destroy');
+                });
             })
 	    .removeClass('display')
 	    .addClass('table table-condensed table-hover');
