edad2d623b55a8c48857fcf79dada2a8f18858ea
Michael Purcaro <purcaro@gmail.com>
2017-03-07 12:05:23 -0500

    revert change

--- website/ui/src/apps/search/components/minipeaks.js
+++ website/ui/src/apps/search/components/minipeaks.js
@@ -123,8 +123,8 @@ class MiniPeaks extends React.Component {
 			   render: renderMax(assay)});
 	    }
 	}
-	cols = cols.concat([{title: "", data: "expIDs",
-			     render: Render.dccLinkCtGroupExpIDs},
+	cols = cols.concat([{title: "", data: "expID",
+			     render: Render.dccLink},
 			    {title: "Tissue of origin", data: "tissue"},
 			    {title: "Cell Type", data: "biosample_type"},
 			    {title: "Biosample", data: "biosample_summary"}]);
