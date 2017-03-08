32ef5c77cc403607295f381f19af9c05fc41c2a6
Michael Purcaro <purcaro@gmail.com>
2017-03-08 16:02:12 -0500

    hide cell compartments if mm10

--- website/ui/src/apps/geneexp/components/facetboxen.js
+++ website/ui/src/apps/geneexp/components/facetboxen.js
@@ -48,7 +48,7 @@ class FacetBoxen extends React.Component {
         return (
             <div>
                 {bts(p)}
-                {cell_compartments(p)}
+                {"mm10" != GlobalAssembly && cell_compartments(p)}
             </div>);
     }
 
