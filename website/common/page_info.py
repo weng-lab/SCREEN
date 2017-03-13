3467fe7e92053289a686e295dd5cc2b15999061d
Michael Purcaro <purcaro@gmail.com>
2017-03-13 10:28:36 -0400

    add ribbon
    Former-commit-id: 1f7786af28726dd6277fdd8483e320a28adb8b79

--- website/common/page_info.py
+++ website/common/page_info.py
@@ -2,6 +2,7 @@ import sys, os, json, cherrypy
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../common/'))
 from constants import PageTitle
+from config import Config
 
 class PageInfoMain:
     def __init__(self, ps, cacheW):
@@ -14,6 +15,7 @@ class PageInfoMain:
         cssFnp = os.path.join(os.path.dirname(__file__),
                               "../static/css.css")
         return {"page": {"title" : PageTitle("")},
+                "Ribbon": Config.ribbon,
                 "Assembly" : None,
                 "userQueryErr" : userQueryErr,
                 "bundlets" : os.path.getmtime(bundleFnp),
