541581b63de699394bf30256291a0675dd1e651a
Michael Purcaro <purcaro@gmail.com>
2017-03-13 23:29:32 -0400

    memcache
    Former-commit-id: cd2937e2e9578c5cd68e5691003408063246114a

--- website/common/memcachew.py
+++ website/common/memcachew.py
@@ -12,14 +12,14 @@ from config import Config
 
 class MemCacheWrapper:
     def __init__(self, subnamespace):
-        self.namespace = Config.version + ':' + subnamespace + ':'
+        self.namespace = "screen" + Config.version + ':' + subnamespace + ':'
         self.mc = None
         try:
             import pylibmc
             self.mc = pylibmc.Client(["127.0.0.1"], binary=True,
                                 behaviors={"tcp_nodelay": True,
                                            "ketama": True})
-            print("using memcache...")
+            print("using memcache...", self.namespace)
         except:
             print("pylibmc not found....")
             pass
@@ -44,22 +44,18 @@ class MemCacheWrapper:
             except:
                 print("could not save", k)
 
-    def get(self, kRaw, quiet = False):
+    def get(self, kRaw):
         if self.mc:
             k = self._key(kRaw)
             if k in self.mc:
-                if not quiet:
-                    print("got", kRaw, "from cache")
                 return self.mc[k]
         return None
 
     def getOrSet(self, name, f, *args, **kwargs):
-        quiet = False
         kRaw = [name] + list(args) + list(kwargs.values())
-        print(kRaw)
         # example use
         if self.mc:
-            ret = self.get(kRaw, quiet)
+            ret = self.get(kRaw)
             if ret:
                 return ret
             data = f(*args, **kwargs)
