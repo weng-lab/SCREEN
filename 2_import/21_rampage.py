f1fbc1bd54ba33957308e9a7cbd03ede2506529e
Michael Purcaro <purcaro@gmail.com>
2017-03-14 20:30:54 -0400

    reversion from branch merge
    Former-commit-id: cd8198cc5a375f30db0c5b5c8b408bdb71ee6ea5

--- 2_import/21_rampage.py
+++ 2_import/21_rampage.py
@@ -70,7 +70,7 @@ maxVal real,
 {fields}
 );""".format(tn = tableName, fields = ','.join([f + " real" for f in fileIDs])))
 
-    curs.copy_from(outF, tableName, '\t', columns = header)
+    curs.copy_from(outF, tableName, '\t', columns = cols)
     printt("inserted", curs.rowcount)
 
     curs.execute("""
