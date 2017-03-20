db13c8065b8f57da8868cba77f17041f09b6016b
Michael Purcaro <purcaro@gmail.com>
2017-03-20 19:20:56 -0400

    refactoring; need to write more coverage tests
    Former-commit-id: eae0e2f63704e8e2e016967558d48edec0771aa5

--- website/common/pg_cre_table.py
+++ website/common/pg_cre_table.py
@@ -31,82 +31,42 @@ class PGcreTable(GetOrSetMemCache):
         self.ctmap = ctmap
         self.ctsTable = ctsTable
 
-    def _getAccessions(self, j, chrom, start, stop):
-        accs = j.get("accessions", [])
-        if not accs or 0 == len(accs):
-            return None, None
+        self.tableName = self.assembly + "_cre_all"
+        self.fields = [
+            "accession", "maxZ",
+            "cre.chrom", "cre.start",
+            "cre.stop - cre.start AS len",
+            "cre.gene_all_id", "cre.gene_pc_id",
+            "0::int as in_cart",
+            "cre.creGroup"]
+        self.whereClauses = []
 
-        fields = []
-        whereclauses = []
-
-        ct = j.get("cellType", None)
+    def _cts(self, ct):
         if ct in self.ctsTable:
-            fields.append("cre.creGroupsSpecific[%s] AS cts" %
+            self.fields.append("cre.creGroupsSpecific[%s] AS cts" %
                               self.ctsTable[ct])
         else:
-            fields.append("0::int AS cts")
+            self.fields.append("0::int AS cts")
 
-        if accs and len(accs) > 0:
-            if type(accs[0]) is dict:
-                accs = [x["value"] for x in accs if x["checked"]]
-            accs = filter(lambda x: isaccession(x), accs)
-            if accs:
-                accs = ["'%s'" % x.upper() for x in accs]
-                accsQuery = "accession IN (%s)" % ','.join(accs)
-                whereclauses.append("(%s)" % accsQuery)
+    def _buildWhereStatement(self, j, chrom, start, stop):
+        ct = j.get("cellType", None)
 
+        self._cts(ct)
         if ct:
-            for assay in [("dnase", "dnase"),
-                          ("promoter", "h3k4me3"),
-                          ("enhancer", "h3k27ac"),
-                          ("ctcf", "ctcf")]:
-                if ct not in self.ctmap[assay[0]]:
-                    fields.append("'' AS %s_zscore" % (assay[0]))
-                    continue
-                cti = self.ctmap[assay[0]][ct]
-                fields.append("cre.%s_zscores[%d] AS %s_zscore" % (assay[1], cti, assay[0]))
-
-                if "rank_%s_start" % assay[0] in j and "rank_%s_end" % assay[0] in j:
-                    _range = [j["rank_%s_start" % assay[0]] / 100.0,
-                              j["rank_%s_end" % assay[0]] / 100.0]
-                    minDefault = -10.0  # must match slider default
-                    maxDefault = 10.0   # must match slider default
-                    if isclose(_range[0], minDefault) and isclose(_range[1], maxDefault):
-                        continue # not actually filtering on zscore, yet...
-                    if not isclose(_range[0], minDefault):
-                        whereclauses.append("(%s)" %
-                                            "cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]))
-                    elif not isclose(_range[1], maxDefault):
-                        whereclauses.append("(%s)" %
-                                            "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1]))
-                    else:
-                        whereclauses.append("(%s)" % " and ".join(
-                                ["cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]),
-                                 "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1])] ))
+            self._ctSpecific(ct, j)
         else:
-            allmap = {"dnase": "dnase_max",
-                      "promoter": "h3k4me3_max",
-                      "enhancer": "h3k27ac_max",
-                      "ctcf": "ctcf_max" }
-            for x in ["dnase", "promoter", "enhancer", "ctcf"]:
-                if "rank_%s_start" % x in j and "rank_%s_end" in j:
-                    _range = [j["rank_%s_start" % x] / 100.0,
-                              j["rank_%s_end" % x] / 100.0]
-                    whereclauses.append("(%s)" % " and ".join(
-                        ["cre.%s >= %f" % (allmap[x], _range[0]),
-                         "cre.%s <= %f" % (allmap[x], _range[1]) ] ))
-                fields.append("cre.%s AS %s_zscore" % (allmap[x], x))
+            self._notCtSpecific(j)
+            
+        self._accessions(j)
+        self._where(chrom, start, stop)
 
-        whereclause = ""
-        if len(whereclauses) > 0:
-            whereclause = "WHERE " + " and ".join(whereclauses)
-        #print(whereclause)
-        return (fields, whereclause)
-
-    def _creTableWhereClause(self, j, chrom, start, stop):
-        fields = []
-        whereclauses = []
-
+        fields = ', '.join(self.fields)
+        ret = ""
+        if len(self.whereClauses) > 0:
+            ret = "WHERE " + " and ".join(self.whereClauses)
+        return fields, ret
+            
+    def creTable(self, j, chrom, start, stop):
         if 0:
             print(j, """TODO need more variables here:
         gene_all_start, gene_all_end,
@@ -118,90 +78,18 @@ class PGcreTable(GetOrSetMemCache):
             tfclause += " and peakintersections.tf ?| array(" + ",".join(["'%s'" % tf for tf in j["tfs"]]) + ")"
         """
 
-        ct = j.get("cellType", None)
-        if ct in self.ctsTable:
-            fields.append("cre.creGroupsSpecific[%s] AS cts" %
-                              self.ctsTable[ct])
-        else:
-            fields.append("0::int AS cts")
-
-        if chrom and start and stop:
-            whereclauses += ["cre.chrom = '%s'" % chrom,
-                             "int4range(cre.start, cre.stop) && int4range(%s, %s)" % (int(start), int(stop))]
-
-        if ct:
-            for assay in [("dnase", "dnase"),
-                          ("promoter", "h3k4me3"),
-                          ("enhancer", "h3k27ac"),
-                          ("ctcf", "ctcf")]:
-                if ct not in self.ctmap[assay[0]]:
-                    fields.append("'' AS %s_zscore" % (assay[0]))
-                    continue
-                cti = self.ctmap[assay[0]][ct]
-                fields.append("cre.%s_zscores[%d] AS %s_zscore" % (assay[1], cti, assay[0]))
-
-                if "rank_%s_start" % assay[0] in j and "rank_%s_end" % assay[0] in j:
-                    _range = [j["rank_%s_start" % assay[0]] / 100.0,
-                              j["rank_%s_end" % assay[0]] / 100.0]
-                    minDefault = -10.0  # must match slider default
-                    maxDefault = 10.0   # must match slider default
-                    if isclose(_range[0], minDefault) and isclose(_range[1], maxDefault):
-                        continue # not actually filtering on zscore, yet...
-                    if not isclose(_range[0], minDefault):
-                        whereclauses.append("(%s)" %
-                                            "cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]))
-                    elif not isclose(_range[1], maxDefault):
-                        whereclauses.append("(%s)" %
-                                            "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1]))
-                    else:
-                        whereclauses.append("(%s)" % " and ".join(
-                                ["cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]),
-                                 "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1])] ))
-        else:
-            allmap = {"dnase": "dnase_max",
-                      "promoter": "h3k4me3_max",
-                      "enhancer": "h3k27ac_max",
-                      "ctcf": "ctcf_max" }
-            for x in ["dnase", "promoter", "enhancer", "ctcf"]:
-                if "rank_%s_start" % x in j and "rank_%s_end" in j:
-                    _range = [j["rank_%s_start" % x] / 100.0,
-                              j["rank_%s_end" % x] / 100.0]
-                    whereclauses.append("(%s)" % " and ".join(
-                        ["cre.%s >= %f" % (allmap[x], _range[0]),
-                         "cre.%s <= %f" % (allmap[x], _range[1]) ] ))
-                fields.append("cre.%s AS %s_zscore" % (allmap[x], x))
-
-        whereclause = ""
-        if len(whereclauses) > 0:
-            whereclause = "WHERE " + " and ".join(whereclauses)
-        #print(whereclause)
-        return (fields, whereclause)
-
-    def creTable(self, j, chrom, start, stop):
-        tableName = self.assembly + "_cre_all"
-
-        fields, whereclause = self._getAccessions(j, chrom, start, stop)
-        if not fields:
-            fields, whereclause = self._creTableWhereClause(j, chrom, start, stop)
-
-        fields = ', '.join(fields + [
-            "accession", "maxZ",
-            "cre.chrom", "cre.start",
-            "cre.stop - cre.start AS len",
-            "cre.gene_all_id", "cre.gene_pc_id",
-            "0::int as in_cart",
-            "cre.creGroup"])
+        fields, whereClause = self._buildWhereStatement(j, chrom, start, stop)
 
         with getcursor(self.pg.DBCONN, "_cre_table") as curs:
             q = """
 SELECT JSON_AGG(r) from(
 SELECT {fields}
 FROM {tn} AS cre
-{whereclause}
+{whereClause}
 ORDER BY maxz DESC
 LIMIT 1000) r
-""".format(fields = fields, tn = tableName,
-           whereclause = whereclause)
+""".format(fields = fields, tn = self.tableName,
+           whereClause = whereClause)
 
             #print("\n", q, "\n")
             if 0:
@@ -214,10 +102,29 @@ LIMIT 1000) r
 
             total = len(rows)
             if total >= 1000: # reached query limit
-                total = self._creTableEstimate(curs, tableName, whereclause)
+                total = self._creTableEstimate(curs, whereClause)
         return {"cres": rows, "total" : total}
 
-    def _creTableEstimate(self, curs, tableName, whereclause):
+    def _accessions(self, j):
+        accs = j.get("accessions", [])
+        if not accs or 0 == len(accs):
+            return
+
+        if accs and len(accs) > 0:
+            if type(accs[0]) is dict:
+                accs = [x["value"] for x in accs if x["checked"]]
+            accs = filter(lambda x: isaccession(x), accs)
+            if accs:
+                accs = ["'%s'" % x.upper() for x in accs]
+                accsQuery = "accession IN (%s)" % ','.join(accs)
+                self.whereClauses.append("(%s)" % accsQuery)
+    
+    def _where(self, chrom, start, stop):
+        if chrom and start and stop:
+            self.whereClauses += ["cre.chrom = '%s'" % chrom,
+                                  "int4range(cre.start, cre.stop) && int4range(%s, %s)" % (int(start), int(stop))]
+            
+    def _creTableEstimate(self, curs, whereClause):
         # estimate count
         # from https://wiki.postgresql.org/wiki/Count_estimate
 
@@ -227,21 +134,63 @@ LIMIT 1000) r
 SELECT count(0)
 FROM {tn} AS cre
 {wc}
-""".format(tn = tableName, wc = whereclause)
+""".format(tn = self.tableName, wc = whereClause)
         if 0:
             timedQuery(curs, q)
         else:
             curs.execute(q)
         return curs.fetchone()[0]
 
+    def _notCtSpecific(self, j):
+        # use max zscores
+        allmap = {"dnase": "dnase_max",
+                  "promoter": "h3k4me3_max",
+                  "enhancer": "h3k27ac_max",
+                  "ctcf": "ctcf_max" }
+        for x in ["dnase", "promoter", "enhancer", "ctcf"]:
+            if "rank_%s_start" % x in j and "rank_%s_end" in j:
+                _range = [j["rank_%s_start" % x] / 100.0,
+                          j["rank_%s_end" % x] / 100.0]
+                self.whereClauses.append("(%s)" % " and ".join(
+                    ["cre.%s >= %f" % (allmap[x], _range[0]),
+                     "cre.%s <= %f" % (allmap[x], _range[1]) ] ))
+            self.fields.append("cre.%s AS %s_zscore" % (allmap[x], x))
+                
+    def _ctSpecific(self, ct, j):
+        for assay in [("dnase", "dnase"),
+                      ("promoter", "h3k4me3"),
+                      ("enhancer", "h3k27ac"),
+                      ("ctcf", "ctcf")]:
+            if ct not in self.ctmap[assay[0]]:
+                self.fields.append("'' AS %s_zscore" % (assay[0]))
+                continue
+            cti = self.ctmap[assay[0]][ct]
+            self.fields.append("cre.%s_zscores[%d] AS %s_zscore" % (assay[1], cti, assay[0]))
+
+            if "rank_%s_start" % assay[0] in j and "rank_%s_end" % assay[0] in j:
+                _range = [j["rank_%s_start" % assay[0]] / 100.0,
+                          j["rank_%s_end" % assay[0]] / 100.0]
+                minDefault = -10.0  # must match slider default
+                maxDefault = 10.0   # must match slider default
+                if isclose(_range[0], minDefault) and isclose(_range[1], maxDefault):
+                    continue # not actually filtering on zscore, yet...
+                if not isclose(_range[0], minDefault):
+                    self.whereClauses.append("(%s)" %
+                                        "cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]))
+                elif not isclose(_range[1], maxDefault):
+                    self.whereClauses.append("(%s)" %
+                                        "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1]))
+                else:
+                    self.whereClauses.append("(%s)" % " and ".join(
+                            ["cre.%s_zscores[%d] >= %f" % (assay[1], cti, _range[0]),
+                             "cre.%s_zscores[%d] <= %f" % (assay[1], cti, _range[1])] ))
+
     def creTableDownloadBed(self, j, fnp):
         chrom = checkChrom(self.assembly, j)
         start = j.get("coord_start", 0)
         stop = j.get("coord_end", 0)
 
-        tableName = self.assembly + "_cre_all"
-
-        fields, whereclause = self._creTableWhereClause(j, chrom, start, stop)
+        fields, whereClause = self._buildWhereStatement(j, chrom, start, stop)
         fields = ', '.join(["cre.chrom", "cre.start",
                             "cre.stop",
                             "accession", "maxZ"])
@@ -250,11 +199,11 @@ FROM {tn} AS cre
 COPY (
 SELECT {fields}
 FROM {tn} AS cre
-{whereclause}
+{whereClause}
 ) to STDOUT
 with DELIMITER E'\t'
 """.format(fields = fields, tn = tableName,
-           whereclause = whereclause)
+           whereClause = whereClause)
 
         with getcursor(self.pg.DBCONN, "_cre_table_bed") as curs:
             with gzip.open(fnp, 'w') as f:
@@ -265,21 +214,19 @@ with DELIMITER E'\t'
         start = j.get("coord_start", None)
         stop = j.get("coord_end", None)
 
-        tableName = self.assembly + "_cre_all"
-
-        fields, whereclause = self._creTableWhereClause(j, chrom, start, stop)
+        fields, whereClause = self._buildWhereStatement(j, chrom, start, stop)
 
         q = """
 copy (
 SELECT JSON_AGG(r) from (
 SELECT *
 FROM {tn} AS cre
-{whereclause}
+{whereClause}
 ) r
 ) to STDOUT
 with DELIMITER E'\t'
-""".format(tn = tableName,
-           whereclause = whereclause)
+""".format(tn = self.tableName,
+           whereClause = whereClause)
 
         with getcursor(self.pg.DBCONN, "_cre_table_json") as curs:
             with gzip.open(fnp, 'w') as f:
