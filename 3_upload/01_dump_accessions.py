618a9ce7a9d88b91bb9b4b998094b1940b64fc06
Michael Purcaro <purcaro@gmail.com>
2017-03-21 17:54:54 -0400

    process file accessions
    Former-commit-id: ac0d4b930527c9b2c9cd065beff787cb8926db32

--- 3_upload/01_dump_accessions.py
+++ 3_upload/01_dump_accessions.py
@@ -6,7 +6,7 @@ import StringIO
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              "../../metadata/utils"))
-from utils import AddPath, Utils, Timer, printt
+from utils import AddPath, Utils, Timer, printt, printWroteNumLines
 from db_utils import getcursor, vacumnAnalyze, makeIndex
 from files_and_paths import Dirs, Tools, Genome, Datasets
 from exp import Exp
@@ -20,6 +20,7 @@ class DumpAccessions:
     def __init__(self, curs, assembly):
         self.curs = curs
         self.assembly = assembly
+        self.fileIDs = set()
 
     def _print(self, tableName):
         self.curs.execute("""
@@ -27,7 +28,7 @@ SELECT fileID
 from {tn}
         """.format(tn = tableName))
         for r in self.curs.fetchall():
-            print(r[0])
+            self.fileIDs.add(r[0])
 
     def _ge(self):
         tableName = "r_rnas_" + self.assembly
@@ -36,16 +37,14 @@ SELECT encode_id
 from {tn}
         """.format(tn = tableName))
         expIDs = [r[0] for r in self.curs.fetchall()]
-        fileIDs = []
 
         for expID in expIDs:
             exp = Exp.fromJsonFile(expID)
             for f in exp.files:
                 if self.assembly == f.assembly:
                     if f.isGeneQuantifications():
-                        fileIDs.append(f.fileID)
-        print('\n'.join(fileIDs))
-        
+                        self.fileIDs.add(f.fileID)
+
     def run(self):
         self._print(self.assembly + "_datasets")
 
@@ -54,8 +53,10 @@ from {tn}
 
         self._ge()
 
-        print("ENCFF471EYL") # Hi-C
-        
+        self.fileIDs.add("ENCFF471EYL") # Hi-C
+        printt("found", len(self.fileIDs))
+        return self.fileIDs
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--assembly", type=str, default="")
@@ -72,12 +73,33 @@ def main():
     if args.assembly:
         assemblies = [args.assembly]
 
+    fileIDs = set()
+
+    d = "/home/mjp/Dropbox/0_accessionsV4/"
+    for fnp in ["/home/mjp/Dropbox/0_accessionsV4/arjan.tsv",
+                "/home/mjp/Dropbox/0_accessionsV4/peaks.txt"]:
+        with open(fnp) as f:
+            ids = [x.strip() for x in f]
+            ids = filter(lambda x: x, ids)
+            printt(fnp, len(ids))
+            ids = set(ids)
+            printt(fnp, len(ids), "unique")
+            fileIDs = fileIDs.union(ids)
+
     for assembly in assemblies:
         print('***********', assembly)
         with getcursor(DBCONN, "DumpAccessions") as curs:
             da = DumpAccessions(curs, assembly)
-            da.run()
+            fileIDs = fileIDs.union(da.run())
 
+    printt("found", len(fileIDs), "total")
+    fileIDs = filter(lambda x: x.startswith("ENCFF"), fileIDs)
+    printt("found", len(fileIDs), "total unique")
+
+    outFnp = os.path.join(d, "v10_file_accessions.txt")
+    with open(outFnp, 'w') as f:
+        f.write('\n'.join(sorted(fileIDs)))
+    printWroteNumLines(outFnp)
     return 0
 
 if __name__ == '__main__':
