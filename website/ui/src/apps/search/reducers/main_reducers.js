13f92e09f08f0b9c9fd787b5d388840b86566ef3
Michael Purcaro <purcaro@gmail.com>
2017-04-12 19:33:05 -0400

    configure genomebrowser actions

--- website/ui/src/apps/search/reducers/main_reducers.js
+++ website/ui/src/apps/search/reducers/main_reducers.js
@@ -49,6 +49,10 @@ const main_reducers = (state, action) => {
 	    return {...state, ...mainTabSetter(state, "configgb"),
 		    configuregb_cre: action.cre,
 		    configuregb_browser: action.name};
+
+	case Actions.TOGGLE_GENOME_BRWOSER_CELLTYPE:
+	    return {..state,
+		    configuregb_cts: doToggle(state.configuregb_cts, action.ct)};
 	    
 	case Actions.SHOW_RE_DETAIL:
             return {...state, ...mainTabSetter(state, "details"),
