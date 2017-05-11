988e256f866f1248651b912e41cb13ed4cd9ad4d
Michael Purcaro <purcaro@gmail.com>
2017-05-11 19:43:58 -0400

    importing other tracks

--- 2_import/25_more_tracks.py
+++ 2_import/25_more_tracks.py
@@ -10,6 +10,9 @@ from utils import AddPath, Utils, Timer, printt, printWroteNumLines
 from db_utils import getcursor, vacumnAnalyze, makeIndex, makeIndexIntRange
 from files_and_paths import Dirs, Tools, Genome, Datasets
 from exp import Exp
+from querydcc import QueryDCC
+from cache_memcache import MemCacheWrapper
+from metadataws import MetadataWS
 
 AddPath(__file__, '../common/')
 from dbconnect import db_connect, db_connect_single
@@ -21,20 +24,18 @@ from pg_common import PGcommon
 from pg import PGsearch
 from postgres_wrapper import PostgresWrapper
 
-class NineState:
+class MoreTracks:
     def __init__(self, curs, assembly, pg):
         self.curs = curs
         self.assembly = assembly
         self.pg = pg
-        self.tableName = assembly + "_nine_state"
-        self.inFnp = paths.path(self.assembly,
-                                self.assembly + "-Look-Up-Matrix.txt")
+        self.pgSearch = PGsearch(pg, assembly)
+        self.tableName = assembly + "_more_tracks"
 
     def run(self):
         self._setupTable()
         self._doImport()
-        self._doIndex()
-        self._doUpdate()
+        #self._doIndex()
         
     def _setupTable(self):
         printt("drop and create", self.tableName)
@@ -43,47 +44,37 @@ class NineState:
         CREATE TABLE {tn}
         (id serial PRIMARY KEY,
         cellTypeName text,
-        cellTypeDesc text,
-        dnase text,
-        h3k4me3 text,
-        h3k27ac text,
-        ctcf text
+        tracks jsonb
         );""".format(tn = self.tableName))
 
     def _doImport(self):
-        printt("reading", self.inFnp)
-        with open(self.inFnp) as f:
-            rows = [line.rstrip('\n').split('\t') for line in f]
-        printt("rows", "{:,}".format(len(rows)))
+        mc = MemCacheWrapper()
+        qd = QueryDCC(cache = mc)
 
-        printt("rewrite rows")
-        outF = StringIO.StringIO()
-        for r in rows:
-            outF.write('\t'.join(r) + '\n')
-        outF.seek(0)
-        cols = ["cellTypeName", "cellTypeDesc", "dnase", "h3k4me3",
-                "h3k27ac", "ctcf"]
-        self.curs.copy_from(outF, self.tableName, '\t', columns = cols)
-        printt("inserted", "{:,}".format(self.curs.rowcount))
+        if "hg19" == self.assembly:
+            m = MetadataWS(Datasets.all_human)
+        elif "mm10" == self.assembly:
+            m = MetadataWS(Datasets.all_mouse)
 
-    def _doUpdate(self):
-        printt("adding col...")
-        self.curs.execute("""
-        ALTER TABLE {tncres}
-        DROP COLUMN IF EXISTS cellTypeDesc;
+        tfs = m.chipseq_tfs_useful(self.assembly)
+        hists = m.chipseq_histones_useful(self.assembly)
+        allExps = tfs + hists
 
-        ALTER TABLE {tncres}
-        ADD COLUMN cellTypeDesc text;
+        ret = {}
+        ns = self.pgSearch.loadNineStateGenomeBrowser()
+        for ctn, v in ns.iteritems():
+            btns = set()
+            for fileID in [v["dnase"], v["h3k4me3"], v["h3k27ac"], v["ctcf"]]:
+                if 'NA' == fileID:
+                    continue
+                exp = qd.getExpFromFileID(fileID)
+                btns.add(exp.biosample_term_name)
 
-        UPDATE {tncres} as ds
-        SET cellTypeDesc = ns.cellTypeDesc
-        FROM {tn} as ns
-        where ds.cellTypeName = ns.cellTypeName
-    """.format(tn = self.tableName, tncres = self.assembly + "_datasets"))
-        if 0 == self.curs.rowcount:
-            raise Exception("error: no cRE rows updated")
-        printt("updated", "{:,}".format(self.curs.rowcount))
-
+            exps = filter(lambda e: e.biosample_term_name in btns,
+                          tfs + hists)
+            ret[ctn] = [e.encodeID for e in exps]
+            print(ctn, ret[ctn])
+                
     def _doIndex(self):
         makeIndex(self.curs, self.tableName, ["cellTypeName", "cellTypeDesc"])
 
@@ -96,12 +87,8 @@ def run(args, DBCONN):
         print('***********', assembly)
         pg = PostgresWrapper(DBCONN)
         with getcursor(DBCONN, "dropTables") as curs:
-            icg = NineState(curs, assembly, pg)
+            icg = MoreTracks(curs, assembly, pg)
             icg.run()
-
-    for assembly in assemblies:
-        with db_connect_single(os.path.realpath(__file__)) as conn:
-            vacumnAnalyze(conn, assembly + "_cre_all", [])
 
 def parse_args():
     parser = argparse.ArgumentParser()
