e5ca281f1c935e2f050729320e85405e28c8e783
Pratt <hpratt111@live.com>
2017-06-06 16:51:55 -0400

    start playing with global intersection page

---
+++ website/common/page_info_intersection.py
@@ -0,0 +1,23 @@
+import sys, os, json, cherrypy
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../common/'))
+from constants import PageTitle
+from config import Config
+
+class PageInfoMain:
+    def __init__(self, ps, cacheW):
+        self.ps = ps
+        self.cacheW = cacheW
+
+    def wholePage(self, userQueryErr):
+        bundleFnp = os.path.join(os.path.dirname(__file__),
+                                 "../ui/dist/bundle.js")
+        cssFnp = os.path.join(os.path.dirname(__file__),
+                              "../static/css.css")
+        return {"page": {"title" : PageTitle("")},
+                "Config": Config,
+                "Assembly" : None,
+                "userQueryErr" : userQueryErr,
+                "bundlets" : os.path.getmtime(bundleFnp),
+                "cssts" : os.path.getmtime(cssFnp)
+        }
