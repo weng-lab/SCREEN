2de0e28e85b57de22b8955b0ed04c54cd5695065
Henry Pratt <pratth@z010.internal.ho.wenglab.org>
2017-06-06 13:38:44 -0400

    start fantomcat pipeline

---
+++ 1_pipeline/06_fantomcat/03_intersect.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import sys, os
+import subprocess
+
+from common import FCPaths
+
+class Intersector:
+
+    @staticmethod
+    def run():
+        with open(FCPaths.intersected, "wb") as o:
+            subprocess.Popen(["bedtools", "intersect", "-wa", "-wb",
+                              "-a", FCPaths.genebed, "-b", FCPaths.cres], stdout = o)
+
+def main():
+    Intersector.run()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
