eb1f6e78480151779741bbca0bf004152e529922
Henry <hpratt111@live.com>
2017-06-08 19:52:44 +0000

    tweak plots

--- website/ui/src/apps/intersection/components/fantomcat/histogramtab.js
+++ website/ui/src/apps/intersection/components/fantomcat/histogramtab.js
@@ -2,6 +2,7 @@ var React = require('react');
 
 import Boxplot from '../../../../plots/components/boxplot';
 import Histogram from '../../../../plots/components/histogram';
+import PlotWithHeader from '../../../../plots/components/plotwithheader';
 
 class HistogramTab extends React.Component {
 
@@ -11,38 +12,37 @@ class HistogramTab extends React.Component {
 
     _render_boxplot(q, o) {
 	if (!q) return <div />;
-	return (<div><div className="row">
-		<div className="col-md-6"><h3>cREs per kb</h3></div>
-		<div className="col-md-6" />
-		</div>
+	return (<div>
 		<div className="row">
-		<div style={{height: "600px"}} className="row-md-6">
-		    <Boxplot viewBox={{width: 600, height: 450}} useiqr={true}
-		        margin={{left: 5, bottom: 5, top: 0, right: 5}}
-		        qsets={q} qsetorder={o} />
-		</div>
-		<div className="col-md-6" />
+	            <PlotWithHeader colspan={6} text="cREs per kb, outliers hidden" height={450} headersize={3}>
+		        <Boxplot viewBox={{width: 600, height: 450}} useiqr={true}
+		          margin={{left: 5, bottom: 5, top: 0, right: 5}}
+		          qsets={q} qsetorder={o} range={[0, 5]} hideoutliers={true} />
+		    </PlotWithHeader>
+		    <PlotWithHeader colspan={6} text="cREs per kb, outliers shown" height={450} headersize={3}>
+		        <Boxplot viewBox={{width: 600, height: 450}} useiqr={true}
+		          margin={{left: 5, bottom: 5, top: 0, right: 5}}
+		          qsets={q} qsetorder={o} />
+		    </PlotWithHeader>
 		</div>
 		</div>);
     }
 
     _render_histograms(q) {
-	return (<div><div className="row">
-		<div className="col-md-6"><h3>cREs per kb, non-coding RNA</h3></div>
-		<div className="col-md-6"><h3>cREs per kb, coding RNA</h3></div>
-		</div>
-		<div className="row">
-	  	<div style={{height: "300px"}} className="col-md-6">
-		    <Histogram x={Array.from(Array(100).keys()).map(x => x / 20.0)} y={q.classes["non-coding"].bins}
-		       viewBox={{width: 400, height: 150}} ticklimit={5}
-		       margin={{left: 5, bottom: 5, top: 0, right: 5}} />
-		</div>
-		<div style={{height: "300px"}} className="col-md-6">
-		    <Histogram x={Array.from(Array(100).keys()).map(x => x / 20.0)} y={q.classes.coding.bins}
-		       viewBox={{width: 400, height: 150}} ticklimit={5}
-		       margin={{left: 5, bottom: 5, top: 0, right: 5}} />
-		</div>
-		</div></div>);
+	return (<div>
+		  <div className="row">
+		    <PlotWithHeader colspan={6} text="cREs per kb, non-coding RNA" height={300} headersize={3}>
+		        <Histogram x={Array.from(Array(100).keys()).map(x => x / 20.0)} y={q.classes["non-coding"].bins}
+		          viewBox={{width: 400, height: 150}} ticklimit={5}
+		          margin={{left: 5, bottom: 5, top: 0, right: 5}} />
+		    </PlotWithHeader>
+		    <PlotWithHeader colspan={6} text="cREs per kb, coding RNA" height={300} headersize={3}>
+		        <Histogram x={Array.from(Array(100).keys()).map(x => x / 20.0)} y={q.classes.coding.bins}
+		          viewBox={{width: 400, height: 150}} ticklimit={5}
+		          margin={{left: 5, bottom: 5, top: 0, right: 5}} />
+		    </PlotWithHeader>
+		  </div>
+		</div>);
     }
     
     render() {
