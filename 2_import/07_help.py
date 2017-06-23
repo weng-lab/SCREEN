bd887283540e858bf95c661dd6925620008e06b0
Michael Purcaro <purcaro@gmail.com>
2017-06-23 10:41:36 -0400

    split help text into pipeline and import steps
    Former-commit-id: fb7c1d4142229dffd3aca80d9b543f62adb131ff

--- 2_import/07_help.py
+++ 2_import/07_help.py
@@ -4,8 +4,7 @@ from __future__ import print_function
 import os
 import sys
 import argparse
-from oauth2client import tools
-import StringIO
+import json
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              "../../metadata/utils"))
@@ -15,10 +14,7 @@ from utils import AddPath, printt
 AddPath(__file__, '../common/')
 from dbconnect import db_connect
 from postgres_wrapper import PostgresWrapper
-from constants import helptext
-
-AddPath(__file__, "../googleapi")
-from helptext import GoogleDocs
+from constants import helptext, paths
 
 class HelpTextImport:
     def __init__(self, args, curs):
@@ -26,66 +22,26 @@ class HelpTextImport:
         self.curs = curs
 
     def run(self):
-        # download GoogleDoc
-        self.data = GoogleDocs(self.args).getcontents(helptext.docid).split("\n")
-
-        # only reset DB if valid helptext was downloaded
-        if len(self.data) == 0:
-            print("no help text could be downloaded; please check GoogleDoc contents at https://docs.google.com/document/d/%s" % helptext.docid)
-            return 1
-        
         self._recreate_tables()
-        self._parse()
-        self._save()
+        self._import()
 
-    def _parse(self):
-        """
-          " load from the cached Google Doc and parse
-          " file format is:
-          "
-          " @key
-          " # comment
-          " %title
-          " help_text
-        """
-        key = None
-        help_text = ""
-        title = ""
+    def _import(self):
+        fnp = paths.path('', "extras", "google-help-text.json")
+        with open(fnp) as f:
+            j = json.load(f)
+        records_list_template = j["records_list_template"]
+        rows = [tuple(r) for r in j["rows"]] # for psycopg2
 
-        rows = []
-        for line in self.data:
-            if line.startswith('@'):
-                if key and help_text:
-                    if not title:
-                        print("error: missing title for", key)
-                    rows.append((key, title, help_text.strip()))
-                key = line.strip()[1:]
-                help_text = ""
-                title = ""
-            elif line.startswith("%"):
-                title = line.replace("%", "").strip()
-            elif not line.startswith("#"):
-                help_text += line.strip() + "\n"
-        if key and help_text:
-            if not title:
-                print("error: missing title for", key)
-            rows.append((key, title, help_text.strip()))
-
-        self.rows = filter(lambda x: not x[0].startswith("key: a specific key"),
-                           rows)
-
-    def _save(self):
-        keys = [r[0] for r in self.rows]
+        keys = [r[0] for r in rows]
         print('\n'.join(keys))
 
         # from http://stackoverflow.com/a/30985541
-        records_list_template = ','.join(['%s'] * len(self.rows))
         q = """
 INSERT INTO helpkeys (key, title, summary)
-VALUES  {}
+VALUES {}
 """.format(records_list_template)
 
-        self.curs.execute(q, self.rows)
+        self.curs.execute(q, rows)
         print("inserted", "{:,}".format(self.curs.rowcount), "help text items")
         
     def _recreate_tables(self):
@@ -105,8 +61,9 @@ def run(args, DBCONN):
         return hti.run()
 
 def parse_args():
-    parser = argparse.ArgumentParser(parents = [tools.argparser])
-    return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    return args
 
 def main():
     args = parse_args()
