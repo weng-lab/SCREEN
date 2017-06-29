68266d55226ce9fa84013a2f421ba4ff49904cf9
hpratt <hpratt111@live.com>
2017-06-28 21:10:28 -0400

    add ignored files

---
+++ 1_screen_pipeline/just21/cpp/src/common/region.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace SCREEN {
+
+  struct region {
+    uint  start;
+    uint  end;
+    float score;
+  };
+
+  std::string regionToString(struct region);
+
+  class RegionSet {
+
+  private:
+    std::unordered_map<std::string, std::vector<struct region>> regions_;
+    void _append(const std::string&, int, float);
+
+  public:
+    RegionSet();
+    RegionSet(std::unordered_map<std::string, std::vector<struct region>>);
+
+    void appendNarrowPeak(const boost::filesystem::path&, float = 0.05);
+    void appendNarrowPeak(const std::string&, float = 0.05);
+    void appendZ(const boost::filesystem::path&);
+    void appendZ(const std::string&);
+
+    void sort();
+    RegionSet rDHS_Cluster();
+    void write_binary(const std::string&, const boost::filesystem::path&);
+    void read_binary(const std::string&, const boost::filesystem::path&);
+    void write(const boost::filesystem::path&);
+
+  };
+
+} // SCREEN
