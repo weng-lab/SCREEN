1c0f938303f121e3ff7e21e3c03c735bf35fd848
hpratt <hpratt111@live.com>
2017-07-17 18:15:39 -0400

    get hg38 signal lists from hg19; temporarily hard-code paths
    Former-commit-id: efe91a0a33e9cd3534212045dbe43b5d42581b4c

---
+++ 1_screen_pipeline/just21/python/00_create_hg38.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import sys, os
+import glob
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../../metadata/utils"))
+from exp import Exp
+from exp_file import ExpFile
+
+class CreateHg38:
+    LISTFILES = ["dnase-list.txt", "h3k4me3-list.txt", "h3k27ac-list.txt", "ctcf-list.txt"]
+
+    @staticmethod
+    def _process(exp):
+        allsignal = glob.glob("/data/projects/encode/data/%s/*.bigWig" % exp.encodeID)
+        signal = {}
+        for signalfile in allsignal:
+            f = ExpFile.fromJsonFile(exp.encodeID, os.path.basename(signalfile).split(".")[0], True)
+            if f.assembly == "GRCh38": signal[f.biological_replicates[0]] = f
+        peaks = {x.biological_replicates[0]: x for x in filter(lambda x: x.assembly == "GRCh38" and x.file_type == "bed broadPeak", exp.files)}
+        return (peaks, signal)
+
+    def __init__(self, rootdir):
+        self.filemap = {}
+        for k in CreateHg38.LISTFILES:
+            self.filemap[k] = {}
+            self.filemap[k + "_all"] = {}
+
+        # for each assay
+        for listfile in CreateHg38.LISTFILES:
+            
+            # load each exp accession from the existing list
+            # for each, append only the first rep to one list and all reps to the other
+            with open(os.path.join(rootdir, listfile), "r") as f:
+                for line in f:
+                    p = line.strip().split('\t')
+                    try:
+                        e = Exp.fromJsonFile(p[0])
+                        peaks, signal = CreateHg38._process(e)
+                        k = p[4] if len(p) >= 5 else p[2]
+                        self.filemap[listfile][(k, e.encodeID)] = (peaks[1].fileID, signal[1].fileID)
+                        self.filemap[listfile + "_all"][(k, e.encodeID)] = [(peaks[x].fileID, signal[x].fileID) for x, _ in signal.iteritems()]
+                    except:
+                        print("00_create_hg38$CreateHg38::__init__: could not process %s; skipping" % p[0])
+            
+            # if DNase, write all reps to Hotspot-List.txt
+            if listfile == "dnase-list.txt":
+                with open("/data/projects/cREs/hg38/Hotspot-List.txt", "wb") as o:
+                    for k, v in self.filemap[listfile + "_all"].iteritems():
+                        ct, acc = k
+                        for peaks, signal in v:
+                            o.write("%s\t%s\t%s\t%s\t%s\n" % (acc, peaks, acc, signal, ct))
+                print("wrote /data/projects/cREs/hg38/Hotspot-List.txt")
+
+            # write first reps to list file
+            with open("/data/projects/cREs/hg38/%s" % listfile, "wb") as o:
+                for k, v in self.filemap[listfile].iteritems():
+                    ct, acc = k
+                    peaks, signal = v
+                    o.write("%s\t%s\t%s\n" % (acc, signal, ct))
+            print("wrote /data/projects/cREs/hg38/%s" % listfile)
+
+def main():
+    CreateHg38("/data/projects/screen/Version-4/ver10/hg19/raw")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
