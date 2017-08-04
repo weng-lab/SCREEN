8122b274e7a9917dcfed725c18d6e63683811306
Michael Purcaro <purcaro@gmail.com>
2017-08-04 15:38:43 -0400

    cell-type agnostic file download

--- website/ui/src/apps/index/components/tab_files.js
+++ website/ui/src/apps/index/components/tab_files.js
@@ -5,7 +5,47 @@ import * as Render from '../../../common/renders'
 import loading from '../../../common/components/loading'
 import {tabPanelize} from '../../../common/utility'
 
-const TableColumns = () => {
+const CtaTableColumns = () => {
+    let klassCenter = "dt-body-center dt-head-center ";
+
+    const fileDownload = (url, fn) => {
+	return '<a href="' + url + '" download="'+ fn +'">' +
+	       '<span class="glyphicon glyphicon-download" aria-hidden="true">' +
+	       '</span>' + '</a>';
+    }
+
+    const dl = (a) => {
+	return fileDownload(a.join('/'), a[1]);
+    }
+    
+    return [
+	{
+	    title: "Assembly", data: "assembly", className: klassCenter,
+	}, {
+            title: "5 group", data: "fiveGroup", className: klassCenter,
+	    render: dl
+	}, {
+            title: "5 group url", data: "fiveGroup", visible: false
+	}, {
+	    title: "9 state high&nbsp;H3K27ac", data: "h3k27ac", className: klassCenter,
+	    render: dl
+	}, {
+	    title: "9 state high&nbsp;H3K27ac", data: "h3k27ac", visible: false
+	}, {
+	    title: "9 state high&nbsp;H3K4me3", data: "h3k4me3", className: klassCenter,
+	    render: dl
+	}, {
+	    title: "9 state high&nbsp;H3K4me3", data: "h3k4me3", visible: false
+	}, {
+            title: "9 state high&nbsp;CTCF", data: "ctcf", className: klassCenter,
+	    render: dl
+	}, {
+            title: "9 state high&nbsp;CTCF", data: "ctcf", visible: false
+	}
+    ];
+}
+
+const CtsTableColumns = () => {
     let klassCenter = "dt-body-center dt-head-center ";
 
     const dccLink = (expID) => {
@@ -111,11 +151,35 @@ class TabFiles extends React.Component {
 
     doRenderWrapper(){
         if("files" in this.state){
-	    return (<ResultsTable data={this.state.files}
-		    cols={TableColumns()}
-                    bFilter={true}
-                    bLengthChange={true}
-                    />);
+	    const base = "http://bib7.umassmed.edu/~purcarom/encyclopedia/Version-4/ver10";
+	    const WWW = "http://bib7.umassmed.edu/~purcarom/screen/ver4/v10";
+	    const cta = [
+		{assembly: "mm10",
+		 ctcf: [WWW, "mm10-cRE.CTCF.cREs.bigBed"],
+		 fiveGroup: [base + "/mm10", "mm10-cREs-V10.bigBed"],
+		 h3k4me3: [WWW, "mm10-cRE.Promoter.cREs.bigBed"],
+		 h3k27ac: [WWW, "mm10-cRE.Enhancer.cREs.bigBed"]},
+		{assembly: "hg19",
+		 ctcf: [WWW, "hg19-cRE.CTCF.cREs.bigBed"],
+		 fiveGroup: [base + "/hg19", "hg19-cREs-V10.bigBed"],
+		 h3k4me3: [WWW, "hg19-cRE.Promoter.cREs.bigBed"],
+		 h3k27ac: [WWW, "hg19-cRE.Enhancer.cREs.bigBed"]}
+	    ];
+	    return (
+		<div>
+		    <h3>Cell type-agnostic</h3>
+		    <ResultsTable data={cta}
+				  cols={CtaTableColumns()}
+				  bFilter={true}
+				  bLengthChange={true}
+		    />
+		    <h3>Cell type-specific</h3>
+		    <ResultsTable data={this.state.files}
+				  cols={CtsTableColumns()}
+				  bFilter={true}
+				  bLengthChange={true}
+                    />
+		</div>);
         }
 	return loading({...this.state})
     }
