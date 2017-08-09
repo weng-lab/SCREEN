00a7bc0b25a94076b5e45a0a4238d758862f5875
hpratt <hpratt111@live.com>
2017-08-09 10:34:18 -0400

    fix error with Global on index page
    Former-commit-id: 7054130251f99f798fb79109cfa6026fbf2619cf

--- website/ui/src/apps/tads/config/initial_state.js
+++ website/ui/src/apps/tads/config/initial_state.js
@@ -1,16 +1,13 @@
 import MainTabInfo from './maintabs.js'
 
-let chrs = Globals.chromCounts.map( x => [x[0], null] );
-console.log(chrs);
-
-const initialState = {
+const initialState = () => ({
     ...GlobalParsedQuery,
     maintabs: MainTabInfo,
     maintabs_active: "ctcf_distr",
     maintabs_visible: true,
     biosamples: [...Globals.tad_biosamples],
     biosample: null,
-    chrs, chr: null
-};
+    chrs: Globals.chromCounts.map( x => [x[0], null] ), chr: null
+});
 
 export default initialState;
