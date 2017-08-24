21f8e4b19e568fb5e752bec62ad6440d1d3af70a
Henry <hpratt111@live.com>
2017-08-24 14:31:21 -0400

    description for colors
    Former-commit-id: 725068c198914948a3411170ee6dfce688269c4b

--- paper/ui/src/components/tab_tables.js
+++ paper/ui/src/components/tab_tables.js
@@ -43,6 +43,11 @@ class TabTables extends React.Component {
 	          <div className="alert alert-info" style={{fontSize: "16pt"}}>
 	              <span className="glyphicon glyphicon-info-sign" style={{marginRight: "10px"}}></span>These tables are interactive. Click the column headers to sort, use the textboxes to search, and click the CSV buttons to download the table contents in CSV format.
 	          </div>
+	          {this.state.selected_table !== 6 ? null : (
+	            <div className="alert alert-info" style={{fontSize: "16pt"}}>
+	              <span className="glyphicon glyphicon-info-sign" style={{marginRight: "10px"}}></span>The colors in this table correspond to the bar colors in <b>Extended Data Figure 9</b>.
+	            </div>
+		  )}	  
 	          {this.props.globals.tables.map( (t, i) => ( i !== this.state.selected_table ? null : 
 		      <div key={i}><h2>{t.title.replace(/Supp./g, "Supplementary")}</h2><t.component /></div>
 		  ) )}
