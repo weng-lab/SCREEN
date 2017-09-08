4d38965cb60efc353ef62123adf195bc9e9477ff
Michael Purcaro <purcaro@gmail.com>
2017-09-08 19:16:00 -0400

    ensure biosample ID doesn't start w/ "/biosamples"; working on check for contaiminated cell lines
    Former-commit-id: 3562b25b32f6ac8b990f7204ce367b57fdaffa83

--- 4_paper/checkCellContaim.py
+++ 4_paper/checkCellContaim.py
@@ -2,12 +2,14 @@
 
 from __future__ import print_function
 
+import re
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils'))
 from utils import Utils, AddPath
 from exp import Exp
+from biosample import Biosample
 from querydcc import QueryDCC
 from cache_memcache import MemCacheWrapper
 
@@ -21,23 +23,48 @@ from cached_objects import CachedObjects
 from pg_common import PGcommon
 from cached_objects import CachedObjectsWrapper
 
+mc = MemCacheWrapper("localhost")
+qd = QueryDCC(cache=mc)
+
 def main():
+    with open("/home/mjp/Dropbox/Cross-Contaminations-v8_0.txt") as f:
+        lines = [x.strip() for x in f]
+    badCts = []
+    for line in lines:
+        result = re.findall(".*(CVCL_....).*", line, re.MULTILINE)
+        for r in result:
+            badCts.append(r)
+    badCts = set(badCts)
+
     DBCONN = db_connect(os.path.realpath(__file__))
 
     ps = PostgresWrapper(DBCONN)
     cacheW = CachedObjectsWrapper(ps)
 
-    ctsHg19 = cacheW["hg19"].datasets.byCellType.keys()
-    ctsMm10 = cacheW["mm10"].datasets.byCellType.keys()
-    print('\n'.join(sorted(ctsHg19 + ctsMm10)))
+    for assembly in ["hg19", "mm10"]:
+        for ct, exps in cacheW[assembly].datasets.byCellType.iteritems():
+            for expInfo in exps:
+                fileID = expInfo["fileID"]
+                print(fileID)
+                exp = qd.getExpFromFileID(fileID)
+                try:
+                    reps = exp.jsondata["replicates"]
+                except:
+                    continue
+                for rep in reps:
+                    biosampleID = rep["library"]["biosample"]["@id"]
+                    bs = Biosample(biosampleID)
+                    for s in bs.jsondata["dbxrefs"]:
+                        result = re.findall(".*(CVCL_....).*", s, re.MULTILINE)
+                        for r in result:
+                            if r in badCts:
+                                raise Exception("found bad " + r)
     sys.exit(1)
     
     url = "https://www.encodeproject.org/search/?searchTerm=Candidate+Regulatory+Elements+%28cREs%29&type=Annotation"
     url += "&format=json"
     url += "&limit=all"
 
-    mc = MemCacheWrapper("localhost")
-    qd = QueryDCC(cache=mc)
     #qd = QueryDCC()
 
     for exp in qd.getExps(url):
