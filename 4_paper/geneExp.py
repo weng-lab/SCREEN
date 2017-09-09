b4e960b332d0289b19906a40e65594712f755d9b
Michael Purcaro <purcaro@gmail.com>
2017-09-09 13:40:22 -0400

    output for cricket
    Former-commit-id: c2b1786c669a0767cbb88ebdf2055a438b39066b

--- 4_paper/geneExp.py
+++ 4_paper/geneExp.py
@@ -23,11 +23,6 @@ from pg_common import PGcommon
 from cached_objects import CachedObjectsWrapper
 
 def main():
-    DBCONN = db_connect(os.path.realpath(__file__))
-
-    ps = PostgresWrapper(DBCONN)
-    cacheW = CachedObjectsWrapper(ps)
-
     url = "https://www.encodeproject.org/search/?type=Annotation&annotation_type=gene+expression&annotation_type=transcript+expression"
     url += "&format=json"
     url += "&limit=all"
@@ -39,7 +34,6 @@ def main():
     exps = qd.getExps(url)
     
     for idx, exp in enumerate(exps):
-        print(idx+1, len(exps))
         for f in exp.files:
             if not f.isTSV():
                 continue
@@ -52,6 +46,8 @@ def main():
                 if t.startswith("EN"):
                     enc = t.split('_')[0]
                     derived.append(enc)
-        
+            #print(idx+1, len(exps),
+            print(exp.encodeID + '\t' + ','.join(sorted(list(set(derived)))))
+
 if __name__ == "__main__":
     sys.exit(main())
