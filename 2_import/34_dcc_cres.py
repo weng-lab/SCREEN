aa76aef2ae8c1fc5f6852f44017ed1835efc903a
Michael Purcaro <purcaro@gmail.com>
2017-10-12 04:13:49 -0400

    almost done importign dcc accessions

--- 2_import/34_dcc_cres.py
+++ 2_import/34_dcc_cres.py
@@ -7,6 +7,7 @@ import sys
 import gzip
 import argparse
 import json
+import re
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../metadata/utils'))
 from utils import Utils, eprint, AddPath, printt
@@ -37,19 +38,42 @@ class DCCCres:
         mc = MemCacheWrapper()
         qd = QueryDCC(cache=mc)
 
+        fnp = paths.path(self.assembly, self.assembly + "-Look-Up-Matrix.txt")
+        ctToBtid = {}
+        with open(fnp) as f:
+            for line in f:
+                line = line.strip().split('\t')
+                btid = re.sub('[^0-9a-zA-Z]+', '-', line[0]) 
+                ctToBtid[btid] = line[0]
+
+        lines = []
         for exp in qd.getExps(url):
             for f in exp.files:
                 if not f.isBigBed():
                     continue
-                print(f.alias)
-        
+                # [u'zhiping-weng:cREs-hg19-v10-ganglionic-eminence-derived-neurospheres-female-fetal-17-weeks-5group-bigBed', u'zhiping-weng:cREs-hg19-v10-ganglionic-eminence-derived-neurospheres-female-fetal-17-weeks-9state-H3K4me3-bigBed']
+                aliases = f.jsondata["aliases"]
+                typs = ["5group", "9state-H3K4me3", "9state-DNase",
+                        "9state-H3K27ac", "9state-CTCF"]
+                for a in aliases:
+                    a = a.replace("zhiping-weng:cREs-hg19-v10-", '').replace("-bigBed", '')
+                    for t in typs:
+                        if t in a:
+                            ct = a.replace(t, '')[:-1] # remove trailing hyphen
+                            if not ct:
+                                continue # agnostic
+                            if ct not in ctToBtid:
+                                raise Exception("missing " + ct)
+                            lines.append((ct, f.fileID, t))
+                        
         printt('***********', "drop and create", self.tableName)
         self.curs.execute("""
 DROP TABLE IF EXISTS {tableName};
 CREATE TABLE {tableName}
 (id serial PRIMARY KEY,
 celltype text,
-dcc_accession text
+dcc_accession text,
+typ text
 );""".format(tableName = self.tableName))
 
         printt('***********', "import links")
