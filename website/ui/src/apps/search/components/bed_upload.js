00c62f87091b570c1fc1cbf43050518dc34ec83b
Michael Purcaro <purcaro@gmail.com>
2017-10-16 17:32:15 -0400

    filling out bed upload

--- website/ui/src/apps/search/components/bed_upload.js
+++ website/ui/src/apps/search/components/bed_upload.js
@@ -2,26 +2,60 @@ import React from 'react';
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-
+import { Button } from 'react-bootstrap';
 import Dropzone from 'react-dropzone';
 
 import HelpIcon from '../../../common/components/help_icon';
 
 import * as Render from '../../../common/zrenders';
 import * as Actions from '../actions/main_actions';
+import * as ApiClient from '../../../common/api_client';
 
 class BedUpload extends React.Component {
     constructor(props) {
 	super(props);
+	this.submitFiles = this.submitFiles.bind(this);
 	this.state = { files: [] }
     }
     
     onDrop(files) {
+	console.log("files:", files);
 	this.setState({
 	    files
 	});
     }
     
+    submitFiles(){
+	let allLines = [];
+	this.state.files.forEach(f => {
+	    const reader = new FileReader();
+            reader.onload = (r) => {
+		const contents = r.target.result;
+		const lines = contents.split('\n');
+		allLines.push(lines);
+            };
+            reader.onabort = () => console.log('file reading was aborted');
+            reader.onerror = () => console.log('file reading has failed');
+		reader.onprogress  = (e) => {
+		    if (e.lengthComputable) {
+			if(e.loaded === e.total){
+			    const j = {uuid: this.props.uuid, 
+				       assembly: this.props.assembly,
+				       allLines};
+			    console.log("about to post", j);
+			    ApiClient.setByPost(JSON.stringify(j),
+						"/post/lines",
+						(r) => {},
+						(msg) => {
+						    console.log("error!");
+						});				    
+			}
+		    };
+		};
+		reader.readAsText(f);
+	});
+    }
+
     render() {
 	return (
 	    <div>
@@ -42,6 +76,9 @@ class BedUpload extends React.Component {
 			}
 		    </ul>
 		</aside>
+
+		<Button onClick={this.submitFiles}>Intersect Files
+	    </Button>
 	    </div>
 	);
     }
