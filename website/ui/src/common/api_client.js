84eedb0c8a30db94a236256afcb12eb04114d9b6
Michael Purcaro <purcaro@gmail.com>
2017-10-20 23:10:23 -0400

    switch to api

--- website/ui/src/common/api_client.js
+++ website/ui/src/common/api_client.js
@@ -6,7 +6,7 @@ export const Servers = (b) => {
     let override = {}
     //override["/gbws/geneTrack"] = "http://localhost:9006/gbws/geneTrack";
     //override["/gbws/trackhub"] = "http://localhost:9006/gbws/trackhub";
-    //override["/gews/search"] = "http://megatux.purcaro.com:9006/gews/search";
+    override["/gews/search"] = "http://api.wenglab.org/gews/search";
     if(b in override){
 	return override[b];
     }
