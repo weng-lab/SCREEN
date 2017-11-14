9ff644a1653b10ef20047068c3330b7cc4f586e3
Michael Purcaro <purcaro@gmail.com>
2017-11-14 15:06:10 -0500

    bigtrackhub sekl

---
+++ bigtrackhub/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+      return (
+	  <pre>
+	      yo
+	      hi
+	  </pre>
+	  
+    );
+  }
+}
+
+export default App;
---
+++ bigtrackhub/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
