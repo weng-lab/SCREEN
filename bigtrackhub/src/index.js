ba391e6973d0cc5f465d959bd7101ad584453b9a
Michael Purcaro <purcaro@gmail.com>
2017-11-14 15:07:15 -0500

    too slow

--- bigtrackhub/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
