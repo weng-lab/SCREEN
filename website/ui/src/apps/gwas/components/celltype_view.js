34a288d47f937d8b8f004112b82aa8d8858a9e48
Michael Purcaro <purcaro@gmail.com>
2017-12-10 15:22:27 -0500

    fix for non-sorting of gwas table

--- website/ui/src/apps/gwas/components/celltype_view.js
+++ website/ui/src/apps/gwas/components/celltype_view.js
@@ -100,6 +100,15 @@ class CelltypeView extends React.Component {
 
 	const columnDefs = [{ "orderData": 2, "targets": 1 }];
 	const actions = this.props.actions;
+
+	let table_order = "";
+	if(vcols["promoter zscore"]){
+	    table_order = ["promoter zscore", false];
+	} else if(vcols["enhancer zscore"]){
+	    table_order = ["enhancer zscore", false];
+	} else {
+	    table_order = ["dnase zscore", false];
+	}
 	
         const creTable = (
 	    <Ztable
@@ -111,7 +120,7 @@ class CelltypeView extends React.Component {
 		cols={cols}
 		bFilter={true}
 		cvisible={vcols}
-		order={[[2, "desc"], [0, "asc"]]}
+		sortCol={table_order}
             />);
 	const pct = Math.round(100.0 * cres.length / +this.props.rdata.numCresOverlap);
 	const numCresOverlap = this.props.rdata.numCresOverlap;
