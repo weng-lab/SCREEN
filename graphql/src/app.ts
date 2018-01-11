5a0d505ab5d976725ede4aed660055963fffa113
Jack <jack.huey@umassmed.edu>
2018-01-11 14:26:28 -0500

    Add CORS options

--- graphql/src/app.ts
+++ graphql/src/app.ts
@@ -18,6 +18,18 @@ maskErrors(schema);
 
 const app = express();
 
+
+app.use('/graphql', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   formatError: logErrors,
