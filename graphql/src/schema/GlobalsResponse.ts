b32f23a0cea327a6bfe4ba395cc33375490d8f5e
Jack <jack.huey@umassmed.edu>
2018-02-20 17:15:32 -0500

    Let helpKey return null, but still give error.

--- graphql/src/schema/GlobalsResponse.ts
+++ graphql/src/schema/GlobalsResponse.ts
@@ -69,7 +69,7 @@ export const HelpKeys = new GraphQLObjectType({
             args: {
                 key: { type: new GraphQLNonNull(GraphQLString) },
             },
-            type: new GraphQLNonNull(HelpKey),
+            type: HelpKey,
             resolve: resolve_help_key,
         },
     })
