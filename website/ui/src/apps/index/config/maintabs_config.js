a6da98a25a9e98e687caa0334f0913c060e5d111
Michael Purcaro <purcaro@gmail.com>
2018-04-13 07:47:43 -0400

    rename main tabs

--- website/ui/src/apps/index/config/maintabs_config.js
+++ website/ui/src/apps/index/config/maintabs_config.js
@@ -8,10 +8,10 @@ import TabData from '../components/tab_data';
 const MainTabsConfig = () => {
     return {main : {title: "Overview", visible: true, f: TabMain},
             about : {title: "About", visible: true, f: TabAbout},
-            tutorial : {title: "Tutorial", visible: true, f: TabTutorial},
-            query: {title: "Query Results", visible: false, f: TabQuery},
-	    files: {title: "Files", visible: true, f: TabFiles},
-	    input_data: {title: "Input Data", visible: true, f: TabData}
+            tutorial : {title: "Tutorials", visible: true, f: TabTutorial},
+	    files: {title: "Downloads", visible: true, f: TabFiles},
+	    input_data: {title: "Versions", visible: true, f: TabData},
+            query: {title: "Query Results", visible: false, f: TabQuery}
     };
 }
 
