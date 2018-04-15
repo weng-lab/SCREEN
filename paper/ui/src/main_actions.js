ca6c5c6653c5ace088ef1455e42cee4034fd2c9f
Michael Purcaro <purcaro@gmail.com>
2018-04-15 17:13:57 -0400

    all in master

--- paper/ui/src/main_actions.js
+++
@@ -1,4 +0,0 @@
-export const SHOW_MAIN_TABS = 'SHOW_MAIN_TABS';
-export const showMainTabs = (show) => ({ type: SHOW_MAIN_TABS, show });
-export const SET_MAIN_TAB = 'SET_MAIN_TAB';
-export const setMainTab = (name) => ({ type: SET_MAIN_TAB, name });
