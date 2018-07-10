weng-lab/SCREEN

commit a6b3e4038d3744dd28fb6a21896998188d1ae012

Rampage is no longer a detail of ccRE. To access, just use the base rampage api with gene. Also, nearbygenes on ccRE now returns in the same format as details.nearbygenes

graphql/src/db/db_common.ts
@@ -673,13 +673,25 @@ export async function rampage_info(assembly) {
     return ret;
 }
 
-export async function rampageEnsemblID(assembly, gene) {
+export async function rampageEnsemblID(
+    assembly: Assembly,
+    gene: string
+): Promise<{ gene: string; ensemblid_ver: string; coords: { chrom: string; start: number; end: number } }> {
     const tableName = assembly + '_gene_info';
     const q = `
-        SELECT ensemblid_ver FROM ${tableName}
+        SELECT ensemblid_ver, approved_symbol as gene, chrom, start, stop
+        FROM ${tableName}
         WHERE approved_symbol = $1
     `;
-    return await db.oneOrNone(q, [gene], r => r && r.ensemblid_ver);
+    return await db.one(q, [gene], r => ({
+        gene: r.gene,
+        ensemblid_ver: r.ensemblid_ver,
+        coords: {
+            chrom: r.chrom,
+            start: r.start,
+            end: r.stop,
+        },
+    }));
 }
 
 export async function linkedGenes(assembly, accession) {

graphql/src/resolvers/credetails.ts
@@ -9,14 +9,14 @@ import { select_cre_intersections, orthologs } from '../db/db_credetails';
 const request = require('request-promise-native');
 const { UserError } = require('graphql-errors');
 
-class CREDetails {
+export class CREDetails {
     assembly;
     accession;
     _coord: Promise<{ chrom: string; start: number; end: number }>;
     genesAll;
     genesPC;
 
-    constructor(assembly, accession) {
+    constructor(assembly: Assembly, accession: string) {
         this.assembly = assembly;
         this.accession = accession;
     }
@@ -63,7 +63,7 @@ class CREDetails {
         }
     }
 
-    async nearbyGenes() {
+    async nearbyGenes(): Promise<{ gene: any; distance: number; pc: boolean }[]> {
         await this.awaitGenes();
         const pcGenes = this.genesPC.map(g => g['approved_symbol']);
         return this.genesAll
@@ -271,13 +271,6 @@ export async function resolve_cre_tfIntersection(source, args, context, info) {
 export async function resolve_cre_cistromeIntersection(source, args, context, info) {
     const cre: CREDetails = source.details;
     return await cre.peakIntersectCount('cistrome');
-}
-
-export async function resolve_cre_rampage(source, args, context, info) {
-    const cre: CREDetails = source.details;
-    const nearbyGenes = await cre.nearbyPcGenes(); // Sorted by distance
-    const nearest = nearbyGenes[0];
-    return getByGene(cre.assembly, nearest);
 }
 
 export async function resolve_cre_linkedGenes(source, args, context, info) {

graphql/src/resolvers/cretable.ts
@@ -1,8 +1,9 @@
 import { getCreTable } from '../db/db_cre_table';
 import { GraphQLFieldResolver } from 'graphql';
 import { parse } from './search';
-import { loadCache } from '../db/db_cache';
+import { loadCache, Assembly } from '../db/db_cache';
 import { UserError } from 'graphql-errors';
+import { CREDetails } from './credetails';
 
 async function cre_table(data, assembly, pagination) {
     const ctmap = await loadCache(assembly).ctmap();
@@ -29,10 +30,12 @@ export async function resolve_data(source, inargs, context, info) {
 }
 
 export async function resolve_data_nearbygenes(source, args, context) {
-    const assembly = source.assembly;
-    const geneIDsToApprovedSymbol = await loadCache(assembly).geneIDsToApprovedSymbol();
-    const all = source['gene_all_id'].slice(0, 3).map(gid => geneIDsToApprovedSymbol[gid]);
-    const pc = source['gene_pc_id'].slice(0, 3).map(gid => geneIDsToApprovedSymbol[gid]);
+    const assembly: Assembly = source.assembly;
+    const accession: string = source.accession;
+    const cre = new CREDetails(assembly, accession);
+    const nearby = await cre.nearbyGenes();
+    const all = nearby.slice(0, 3);
+    const pc = nearby.filter(gene => gene.pc).slice(0, 3);
     return {
         all,
         pc,

graphql/src/resolvers/gwas.ts
@@ -38,7 +38,7 @@ class Gwas {
     async mainTableInfo(gwas_study) {
         const total = await this.totalLDblocks(gwas_study);
         const overlap = await this.numLdBlocksOverlap(gwas_study);
-        const overlapStr = `${overlap} (${Math.round(overlap / total * 100.0)}%)`;
+        const overlapStr = `${overlap} (${Math.round((overlap / total) * 100.0)}%)`;
         return [
             {
                 totalLDblocks: total,

graphql/src/resolvers/rampage.ts
@@ -1,7 +1,7 @@
 import { GraphQLFieldResolver } from 'graphql';
 import * as DbCommon from '../db/db_common';
-import HelperGrouper from '../helpergrouper';
-import { natsort, getAssemblyFromCre, natsorter } from '../utils';
+import { natsorter } from '../utils';
+import { Assembly } from '../db/db_cache';
 
 const sortTranscripts = (a, b) => natsorter(a.transcript, b.transcript);
 
@@ -21,8 +21,8 @@ const _process = (transcript, ri) => {
     };
 };
 
-export async function getByGene(assembly, gene) {
-    const ensemblid_ver = gene.gene.ensemblid_ver;
+export async function getByGene(assembly: Assembly, gene: { ensemblid_ver: string; gene: string }) {
+    const ensemblid_ver = gene.ensemblid_ver;
     const transcripts = await DbCommon.rampageByGene(assembly, ensemblid_ver);
 
     const ri = await DbCommon.rampage_info(assembly);
@@ -33,18 +33,9 @@ export async function getByGene(assembly, gene) {
     };
 }
 
-async function rampage(assembly, gene) {
-    const egene = await DbCommon.rampageEnsemblID(assembly, gene);
-    const r = {
-        ensemblid_ver: egene,
-        name: gene,
-    };
+export const resolve_rampage: GraphQLFieldResolver<any, any> = async (source, args, context) => {
+    const assembly: Assembly = args.assembly;
+    const gene: string = args.gene;
+    const r = await DbCommon.rampageEnsemblID(assembly, gene);
     return getByGene(assembly, r);
-}
-
-const global_data_global = require('../db/db_cache').global_data_global;
-export const resolve_rampage: GraphQLFieldResolver<any, any> = (source, args, context) => {
-    const assembly = args.assembly;
-    const gene = args.gene;
-    return rampage(assembly, gene);
 };

graphql/src/schema/CommonSchema.ts
@@ -12,7 +12,7 @@ import {
     GraphQLEnumType,
 } from 'graphql';
 import * as CommonTypes from './CommonSchema';
-import { CreDetailsResponse } from './CreDetailsResponse';
+import { CreDetailsResponse, NearbyGene } from './CreDetailsResponse';
 import { resolve_data_nearbygenes, resolve_data_range, resolve_data_ctspecific } from '../resolvers/cretable';
 import { resolve_details } from '../resolvers/credetails';
 
@@ -283,11 +283,11 @@ export const genes = new GraphQLObjectType({
     fields: {
         pc: {
             description: 'Nearby protein-coding genes',
-            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(GraphQLString))),
+            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(NearbyGene))),
         },
         all: {
             description: 'All nearby genes, including protein-coding and non-protein-coding',
-            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(GraphQLString))),
+            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(NearbyGene))),
         },
     },
 });

graphql/src/schema/CreDetailsResponse.ts
@@ -2,7 +2,7 @@ import { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLNonNull, GraphQLE
 import * as CreDetailsResolver from '../resolvers/credetails';
 import * as CommonTypes from './CommonSchema';
 import { GraphQLFloat, GraphQLInt, GraphQLBoolean } from 'graphql/type/scalars';
-import { SNP, Gene } from './SearchResponse';
+import { SNP } from './SearchResponse';
 import { GeneExpGene } from './GeneExpResponse';
 const GraphQLJSON = require('graphql-type-json');
 
@@ -44,7 +44,7 @@ export const CTAssayData = new GraphQLObjectType({
     }),
 });
 
-const NearbyGene = new GraphQLObjectType({
+export const NearbyGene = new GraphQLObjectType({
     name: 'NearbyGene',
     description: 'Distance and gene info for a nearby gene',
     fields: () => ({
@@ -201,21 +201,6 @@ export const FantomCat = new GraphQLObjectType({
     }),
 });
 
-const RampageGene = new GraphQLObjectType({
-    name: 'RampageGene',
-    description: 'Distance and gene info for a nearby gene',
-    fields: () => ({
-        distance: {
-            type: new GraphQLNonNull(GraphQLInt),
-            description: 'The distance to the ccRE',
-        },
-        gene: {
-            type: new GraphQLNonNull(GeneExpGene),
-            description: 'The gene',
-        },
-    }),
-});
-
 export const RampageGeneData = new GraphQLObjectType({
     name: 'RampageGeneData',
     fields: () => ({
@@ -223,7 +208,7 @@ export const RampageGeneData = new GraphQLObjectType({
             type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(RampageTranscript))),
         },
         gene: {
-            type: new GraphQLNonNull(RampageGene),
+            type: new GraphQLNonNull(GeneExpGene),
         },
     }),
 });
@@ -379,11 +364,6 @@ export const CreDetailsResponse = new GraphQLObjectType({
                 'Returns intersection counts for cistrome transcription factor and histone modification ChIP-seq data',
             type: new GraphQLNonNull(ChIPSeqIntersections),
             resolve: CreDetailsResolver.resolve_cre_cistromeIntersection,
-        },
-        rampage: {
-            description: 'Returns RAMPAGE data of closest gene',
-            type: new GraphQLNonNull(RampageGeneData),
-            resolve: CreDetailsResolver.resolve_cre_rampage,
         },
         linkedGenes: {
             description: 'Returns linked genes',

graphql/src/schema/RampageResponse.ts
@@ -1,14 +1,0 @@
-import { GraphQLString, GraphQLObjectType } from 'graphql';
-const GraphQLJSON = require('graphql-type-json');
-
-export const RampageResponse = new GraphQLObjectType({
-    name: 'Rampage',
-    description: 'RAMPAGE data',
-    fields: () => ({
-        sortedTranscripts: { type: GraphQLJSON },
-        tsss: { type: GraphQLJSON },
-        gene: { type: GraphQLJSON },
-    }),
-});
-
-export default RampageResponse;

commit ba25ab187af1168ee1feeaf003cd8f83ad3db73d

Fix performance regression from a6b3e4038. Since nearbyGenes actually returns objects now instead just strings, this was sending a new query for each accession. So instead, let's batch all these calls using DataLoader. This can also be sped up a bit more by eliminating the duplicate 'all' and 'pc' queries, either through a more clever query or by changing the schema.

graphql/src/db/db_cache.ts
@@ -7,6 +7,7 @@ import * as DataLoader from 'dataloader';
 import { TypeMap } from 'mime';
 import { Assembly, assaytype, ctspecificdata } from '../types';
 import { getCtSpecificData } from './db_cre_table';
+import { nearbyGene } from '../resolvers/credetails';
 import { reduceAsKeys } from '../utils';
 
 const Raven = require('raven');
@@ -22,6 +23,12 @@ const globalcacheLoader = (cacheMap: loadableglobalcache) =>
 const ccRECtspecificLoader = (assembly: Assembly) =>
     new DataLoader<string, ctspecificdata>(keys => getCtSpecificData(assembly, keys));
 export const ccRECtspecificLoaders = reduceAsKeys(assemblies, ccRECtspecificLoader);
+const nearbyPcGenesLoader = (assembly: Assembly) =>
+    new DataLoader<string, nearbyGene[]>(keys => Common.getGenesMany(assembly, keys, 'pc'));
+const nearbyAllGenesLoader = (assembly: Assembly) =>
+    new DataLoader<string, nearbyGene[]>(keys => Common.getGenesMany(assembly, keys, 'all'));
+export const nearbyPcGenesLoaders = reduceAsKeys(assemblies, nearbyPcGenesLoader);
+export const nearbyAllGenesLoaders = reduceAsKeys(assemblies, nearbyAllGenesLoader);
 
 async function indexFilesTab(assembly) {
     const datasets = await Common.datasets(assembly);

graphql/src/db/db_common.ts
@@ -3,6 +3,7 @@ import { db } from './db';
 import { getCreTable } from './db_cre_table';
 import { loadCache, Biosample } from './db_cache';
 import { Assembly, assaytype } from '../types';
+import { nearbyGene } from '../resolvers/credetails';
 import { UserError } from 'graphql-errors';
 
 export async function chromCounts(assembly) {
@@ -528,11 +529,64 @@ async function getGenes(assembly, accession, allOrPc) {
     return db.any(q, [accession]);
 }
 
-export async function creGenes(assembly, accession, chrom) {
-    return {
-        genesAll: await getGenes(assembly, accession, 'all'),
-        genesPC: await getGenes(assembly, accession, 'pc'),
-    };
+export async function getGenesMany(assembly, accessions: string[], allOrPc): Promise<nearbyGene[][]> {
+    const tableall = assembly + '_cre_all';
+    const tableinfo = assembly + '_gene_info';
+    const tableTss = assembly + '_tss_info';
+    // accession => index
+    // Need to ensure that we return data in the same order that we were asked
+    const requests = accessions.reduce(
+        (prev, accession, index) => {
+            prev[accession] = index;
+            return prev;
+        },
+        {} as Record<string, number>
+    );
+    const q = `
+SELECT g.accession, gi.approved_symbol, g.distance, gi.ensemblid_ver, gi.chrom, gi.start, gi.stop, gi.strand, tss.chrom as tss_chrom, tss.start as tss_start, tss.stop as tss_stop
+FROM (
+    SELECT UNNEST(gene_${allOrPc}_id) geneid, UNNEST(gene_${allOrPc}_distance) distance, accession
+    FROM ${tableall}
+    WHERE accession = ANY($1)
+) AS g
+INNER JOIN ${tableinfo} AS gi
+ON g.geneid = gi.geneid
+INNER JOIN ${tableTss} as tss
+ON gi.ensemblid_ver = tss.ensemblid_ver
+    `;
+    const res = await db.any(q, [accessions]);
+    const map: Record<string, nearbyGene[]> = res.reduce((prev, row) => {
+        prev[row.accession] = prev[row.accession] || [];
+
+        prev[row.accession].push({
+            gene: {
+                gene: row.approved_symbol,
+                ensemblid_ver: row.ensemblid_ver,
+                coords: {
+                    chrom: row.chrom,
+                    start: row.start,
+                    end: row.stop,
+                    strand: row.strand,
+                },
+                tsscoords: {
+                    chrom: row.tss_chrom,
+                    start: row.tss_start,
+                    end: row.tss_stop,
+                    strand: row.strand,
+                },
+            },
+            distance: row.distance,
+        });
+        return prev;
+    }, {} as Record<string, nearbyGene[]>);
+    return Object.keys(map).reduce(
+        (prev, accession) => {
+            const index = requests[accession];
+            prev[index] = map[accession];
+            return prev;
+        },
+        Array.from(Array(requests.length)) as nearbyGene[][]
+    );
 }
 
 export async function getTadOfCRE(assembly, accession) {

graphql/src/resolvers/credetails.ts
@@ -3,19 +3,39 @@ import { getCreTable } from '../db/db_cre_table';
 import { natsort, getAssemblyFromCre } from '../utils';
 import HelperGrouper from '../helpergrouper';
 import { getByGene } from './rampage';
-import { loadCache } from '../db/db_cache';
+import { loadCache, nearbyPcGenesLoaders, nearbyAllGenesLoaders } from '../db/db_cache';
 import { select_cre_intersections, orthologs } from '../db/db_credetails';
 import { Assembly } from '../types';
 
 const request = require('request-promise-native');
 const { UserError } = require('graphql-errors');
 
+export type nearbyGene = {
+    gene: {
+        gene: string;
+        ensemblid_ver: string;
+        coords: {
+            chrom: string;
+            start: number;
+            end: number;
+            strand: string;
+        },
+        tsscoords: {
+            chrom: string;
+            start: number;
+            end: number;
+            strand: string;
+        },
+    },
+    distance: number;
+};
+
 export class CREDetails {
-    assembly;
+    assembly: Assembly;
     accession;
     _coord: Promise<{ chrom: string; start: number; end: number }>;
-    genesAll;
-    genesPC;
+    genesAll: nearbyGene[] | null;
+    genesPC: nearbyGene[] | null;
 
     constructor(assembly: Assembly, accession: string) {
         this.assembly = assembly;
@@ -56,63 +76,27 @@ export class CREDetails {
     }
 
     private async awaitGenes() {
-        const coord = await this.coord();
         if (!this.genesAll || !this.genesPC) {
-            const { genesAll, genesPC } = await DbCommon.creGenes(this.assembly, this.accession, coord.chrom);
+            const genesAll = await nearbyAllGenesLoaders[this.assembly].load(this.accession);
+            const genesPC = await nearbyPcGenesLoaders[this.assembly].load(this.accession);
             this.genesAll = genesAll;
             this.genesPC = genesPC;
         }
+        return { genesAll: this.genesAll, genesPC: this.genesPC };
     }
 
     async nearbyGenes(): Promise<{ gene: any; distance: number; pc: boolean }[]> {
-        await this.awaitGenes();
-        const pcGenes = this.genesPC.map(g => g['approved_symbol']);
-        return this.genesAll
-            .map(g => ({
-                gene: {
-                    gene: g.approved_symbol,
-                    ensemblid_ver: g.ensemblid_ver,
-                    coords: {
-                        chrom: g.chrom,
-                        start: g.start,
-                        end: g.stop,
-                        strand: g.strand,
-                    },
-                    tsscoords: {
-                        chrom: g.tss_chrom,
-                        start: g.tss_start,
-                        end: g.tss_stop,
-                        strand: g.strand,
-                    },
-                },
-                distance: g.distance,
-                pc: pcGenes.includes(g.approved_symbol),
-            }))
-            .sort((a, b) => a.distance - b.distance);
+        const { genesAll, genesPC } = await this.awaitGenes();
+        const pcGenes = genesPC.map(g => g.gene.gene);
+        for (const g of genesAll) {
+            g['pc'] = pcGenes.includes(g.gene.gene);
+        }
+        return genesAll.sort((a, b) => a.distance - b.distance) as (nearbyGene & { pc: boolean })[];
     }
 
     async nearbyPcGenes() {
-        await this.awaitGenes();
-        return this.genesPC
-            .map(g => ({
-                gene: {
-                    gene: g.approved_symbol,
-                    ensemblid_ver: g.ensemblid_ver,
-                    coords: {
-                        chrom: g.chrom,
-                        start: g.start,
-                        end: g.stop,
-                        strand: g.strand,
-                    },
-                    tsscoords: {
-                        chrom: g.tss_chrom,
-                        start: g.tss_start,
-                        end: g.tss_stop,
-                        strand: g.strand,
-                    },
-                },
-                distance: g.distance,
-            }))
+        const { genesPC } = await this.awaitGenes();
+        return genesPC
             .sort((a, b) => a.distance - b.distance);
     }
 