647da89b81e98468ddd1403cdb633bbabcbf33c1
hpratt <hpratt111@live.com>
2019-12-06 23:10:42 -0500

    move around tutorial and UCSC tabs

--- website/ui/src/apps/index/config/maintabs_config.js
+++ website/ui/src/apps/index/config/maintabs_config.js
@@ -1,6 +1,6 @@
 import TabMain from '../components/tab_main';
 import TabAbout from '../components/tab_about';
-import TabTutorial from '../components/tab_tutorial';
+import TabTutorial, { TabUCSC } from '../components/tab_tutorial';
 import TabQuery from '../components/tab_query';
 import TabFiles from '../components/tab_files';
 import TabVersions from '../components/tab_versions';
@@ -9,6 +9,7 @@ import TabAPI from '../components/tab_api';
 const MainTabsConfig = () => {
     return {main : {title: "Overview", visible: true, f: TabMain},
             about : {title: "About", visible: true, f: TabAbout},
+	    ucsc : {title: "UCSC Genome Browser", visible: true, f: TabUCSC},
             tutorial : {title: "Tutorials", visible: true, f: TabTutorial},
 	    /*files: {title: "Downloads", visible: true, f: TabFiles}, */
 	    versions: {title: "Versions", visible: true, f: TabVersions},
