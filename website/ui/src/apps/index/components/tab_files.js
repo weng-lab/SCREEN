3e396f45362588683f596af568af1ab6385cbdd4
hpratt <hpratt111@live.com>
2020-05-18 01:33:27 -0400

    start redesigning download page

--- website/ui/src/apps/index/components/tab_files.js
+++ website/ui/src/apps/index/components/tab_files.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 import * as Render from '../../../common/zrenders';
 import Ztable from '../../../common/components/ztable/ztable';
 import loading from '../../../common/components/loading';
 import {tabPanelize} from '../../../common/utility';
 import * as ApiClient from '../../../common/api_client';
+
+import HumanSVG from './human';
+import MouseSVG from './mouse';
+import Download from './download';
+import RightArrow from './rightarrow';
+import SearchIcon from './search';
 
 const dccLink = (fileID) => {
     if("NA" === fileID){
@@ -15,94 +22,51 @@ const dccLink = (fileID) => {
     return fileDownload(url, fn, fileID);
 }
 
+const check = value => value ? (
+	<span className="glyphicon glyphicon-check" style={{ fontSize: "1.2em", verticalAlign: "middle" }} />
+) : null;
+
 const fileDownload = (url, fn, fileID) => {
     return (
 	<span>
-	    <a href={url} download={fn}>
+	    <a href={url} download>
 		<span className="glyphicon glyphicon-download" aria-hidden="true"
 		      style={{fontSize: "1.2em",
 			      verticalAlign: "middle",
 			      paddingRight: "5px"}} />
 	    </a>
-	    {Render.dccLinkFile(fileID)}
 	</span>);
 }
 
-const CtaTableColumns = () => {
-    let klassCenter = "dt-body-center dt-head-center ";
+const EMBRYONIC = 'EMBRYONIC';
+const ADULT = 'ADULT';
+const CELL_LINES = 'CELL_LINES';
 
-    return [
+const TITLES = {
+    EMBRYONIC: species => "Download cCREs active in " + species + " embryonic tissues",
+    ADULT: species => "Download cCREs active in " + species + " primary cells and tissues",
+    CELL_LINES: species => "Download cCREs active in " + species + " immortalized cell lines",
+};
+
+const CtsTableColumns = ([
+    {
+	    title: "Tissue", data: "tissue"
+    },
 	{
-	    title: "Assembly", data: "assembly", className: klassCenter,
+	    title: "Cell Type", data: "celltype"
 	}, {
-            title: "7 group", data: "5group",
-	    className: klassCenter, render: dccLink
+            title: "DNase", data: "dnase", render: check
 	}, {
-            title: "7 group url", data: "5group", visible: false
+            title: "H3K4me3", data: "h3k4me3", render: check
 	}, {
-	    title: <span>9 state high&nbsp;H3K27ac</span>, data: "9state-H3K27ac",
-	    className: klassCenter, render: dccLink
+            title: "H3K27ac", data: "h3k27ac", render: check
 	}, {
-	    title: <span>9 state high&nbsp;H3K27ac</span>, data: "9state-H3K27ac",
-	    visible: false
+            title: "CTCF", data: "ctcf", render: check
 	}, {
-	    title: <span>9 state high&nbsp;H3K4me3</span>, data: "9state-H3K4me3",
-	    className: klassCenter, render: dccLink
-	}, {
-	    title: <span>9 state high&nbsp;H3K4me3</span>, data: "9state-H3K4me3",
-	    visible: false
-	}, {
-            title: <span>9 state high&nbsp;CTCF</span>, data: "9state-CTCF",
-	    className: klassCenter, render: dccLink
-	}, {
-            title: <span>9 state high&nbsp;CTCF</span>, data: "9state-CTCF",
-	    visible: false
-	}
-    ];
-}
-
-const CtsTableColumns = () => {
-    const klassCenter = "dt-body-center dt-head-center ";
-
-    return [
-	{
-	    title: "Assembly", data: "assembly", className: klassCenter,
-	}, {
-	    title: "Tissue", data: "tissue", className: klassCenter,
-	}, {
-	    title: "Biosample", data: "celltypedesc", className: klassCenter,
-	}, {
-            title: "7 group", data: "5group", className: klassCenter,
+            title: "Download", data: "file", className: "dt-body-center dt-head-center ",
 	    render: dccLink
-	}, {
-            title: "7 group url", data: "5group", visible: false
-	}, {
-            title: <span>9 state high&nbsp;DNase</span>, data: "9state-DNase",
-	    className: klassCenter, render: dccLink
-	}, {
-            title: <span>9 state high&nbsp;DNase</span>, data: "9state-DNase",
-	    visible: false
-	}, {
-	    title: <span>9 state high&nbsp;H3K27ac</span>, data: "9state-H3K27ac",
-	    className: klassCenter, render: dccLink
-	}, {
-	    title: <span>9 state high&nbsp;H3K27ac</span>, data: "9state-H3K27ac",
-	    visible: false
-	}, {
-	    title: <span>9 state high&nbsp;H3K4me3</span>, data: "9state-H3K4me3",
-	    className: klassCenter, render: dccLink
-	}, {
-	    title: <span>9 state high&nbsp;H3K4me3</span>, data: "9state-H3K4me3",
-	    visible: false
-	}, {
-            title: <span>9 state high&nbsp;CTCF</span>, data: "9state-CTCF",
-	    className: klassCenter, render: dccLink
-	}, {
-            title: <span>9 state high&nbsp;CTCF</span>, data: "9state-CTCF",
-	    visible: false
 	}
-    ];
-}
+]);
 
 class TabFiles extends React.Component {
     constructor(props) {
@@ -128,43 +92,132 @@ class TabFiles extends React.Component {
     }
 
     loadFiles(nextProps){
-        if("files" in this.state){
-            return;
-        }
-	if(this.state.isFetching){
-	    return;
-	}
-	this.setState({isFetching: true});
-	ApiClient.globalTabFiles2(
-	    (r) => {
-		this.setState({files: r, isFetching: false, isError: false});
-	    },
-	    (err) => {
-		console.log("err loading files");
-		console.log(err);
-                this.setState({isFetching: false, isError: true});
+	fetch("https://screen-beta.wenglab.org/cCRE-biosample-metadata.GRCh38.json")
+	    .then( response => response.json())
+	    .then( data => {
+                this.setState({ data });
 	    });
     }
 
     doRenderWrapper(){
-        if("files" in this.state){
+	console.log(this.state.data);
 	    return (
 		<div>
-		    <h3>Cell type-agnostic</h3>
-		    <Ztable data={this.state.files["agnostic"]}
-				  cols={CtaTableColumns()}
-				  bFilter={true}
-				  bLengthChange={true}
-		    />
-		    <h3>Cell type-specific</h3>
-		    <Ztable data={this.state.files["specific"]}
-				  cols={CtsTableColumns()}
-				  bFilter={true}
-				  bLengthChange={true}
-                    />
-		</div>);
-        }
-	return loading({...this.state})
+		  <div className="row">
+		    <div className="col-md-6">
+		        <HumanSVG text="926,535 cCREs" />
+		    </div>
+		    <div className="col-md-6">
+                        <MouseSVG text="339,815 cCREs" />
+		    </div>
+		    </div>
+		    <div className="row" style={{ paddingTop: "1.0em" }}>
+		    <div className="col-md-1" />
+                    <div className="col-md-4" style={{ textAlign: "center" }}>
+		        <a className={"btn btn-primary btn-lg"} role={"button"} href="http://gcp.wenglab.org/GRCh38-ccREs.bed" download>
+		    Download all human cCREs<br/><span style={{ fontSize: "0.8em" }}>(GRCh38)</span>
+		        </a>
+		    </div>
+   		    <div className="col-md-2" />
+		    <div className="col-md-4" style={{ textAlign: "center" }}>
+		        <a className={"btn btn-primary btn-lg"} role={"button"} href="http://gcp.wenglab.org/mm10-ccREs.bed" download>
+		    Download all mouse cCREs<br/><span style={{ fontSize: "0.8em" }}>(mm10)</span>
+		        </a>
+		    </div>
+                  </div>
+		    <div className="row" style={{ paddingTop: "1.4em" }}>
+		    <div className="col-md-6" style={{ textAlign: "center" }}>
+		    <div style={{ width: "100%", display: "table" }}>
+		    <div style={{ display: "table-row" }}>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/mm10-ccREs.PLS.bed" title="Promoter-like (PLS)" number={34803} bar="#ff0000"/>
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/mm10-ccREs.pELS.bed" title="Proximal enhancer-like (pELS)" number={141830} bar="#ffa700" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/mm10-ccREs.dELS.bed" title="Distal enhancer-like (dELS)" number={667599} bar="#ffcd00" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/mm10-ccREs.CTCF-only.bed" title="CTCF-only" number={56766} bar="#00b0f0" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/mm10-ccREs.DNase-H3K4me3.bed" title="DNase-H3K4me3" number={25537} bar="#ffaaaa" />
+		    </div>
+		    </div>
+		    </div>
+		    </div>
+		    <div className="col-md-6" style={{ textAlign: "center" }}>
+		    <div style={{ width: "100%", display: "table" }}>
+		    <div style={{ display: "table-row" }}>
+		    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/GRCh38-ccREs.PLS.bed" title="Promoter-like (PLS)" number={23762} bar="#ff0000"/>
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/GRCh38-ccREs.pELS.bed" title="Proximal enhancer-like (pELS)" number={72794} bar="#ffa700" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/GRCh38-ccREs.dELS.bed" title="Distal enhancer-like (dELS)" number={209040} bar="#ffcd00" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		        <Download href="http://gcp.wenglab.org/GRCh38-ccREs.CTCF-only.bed" title="CTCF-only" number={23836} bar="#00b0f0" />
+		    </div>
+                    <div style={{ width: "20%", padding: "2%", display: "table-cell" }}>
+		    <Download href="http://gcp.wenglab.org/GRCh38-ccREs.DNase-H3K4me3.bed" title="DNase-H3K4me3" number={10383} bar="#ffaaaa" />
+		    </div>
+		    </div>
+		    </div>
+		    </div>
+                    </div>
+		    { this.state.data && (
+			<div>
+		    <div className="row" style={{ paddingTop: "1.4em" }}>
+		    <div className="col-md-1" /><div className="col-md-4"><hr/></div><div className="col-md-1" />
+		    <div className="col-md-1" /><div className="col-md-4"><hr/></div><div className="col-md-1" />
+			    </div>
+		    <div className="row" style={{ paddingTop: "1.0em" }}>
+			    <div className="col-md-4" style={{ fontSize: "1.4em", marginLeft: "1.4em" }}>Download cCREs active in a cell type</div><div className="col-md-2" />
+			    <div className="col-md-4" style={{ fontSize: "1.4em", marginLeft: "1.4em" }}>Download cCREs active in a cell type</div><div className="col-md-1" />
+		    </div>
+			    <div className="col-md-6" style={{ textAlign: "center", paddingTop: "1.2em" }}>
+		    <div style={{ width: "100%", display: "table" }}>
+		    <div style={{ display: "table-row" }}>
+                    <div style={{ width: "33%", padding: "2%", display: "table-cell" }}>
+			<SearchIcon
+			    title="Cell Lines"
+			    number={Object.keys(this.state.data).filter( x => this.state.data[x].biosample_type === "cell line" ).length}
+			onClick={ () => { this.setState({ modal: { species: "human", type: CELL_LINES, d: Object.keys(this.state.data).filter( x => this.state.data[x].biosample_type === "cell line" ) } }); }}
+			/>
+		    </div>
+                    <div style={{ width: "33%", padding: "2%", display: "table-cell" }}>
+			<SearchIcon
+			    title="Adult Primary Cells and Tissues"
+			    number={Object.keys(this.state.data).filter( x => this.state.data[x].life_stage === "adult" ).length}
+			onClick={ () => { this.setState({ modal: { species: "human", type: ADULT, d: Object.keys(this.state.data).filter( x => this.state.data[x].life_stage === "adult" && this.state.data[x].biosample_type !== "cell line" ) } }); }}
+			/>
+		    </div>
+		    <div style={{ width: "33%", padding: "2%", display: "table-cell" }}>
+			    <SearchIcon
+			title="Embryonic Tissues"
+			number={Object.keys(this.state.data).filter( x => this.state.data[x].life_stage === "embryonic" ).length}
+			onClick={ () => { this.setState({ modal: { d: Object.keys(this.state.data).filter( x => this.state.data[x].life_stage === "embryonic" && this.state.data[x].biosample_type !== "cell line" ), species: "human", type: EMBRYONIC } }); }}
+			    />
+		    </div>
+		    </div>
+		    </div>
+			    </div>
+			    </div>
+		    )}
+		    <Modal isOpen={!!this.state.modal} onRequestClose={ () => { this.setState({ modal: null }); }}>
+		    { this.state.modal && (
+			<div>
+			    <h2>{TITLES[this.state.modal.type](this.state.modal.species)}</h2>
+			    <Ztable sortCol={[ "tissue", true ]} cols={CtsTableColumns} data={this.state.modal.d.map( k => ({ ...this.state.data[k], celltype: k.replace(/_/g, " "), tissue: this.state.data[k].tissue[0] || "", file: "" }) )} />
+			    </div>
+		        )}
+		    </Modal>
+		</div>    
+	    );
     }
 
     
