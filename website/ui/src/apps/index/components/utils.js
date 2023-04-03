b12391e8e801222f77c3131303ff6865cb1b4632
Jordan Velez-Bomba <jordan.velezbomba@gmail.com>
2023-04-03 15:25:50 -0400

    minor changes: passed props, removed unused packages

--- website/ui/src/apps/index/components/utils.js
+++ website/ui/src/apps/index/components/utils.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export function useGetDownloadFileUrl(primaryurl, secondaryurl) {
   const [url, setUrl] = useState(primaryurl)
